9bd5c22ee1ceff683789c8364f393b5467cd5704
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-05-27 15:42:28 -0300

    Remove unused import

--- packages/fuselage/.storybook/config.js
+++ packages/fuselage/.storybook/config.js
@@ -1,4 +1,4 @@
-import { addDecorator, addParameters, configure, load } from '@storybook/react';
+import { addDecorator, addParameters, configure } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 import { withTests } from '@storybook/addon-jest';
 import { create } from '@storybook/theming';
