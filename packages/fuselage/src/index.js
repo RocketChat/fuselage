e2f0318c71c402ac874732e890cd6a439ac94c5e
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-06-11 16:19:53 -0300

    Add missing exports

--- packages/fuselage/src/index.js
+++ packages/fuselage/src/index.js
@@ -2,5 +2,7 @@ import './helpers/cssCustomPropertiesPonyfill';
 
 export * from './components/Button';
 export * from './components/ButtonGroup';
+export * from './components/CheckBox';
+export * from './components/Grid';
 export * from './components/Tab';
 export * from './components/Tabs';
