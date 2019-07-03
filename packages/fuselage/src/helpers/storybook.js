32e46e15d00679f0f83a5e7b4b4e37850361c0f5
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-07-03 18:21:42 -0300

    Add story to FormField

--- packages/fuselage/src/helpers/storybook.js
+++ packages/fuselage/src/helpers/storybook.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import centered from '@storybook/addon-centered/react';
+import { boolean, text } from '@storybook/addon-knobs/react';
 
 
 export const horizontallyCentered = (storyFn) => centered(() => (
@@ -17,3 +18,18 @@ export const verticallyCentered = (storyFn) => centered(() => (
     {storyFn()}
   </div>
 ));
+
+export const createPropsFromKnobs = (defaults = {}, itemName = null) => () =>
+  Object.entries(defaults).reduce((props, [propName, defaultValue]) => {
+    if (propName === 'children' && React.isValidElement(defaultValue)) {
+      return { ...props, [propName]: defaultValue };
+    }
+
+    const knobName = itemName ? `${ itemName } / ${ propName }` : propName;
+
+    if (typeof defaultValue === 'boolean') {
+      return { ...props, [propName]: boolean(knobName, defaultValue) };
+    }
+
+    return { ...props, [propName]: text(knobName, defaultValue) };
+  }, {});
