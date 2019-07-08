301463bb55ce7a50e147462a934015ccab976975
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-07-08 17:07:34 -0300

    Build an icon set compatible with ES modules

--- packages/icons/helpers.js
+++ packages/icons/helpers.js
@@ -1,13 +1,19 @@
+const isValidIdentifier = require('is-valid-identifier');
+
 const toCamelCase = (string) =>
   string.replace(/([-_][a-z])/ig, (match) => match.toUpperCase().replace('-', '').replace('_', ''));
 
-const createIconList = (icons) =>
-  `module.exports = ${ JSON.stringify(
-    icons.reduce((obj, { name }) => ({ ...obj, [toCamelCase(name)]: name }), {}), null, 2) };
-`;
+const toIdentifier = (string) => (isValidIdentifier(string) ? string : `_${ string }`);
 
-const createIconListModule = (icons) =>
-  icons.map(({ name }) => `export const ${ toCamelCase(name) } = ${ JSON.stringify(name) };`).join('\n');
+const getIconNames = (icons) => Array.from(icons.reduce((set, { name }) => set.add(name), new Set()));
+
+const createIconList = (icons) => getIconNames(icons)
+  .map((name) => `exports.${ toIdentifier(toCamelCase(name)) } = ${ JSON.stringify(name) };`)
+  .join('\n');
+
+const createIconListModule = (icons) => getIconNames(icons)
+  .map((name) => `export const ${ toIdentifier(toCamelCase(name)) } = ${ JSON.stringify(name) };`)
+  .join('\n');
 
 module.exports.createIconList = createIconList;
 module.exports.createIconListModule = createIconListModule;
