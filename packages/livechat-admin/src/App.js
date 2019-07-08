3c98afe75a7aca5937aae656377c0c178bcc9555
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-07-07 22:44:37 -0300

    Update App

--- packages/livechat-admin/src/App.js
+++ packages/livechat-admin/src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { Button, Icon } from '@rocket.chat/fuselage';
-import './App.css';
+
+import { OfficeHours } from './OfficeHours';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Button primary>Without theme <Icon name="cross" /></Button>
-      <div style={{ '--rcx-button-primary-background': 'tomato' }}>
-        <Button primary>With theme <Icon name="check" /></Button>
-      </div>
-    </div>
-  );
+export function App() {
+  return <OfficeHours />;
 }
-
-
-export default App;
