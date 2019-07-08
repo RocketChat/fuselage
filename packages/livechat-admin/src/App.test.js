6a8b0d77906852aad7ed1767493bfd3264d75a05
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-07-07 22:52:23 -0300

    Fix broken test

--- packages/livechat-admin/src/App.test.js
+++ packages/livechat-admin/src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import '../mocks/matchMediaMock';
+import { App } from './App';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
