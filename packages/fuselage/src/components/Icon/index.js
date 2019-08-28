9e01ff599970b855a04e1eebd078f31e88809199
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-08-28 02:08:37 -0300

    Refactor Icon to use Box

--- packages/fuselage/src/components/Icon/index.js
+++ packages/fuselage/src/components/Icon/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
 import * as characters from '@rocket.chat/icons/dist/font/characters';
 import * as names from '@rocket.chat/icons/dist/font/index';
+import React from 'react';
+import styled from 'styled-components';
 
-import { useStyle } from '../../hooks/styles';
-import styles from './styles.scss';
+import { Box } from '../Box';
 
 
 const mapNames = Object.entries(names).reduce((map, [symbol, name]) => Object.assign(map, { [name]: symbol }), {});
 
-export const Icon = React.forwardRef(function Icon({
+const UnstyledIcon = React.forwardRef(({
   name,
-  className,
   ...props
-}, ref) {
-  const iconClassName = useStyle(styles, 'rcx-icon');
+}, ref) => <Box
+  aria-hidden='true'
+  as='i'
+  baseClassName='rcx-icon'
+  ref={ref}
+  {...props}
+/>);
 
-  return <i className={iconClassName} data-char={characters[mapNames[name]]} ref={ref} {...props} />;
-});
+export const Icon = styled(UnstyledIcon)`
+  display: inline-block;
+  font-family: 'RocketChat';
+  font-weight: 400;
+  font-style: normal;
+  font-variant: normal;
+  line-height: 1;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  text-rendering: auto;
+
+  &::before {
+    content: ${ ({ name }) => JSON.stringify(characters[mapNames[name]]) };
+  }
+`;
