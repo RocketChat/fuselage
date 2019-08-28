47a85db57ef2614d2d6ea8be10fe3657d9692bff
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-08-28 02:07:55 -0300

    Add baseClassName to Box

--- packages/fuselage/src/components/Box/index.js
+++ packages/fuselage/src/components/Box/index.js
@@ -1,7 +1,22 @@
 import styled, { css } from 'styled-components';
 
 
-export const Box = styled.span`
+const mapProps = ({
+  baseClassName,
+  className,
+  ...props
+}) => ({
+  className: [
+    baseClassName,
+    ...Object.entries(props).map(([modifier, value]) =>
+      typeof value === 'boolean' && value && `${ baseClassName }--${ modifier }`,
+    ),
+    className,
+  ].filter(Boolean).join(' '),
+  ...props,
+});
+
+export const Box = styled.span.attrs(mapProps)`
   box-sizing: border-box;
   margin: 0;
   padding: 0;
