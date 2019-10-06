a43ac7f2069a28c78bf0e5f12875b38931457595
Tasso Evangelista <tasso@tassoevan.me>
2019-10-06 18:13:26 -0300

    Add CSS utilities

--- packages/fuselage/.storybook/config.js
+++ packages/fuselage/.storybook/config.js
@@ -1,5 +1,6 @@
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import { withTests } from '@storybook/addon-jest';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { addDecorator, addParameters, configure } from '@storybook/react';
 import { create } from '@storybook/theming';
 import 'loki/configure-react';
@@ -34,6 +35,51 @@ addParameters({
     //   const getRootIndex = ({ kind }) => roots.findIndex((root) => kind.startsWith(`${ root }|`));
     //   return getRootIndex(a) - getRootIndex(b);
     // },
+  },
+  viewport: {
+    viewports: {
+      xs: {
+        name: 'xs',
+        styles: {
+          width: '320px',
+          height: '90%',
+        },
+        type: 'desktop',
+      },
+      sm: {
+        name: 'sm',
+        styles: {
+          width: '600px',
+          height: '90%',
+        },
+        type: 'desktop',
+      },
+      md: {
+        name: 'md',
+        styles: {
+          width: '768px',
+          height: '90%',
+        },
+        type: 'desktop',
+      },
+      lg: {
+        name: 'lg',
+        styles: {
+          width: '1024px',
+          height: '90%',
+        },
+        type: 'desktop',
+      },
+      xl: {
+        name: 'xl',
+        styles: {
+          width: '1440px',
+          height: '90%',
+        },
+        type: 'desktop',
+      },
+      ...INITIAL_VIEWPORTS,
+    },
   },
 });
 
