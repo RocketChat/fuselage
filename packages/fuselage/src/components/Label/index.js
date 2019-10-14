20ea6cdd144ddf481744279d125008ca33817df6
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-10-14 02:58:09 -0300

    Update Label component

--- packages/fuselage/src/components/Label/index.js
+++ packages/fuselage/src/components/Label/index.js
@@ -1,40 +1,43 @@
+import { useClassName } from '@rocket.chat/fuselage-hooks';
 import PropTypes from 'prop-types';
 import React, { createContext, useContext } from 'react';
-import { useClassName } from '@rocket.chat/fuselage-hooks';
 
-import { Box } from '../Box';
+import { useTheme } from '../../hooks/useTheme';
+import { StyledLabel, Wrapper, Text, Error } from './styles';
 
 const LabelContext = createContext(false);
 
 export const Label = React.forwardRef(function Label({
   className,
   children,
-
+  disabled,
   error,
+  is,
   position = 'top',
   required,
   text,
-
   ...props
 }, ref) {
-  const isInsideLabel = useContext(LabelContext);
-
   const classNames = {
-    container: useClassName('rcx-label', { required, position }, className),
-    wrapper: useClassName('rcx-label__wrapper', { position }),
-    text: useClassName('rcx-label__text', { required }),
+    container: useClassName('rcx-label', {}, className),
+    wrapper: useClassName('rcx-label__wrapper', {}),
+    text: useClassName('rcx-label__text', {}),
     error: useClassName('rcx-label__error'),
   };
+  const theme = useTheme();
+
+  const isInsideLabel = useContext(LabelContext);
+  const component = is || (isInsideLabel && 'span') || 'label';
 
   return <LabelContext.Provider value={true}>
-    <Box className={classNames.container} is={isInsideLabel ? 'span' : 'label'} ref={ref} {...props}>
-      {(text || error) && <Box className={classNames.wrapper} is='span'>
-        {text && <Box className={classNames.text} is='span' required={required}>{text}</Box>}
-        {error && <Box className={classNames.error} is='span'>{error}</Box>}
-      </Box>}
+    <StyledLabel className={classNames.container} as={component} position={position} ref={ref} theme={theme} {...props}>
+      {(text || error) && <Wrapper className={classNames.wrapper} position={position} theme={theme}>
+        {text && <Text className={classNames.text} disabled={disabled} required={required} theme={theme}>{text}</Text>}
+        {error && <Error className={classNames.error} theme={theme}>{error}</Error>}
+      </Wrapper>}
 
       {children}
-    </Box>
+    </StyledLabel>
   </LabelContext.Provider>;
 });
 
@@ -45,6 +48,9 @@ Label.defaultProps = {
 Label.displayName = 'Label';
 
 Label.propTypes = {
+  disabled: PropTypes.bool,
+  invisible: PropTypes.bool,
+  is: PropTypes.elementType,
   position: PropTypes.oneOf(['top', 'start', 'end']),
   required: PropTypes.bool,
   text: PropTypes.string,
--- packages/fuselage/src/styles/variables/textColors.js
+++ packages/fuselage/src/styles/variables/textColors.js
@@ -6,6 +6,7 @@ export default {
   default: theme('text-colors-default', colors.dark800),
   info: theme('text-colors-info', colors.dark700),
   hint: theme('text-colors-hint', colors.dark600),
+  disabledLabel: theme('text-colors-disabled-label', colors.dark500),
   disabled: theme('text-colors-disabled', colors.dark400),
   alternative: theme('text-colors-alternative', colors.white),
   primary: theme('text-colors-primary', colors.blue500),
