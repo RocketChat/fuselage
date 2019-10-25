dff0cec43bd2a014ec6f62a4371c945be09de8d4
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-10-25 19:11:38 -0300

    Refactor CheckBox styled components

---
+++ packages/fuselage/src/styles/utilities/controls.js
@@ -0,0 +1,74 @@
+import { css } from 'styled-components';
+
+import box from '../box';
+import { clickable } from './interactivity';
+import { visuallyHidden } from './layout';
+
+export const selectionButton = (Input, Fake) => (states, content) => css`
+  ${ box }
+
+  display: inline-flex;
+
+  vertical-align: middle;
+
+  ${ clickable }
+
+  & > ${ Input } {
+    ${ visuallyHidden }
+  }
+
+  & > ${ Fake } {
+    ${ box }
+
+    position: relative;
+
+    height: ${ ({ theme }) => theme.sizes.x20 };
+
+    border-width: ${ ({ theme }) => theme.borders.width.x2 };
+
+    ${ content }
+  }
+
+  ${ ['unchecked', 'checked', 'indeterminate'].map((valueState) => {
+    if (!states[valueState].all) {
+      return;
+    }
+
+    const valueStateClass = valueState === 'unchecked' ? '' : `.${ valueState }`;
+    const valueStatePseudoClass = valueState === 'unchecked' ? '' : `:${ valueState }`;
+
+    return css`
+      & > ${ Input }${ valueStateClass } + ${ Fake },
+      & > ${ Input }${ valueStatePseudoClass } + ${ Fake } {
+        ${ states[valueState].all }
+      }
+    `;
+  }) }
+
+  ${ ['default', 'hover', 'focus', 'active', 'disabled'].map((interactionState) =>
+    ['unchecked', 'checked', 'indeterminate'].map((valueState) => {
+      if (!states[valueState][interactionState]) {
+        return;
+      }
+
+      const interactionStateClass = interactionState === 'default' ? '' : `.${ interactionState }`;
+      const interactionStatePseudoClass = interactionState === 'default' ? '' : `:${ interactionState }`;
+      const valueStateClass = valueState === 'unchecked' ? '' : `.${ valueState }`;
+      const valueStatePseudoClass = valueState === 'unchecked' ? '' : `:${ valueState }`;
+
+      return css`
+        &${ interactionStateClass } > ${ Input }${ valueStateClass } + ${ Fake },
+        &${ interactionStateClass } > ${ Input }${ valueStatePseudoClass } + ${ Fake },
+        & > ${ Input }${ valueStateClass }${ interactionStatePseudoClass } + ${ Fake },
+        & > ${ Input }${ valueStatePseudoClass }${ interactionStatePseudoClass } + ${ Fake } {
+          ${ states[valueState][interactionState] }
+        }
+      `;
+    })
+  ) }
+
+  &.disabled > ${ Input } + ${ Fake },
+  & > ${ Input }:disabled + ${ Fake } {
+    cursor: not-allowed;
+  }
+`;
