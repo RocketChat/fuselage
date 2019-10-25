0749cf37bdced498a2d60b82ce4834a04f3c6059
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-10-25 03:00:36 -0300

    Refactor Callout styled components

--- packages/fuselage/src/components/Callout/styles.js
+++ packages/fuselage/src/components/Callout/styles.js
@@ -1,53 +1,71 @@
-import styled, { css } from 'styled-components';
+import { css } from 'styled-components';
 import colors from '@rocket.chat/fuselage-tokens/colors';
 
 import box from '../../styles/box';
-import { truncate, caption, captionBold } from '../../styles/utilities/typography';
+import { caption, captionBold, truncate } from '../../styles/utilities/typography';
 import { StyledIcon } from '../Icon/styles';
 
-export const StyledCallout = styled.div`
+const container = css`
   ${ box }
 
   display: flex;
+
   margin: 0 0 ${ ({ theme }) => theme.spaces.x24 };
   padding: ${ ({ theme }) => theme.spaces.x16 };
+
   border-radius: ${ ({ theme }) => theme.borders.radius.x2 };
 
-  ${ ({ type }) =>
-    (type === 'info' && css`
-      background-color: ${ colors.blue200 };
-    `)
-    || (type === 'success' && css`
-      background-color: ${ colors.green200 };
-    `)
-    || (type === 'warning' && css`
-      background-color: ${ colors.yellow200 };
-    `)
-    || (type === 'danger' && css`
-      background-color: ${ colors.red200 };
-    `) }
+  ${ ({ modifiers }) => modifiers.type === 'info' && css`
+    background-color: ${ colors.blue200 };
+  ` }
+
+  ${ ({ modifiers }) => modifiers.type === 'success' && css`
+    background-color: ${ colors.green200 };
+  ` }
+
+  ${ ({ modifiers }) => modifiers.type === 'warning' && css`
+    background-color: ${ colors.yellow200 };
+  ` }
+
+  ${ ({ modifiers }) => modifiers.type === 'danger' && css`
+    background-color: ${ colors.red200 };
+  ` }
 
   & > ${ StyledIcon } {
     font-size: ${ ({ theme }) => theme.sizes.x16 };
   }
 `;
 
-export const Wrapper = styled.div`
+const wrapper = css`
   ${ box }
+
+  display: flex;
+
   overflow: hidden;
   flex: 1 1 0;
-  display: flex;
-  flex-flow: column nowrap;
+
   margin: 0 ${ ({ theme }) => theme.spaces.x8 };
+
   color: ${ ({ theme }) => theme.textColors.default };
+  flex-flow: column nowrap;
+
   ${ ({ theme }) => caption(theme) }
 `;
 
-export const Title = styled.div`
+const title = css`
   ${ box }
+
+  ${ ({ theme }) => caption(theme) }
   ${ ({ theme }) => captionBold(theme) }
-  ${ ({ hasChildren, theme }) => hasChildren && css`
+
+  ${ ({ modifiers, theme }) => modifiers.hasChildren && css`
     margin-bottom: ${ theme.spaces.x4 };
   ` }
   ${ truncate }
 `;
+
+export default {
+  'rcx-callout': container,
+  'rcx-callout__wrapper': wrapper,
+  'rcx-callout__title': title,
+};
---
+++ packages/fuselage/src/styles/index.js
@@ -0,0 +1,33 @@
+import styledFn from 'styled-components';
+
+import variables from './variables';
+
+const fromCamelToKebabCase = (camelCaseString) =>
+  camelCaseString.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+
+const mapAttrs = (componentClassName) => ({
+  className,
+  modifiers = {},
+}) => ({
+  className: [
+    componentClassName,
+    ...Object.entries(modifiers)
+      .filter(([, value]) => !!value)
+      .map(([key, value]) => (typeof value === 'boolean'
+        ? `${ componentClassName }--${ fromCamelToKebabCase(key) }`
+        : `${ componentClassName }--${ fromCamelToKebabCase(key) }-${ fromCamelToKebabCase(String(value)) }`)),
+    className,
+  ].filter(Boolean).join(' '),
+});
+
+export const createStyledComponent = (styles, componentClassName, component = 'div') => {
+  const StyledComponent = styledFn(component).attrs(mapAttrs(componentClassName))([], styles[componentClassName] || []);
+
+  StyledComponent.defaultProps = {
+    theme: variables,
+  };
+
+  StyledComponent.displayName = componentClassName;
+
+  return StyledComponent;
+};
