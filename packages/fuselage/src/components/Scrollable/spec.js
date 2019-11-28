0a5896bc1264e671f6f402527a1b7bd33f64c752
Tasso Evangelista <tasso@tassoevan.me>
2019-11-28 17:54:23 -0300

    Add Scrollable

--- packages/fuselage/src/components/Scrollable/spec.js
+++ packages/fuselage/src/components/Scrollable/spec.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { ScrollableArea } from './index';
-
+import { Scrollable } from '../..';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ScrollableArea />, div);
+  ReactDOM.render(<Scrollable />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
