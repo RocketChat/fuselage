ee30c8bdf995b47c3380c61058216d2599d74820
Tasso Evangelista <tasso@tassoevan.me>
2019-12-03 20:10:13 -0300

    Remove unnecessary configuration for ui-kit

--- packages/ui-kit/webpack.config.js
+++ packages/ui-kit/webpack.config.js
@@ -23,71 +23,15 @@ module.exports = (env, argv) => ({
       {
         test: /\.ts$/,
         loader: 'ts-loader',
-        // options: { configFile: './tsconfig.json' },
         exclude: /node_modules/,
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-react', {
-                useBuiltIns: true,
-                development: argv.mode !== 'production',
-              }],
-            ],
-            plugins: ['@babel/plugin-transform-runtime'],
-          },
-        },
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader/useable',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                require('postcss-custom-properties')(),
-                require('autoprefixer')(),
-                require('cssnano'),
-              ],
-            },
-          },
-          'sass-loader',
-        ],
       },
     ],
   },
-  externals: [
-    {
-      react: {
-        commonjs: 'react',
-        commonjs2: 'react',
-        amd: 'react',
-        root: 'React',
-      },
-    },
-    '@rocket.chat/icons',
-    '@rocket.chat/fuselage',
-    '@rocket.chat/ui-kit',
-    '@rocket.chat/fuselage-hooks',
-  ],
   plugins: [
     new ReplacePlugin({
       include: ['index.ts'],
       values: {
         '"VERSION"': JSON.stringify(pkg.version),
-
       },
     }),
   ],
