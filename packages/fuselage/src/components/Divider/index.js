6df2576a0dbefa40e73965c059157de26005aefe
Guilherme Gazzo <guilhermegazzo@gmail.com>
2019-12-03 23:23:05 -0300

    circle

--- packages/fuselage/src/components/Divider/index.js
+++ packages/fuselage/src/components/Divider/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
-
 import { createStyledComponent } from '../../styles';
 
-const Container = createStyledComponent('rcx-divider', 'hr');
+export const Divider = createStyledComponent('rcx-divider', 'hr');
 
-export const Divider = (props) => <Container {...props}/>;
+Divider.displayName = 'Divider';
