547b91f47fc2d5a6d43dd903655d8a010c403740
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-12-16 14:09:43 -0300

    refactor: Atomic styles (#111)
    * Refactor Divider component
    
    * Add missing Loki references
    
    * Deprecate some placeholder selectors
    
    * Add tile atoms
    
    * Fix stylelint errors
    
    * Add button atom styles
    
    * Add input atom styles
    
    * Add prototype of selection button atom styles
    
    * Rewrite some mixins
    
    * Simplify selection button styles

--- packages/fuselage/src/components/Divider/index.js
+++ packages/fuselage/src/components/Divider/index.js
@@ -1,6 +1,7 @@
+import React from 'react';
 
 import { Box } from '../Box';
 
-export const Divider = Box.extend('rcx-divider', 'hr');
-
-Divider.displayName = 'Divider';
+export function Divider(props) {
+  return <Box componentClassName='rcx-divider' is='hr' {...props} />;
+}
