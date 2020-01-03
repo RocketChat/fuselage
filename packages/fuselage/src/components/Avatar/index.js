a56ba6ca3ec959947326e0df5ba24fce19ba0ea0
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-01-03 17:29:02 -0300

    Fix Avatar

--- packages/fuselage/src/components/Avatar/index.js
+++ packages/fuselage/src/components/Avatar/index.js
@@ -1,36 +1,25 @@
-
-import React, { useContext, createContext } from 'react';
 import PropTypes from 'prop-types';
+import React, { useContext, createContext } from 'react';
 
-import { Box, Flex } from '../Box';
+import { Box } from '../Box';
 
-const context = createContext({
+const AvatarContext = createContext({
   baseUrl: '',
 });
 
-const Img = Box.extend('rcx-avatar__element', 'img');
+export function Avatar({ title, url, size = 'x32', ...props }) {
+  const { baseUrl } = useContext(AvatarContext);
 
-const Container = Box.extend('rcx-avatar', 'figure');
-export const Avatar = ({ title, url, size = 'x32', ...props }) => {
-  const modifier = { [`mod-${ size }`]: true };
+  return <Box is='figure' componentClassName='rcx-avatar' aria-label={title} mod-size={size} {...props}>
+    <Box is='img' componentClassName='rcx-avatar__element' src={`${ baseUrl }${ url }`} mod-size={size} />
+  </Box>;
+}
 
-  const { baseUrl } = useContext(context);
-
-  return <Flex.Container><Container {...props} {...modifier} aria-label={title}>
-    <Flex.Item grow={1} shrink={1}><Img src={`${ baseUrl }${ url }`}/></Flex.Item>
-  </Container></Flex.Container>;
-};
-
-Avatar.displayName = 'Avatar';
-
-Avatar.context = context;
+Avatar.Context = AvatarContext;
 
 Avatar.propTypes = {
-  className: PropTypes.string,
   url: PropTypes.string,
   title: PropTypes.string,
-  componentClassName: PropTypes.string,
   is: PropTypes.elementType,
-  style: PropTypes.object,
-  size: PropTypes.oneOf(['x1', 'x2', 'x4', 'x8', 'x12', 'x16', 'x20', 'x24', 'x28', 'x32', 'x36', 'x40', 'x44']),
+  size: PropTypes.string,
 };
