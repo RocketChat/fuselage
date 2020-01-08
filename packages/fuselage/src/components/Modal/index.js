afd5a4a24d0138efe56b4c033183d8b9a145a12d
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-01-08 19:12:47 -0300

    Refactor Modal

--- packages/fuselage/src/components/Modal/index.js
+++ packages/fuselage/src/components/Modal/index.js
@@ -1,66 +1,75 @@
-
 import React from 'react';
 
-import { Tile } from '../Tile';
-import { Box, Margins, MarginsWrapper } from '../Box';
-import { Button } from '../Button';
-import { Scrollable } from '../Box/Scrollable';
-import { Flex } from '../Box/Flex';
-import { Icon } from '../Icon';
 import { Avatar } from '../Avatar';
+import { Box, Flex, Margins, MarginsWrapper, Scrollable } from '../Box';
+import { Button } from '../Button';
+import { Icon } from '../Icon';
+import { Tile } from '../Tile';
 
-
-const HeaderInner = Box.extend('rcx-modal__header-inner', 'div');
-const Container = Box.extend('rcx-modal__container', 'div');
-const WrapperContainer = Box.extend('rcx-modal__container-wrapper', 'div');
-const Footer = Box.extend('rcx-modal__footer', 'footer');
-const Thumb = React.memo((props) => <Avatar size='x32' {...props} />);
-
-
-const Close = React.memo((props) => <Flex.Item shrink={0}><Button small ghost {...props}><Icon name='cross' size='24'/></Button></Flex.Item>);
-
-export const Header = React.memo(({ children, ...props }) => (
-  <Margins all={32}>
-    <Box is='header' className='rcx-modal__header' {...props}>
-      <Flex.Container alignItems='center'>
-        <MarginsWrapper all={8}>
-          <HeaderInner>{React.Children.map(children, (c, i) => <Margins key={i} all={8}>{c}</Margins>)}</HeaderInner>
-        </MarginsWrapper>
-      </Flex.Container>
-    </Box>
-  </Margins>
-));
-
-export const Modal = React.forwardRef(({ children, ...props }, ref) => (
+export const Modal = ({ children, ...props }) =>
   <Flex.Container>
-    <Box componentClassName='rcx-modal' { ...props } is='dialog'>
+    <Box is='dialog' componentClassName='rcx-modal' { ...props }>
       <Flex.Container direction='column'>
         <Flex.Item grow={1}>
-          <Tile ref={ref} elevation='2' className='rcx-modal__inner' padding='none' is='div'>
+          <Tile elevation='2' className='rcx-modal__inner' padding='none'>
             {children}
           </Tile>
         </Flex.Item>
       </Flex.Container>
     </Box>
-  </Flex.Container>
-));
-Modal.Title = React.memo(({ children, ...props }) => (
-  <Flex.Item grow={1} shrink={1}>
-    <Box textStyle='headline' {...props} className='rcx-modal__title'>{children}</Box>
-  </Flex.Item>
-));
-Modal.Header = Header;
-Modal.Footer = React.memo(({ children }) => (
-  <Margins all={32}>
-    <Footer> {children}</Footer>
-  </Margins>
-));
-Modal.Container = ({ children }) => (
+  </Flex.Container>;
+
+export const ModalHeader = ({ children, ...props }) =>
+  <Margins all='32'>
+    <Box is='header' className='rcx-modal__header' {...props}>
+      <Flex.Container alignItems='center' wrap='nowrap'>
+        <MarginsWrapper all='8'>
+          <Box componentClassName='rcx-modal__header-inner'>
+            <Margins all='8'>
+              {children}
+            </Margins>
+          </Box>
+        </MarginsWrapper>
+      </Flex.Container>
+    </Box>
+  </Margins>;
+
+export const ModalThumb = (props) => <Avatar size='x32' {...props} />;
+
+export const ModalTitle = ({ children, ...props }) =>
+  <Flex.Item grow='1' shrink='1'>
+    <Box componentClassName='rcx-modal__title' textColor='default' textStyle='headline' {...props}>
+      {children}
+    </Box>
+  </Flex.Item>;
+
+export const ModalClose = (props) => <Flex.Item shrink={0}>
+  <Button small ghost {...props}>
+    <Icon name='cross' size='x24' />
+  </Button>
+</Flex.Item>;
+
+export const ModalContent = ({ children }) =>
   <Scrollable vertical>
-    <Container>
-      <Margins inline={32}><WrapperContainer>{children}</WrapperContainer></Margins>
-    </Container>
-  </Scrollable>
-);
-Modal.Close = Close;
-Modal.Thumb = Thumb;
+    <Box componentClassName='rcx-modal__content'>
+      <Margins inline='32'>
+        <Box componentClassName='rcx-modal__content-wrapper'>
+          {children}
+        </Box>
+      </Margins>
+    </Box>
+  </Scrollable>;
+
+export const ModalFooter = ({ children }) =>
+  <Margins all='32'>
+    <Box componentClassName='rcx-modal__footer'>
+      {children}
+    </Box>
+  </Margins>;
+
+Modal.Header = ModalHeader;
+Modal.Thumb = ModalThumb;
+Modal.Title = ModalTitle;
+Modal.Close = ModalClose;
+Modal.Content = ModalContent;
+Modal.Footer = ModalFooter;
