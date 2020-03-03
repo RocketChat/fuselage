ab40f313df690022b529a3c416d00a257f391764
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-03-03 17:10:30 -0300

    Shadow, props

--- packages/fuselage/src/components/ScrollableArea/index.js
+++ packages/fuselage/src/components/ScrollableArea/index.js
@@ -3,7 +3,7 @@ import React, { useRef, useCallback, useState, useEffect } from 'react';
 
 import { Box, Scrollable } from '../Box';
 
-export const ScrollableArea = function scrollableArea({ children, style, ...props }) {
+export const ScrollableArea = function scrollableArea({ children, vertical, horizontal, ...props }) {
   const ref = useRef();
 
   const [scrollPos, setScrollPos] = useState({ top: 0, bottom: 0 });
@@ -25,7 +25,7 @@ export const ScrollableArea = function scrollableArea({ children, style, ...prop
   useEffect(() => setScrollPos({ top: !!ref.current.scrollTop, bottom: !!(ref.current.scrollTop + ref.current.offsetHeight - ref.current.scrollHeight) }), []);
 
   return <Box componentClassName='rcx-scrollable-area' is='div' mod-shadow-top={scrollPos.top} mod-shadow-bottom={scrollPos.bottom}>
-    <Scrollable>
+    <Scrollable vertical={vertical} horizontal={horizontal}>
       <Box className='rcx-scrollable-area--box' {...props} children={children} onScroll={handleScroll} ref={ref} />
     </Scrollable>
   </Box>;
