c928cefa49e958e4d915a97bb3db269a58d12c62
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-03-08 18:24:41 -0300

    Wrap CSSStylSheet.insertRule rules into @media all

--- packages/fuselage/src/components/Box/useCss.js
+++ packages/fuselage/src/components/Box/useCss.js
@@ -2,53 +2,35 @@ import hash from '@emotion/hash';
 import Stylis from '@emotion/stylis';
 import { useLayoutEffect, useMemo } from 'react';
 
-const stylis = new Stylis();
+const createReplacementsMapping = (rules = []) => (value) => {
+  if (value === 0) {
+    return '0';
+  }
 
-export const css = (strings, ...values) => (postStrings = []) => {
-  const replacements = values.map((value) => {
-    if (value === 0) {
-      return '0';
-    }
+  if (!value) {
+    return '';
+  }
 
-    if (!value) {
-      return '';
-    }
+  if (typeof value === 'function') {
+    return value(rules);
+  }
 
-    if (typeof value === 'function') {
-      return String(value(postStrings));
-    }
-
-    return String(value);
-  });
-
-  return String.raw(strings, ...replacements);
+  return String(value);
 };
 
-export const keyframes = (strings, ...values) => (postStrings = []) => {
-  const replacements = values.map((value) => {
-    if (value === 0) {
-      return '0';
-    }
-
-    if (!value) {
-      return '';
-    }
-
-    if (typeof value === 'function') {
-      return String(value(postStrings));
-    }
-
-    return String(value);
-  });
-
-  const content = String.raw(strings, ...replacements);
-  const animationName = `rcx-animation-${ hash(content) }`;
-  postStrings.push(`@keyframes ${ animationName } {${ content }}`);
-
-  return animationName;
+export const css = (slices, ...values) => (rules = []) => {
+  const replacements = values.map(createReplacementsMapping(rules));
+  return String.raw(slices, ...replacements);
 };
 
-const styleTagId = 'rcx-styles';
+export const keyframes = (slices, ...values) => (rules = []) => {
+  const replacements = values.map(createReplacementsMapping(rules));
+  const content = String.raw(slices, ...replacements);
+  const encodedAnimationName = `rcx-\\\@${ hash(content) }`;
+  rules.push(`@keyframes ${ encodedAnimationName }{${ content }}`);
+  return encodedAnimationName;
+};
+
 let styleTag;
 let sheet;
 
@@ -57,40 +39,60 @@ const ensureStyleSheet = () => {
     return;
   }
 
+  const styleTagId = 'rcx-styles';
+
   styleTag = document.getElementById(styleTagId) || document.createElement('style');
   if (!styleTag.parentElement) {
     styleTag.id = styleTagId;
+    styleTag.appendChild(document.createTextNode(''));
     document.head.appendChild(styleTag);
   }
 
-  sheet = Array.from(document.styleSheets).find(({ ownerNode }) => ownerNode === styleTag);
+  sheet = styleTag.sheet || Array.from(document.styleSheets).find(({ ownerNode }) => ownerNode === styleTag);
 };
 
-const ruleAttachers = new Map();
+const attachRulesInProduction = (rules) => {
+  ensureStyleSheet();
 
-const attachRule = (className, rule) => {
-  const textNode = document.createTextNode(rule);
+  const index = sheet.insertRule(`@media all{${ rules }}`);
+  const insertedRule = sheet.rules[index];
+
+  return () => {
+    const index = Array.prototype.findIndex.call(sheet.rules, (currentRule) => currentRule === insertedRule);
+    sheet.deleteRule(index);
+  };
+};
+
+const attachRulesInDevelopment = (rules) => {
+  ensureStyleSheet();
+
+  const textNode = document.createTextNode(rules);
   styleTag.appendChild(textNode);
+
   return () => textNode.remove();
 };
 
+const attachRules = (process.env.NODE_ENV === 'production' && attachRulesInProduction)
+  || attachRulesInDevelopment;
+
+const ruleAttachers = new Map();
+const emptyEffect = () => {};
+
 const getRuleAttacher = (className, rule) => {
-  if (!className || !rule) {
-    return () => {};
+  if (!className) {
+    return emptyEffect;
   }
 
   if (ruleAttachers.has(className)) {
     return ruleAttachers.get(className);
   }
 
-  ensureStyleSheet();
-
   let count = 0;
   let detachRule;
 
   ruleAttachers.set(className, () => {
     if (count === 0) {
-      detachRule = attachRule(className, rule);
+      detachRule = attachRules(rule);
     }
     ++count;
 
@@ -106,25 +108,26 @@ const getRuleAttacher = (className, rule) => {
   return ruleAttachers.get(className);
 };
 
-export const useCss = (cssFn, deps) => {
-  const [className, rule] = useMemo(() => {
-    const postStrings = [];
-    const css = [
-      ...Array.isArray(cssFn)
-        ? cssFn.map((fn) => fn && fn(postStrings)).filter(Boolean)
-        : [cssFn(postStrings)],
-      ...postStrings,
-    ].join('');
+const stylis = new Stylis();
 
-    if (!css) {
+export const useCss = (css, deps) => {
+  const [className, rules] = useMemo(() => {
+    const cssFns = (Array.isArray(css) ? css : [css]).filter(Boolean);
+    const rules = [];
+    rules.push(...cssFns.map((cssFn) => cssFn(rules)));
+    const content = rules.filter(Boolean).join('');
+
+    if (!content) {
       return [];
     }
 
-    const cssHash = hash(css);
-    return [`rcx${ cssHash }`, stylis(`.rcx-box.rcx${ cssHash }`, css)];
+    const className = `rcx-@${ hash(content) }`;
+    const encodedClassName = `rcx-\\@${ hash(content) }`;
+    const transpiledContent = stylis(`.rcx-box.${ encodedClassName }`, content);
+    return [className, transpiledContent];
   }, deps);
 
-  useLayoutEffect(getRuleAttacher(className, rule), [className, rule]);
+  useLayoutEffect(getRuleAttacher(className, rules), [className, rules]);
 
   return className;
 };
