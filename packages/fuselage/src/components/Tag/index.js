e3b7f7cca4fe2ecc4ca96cd2db184d991595ef52
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-03-16 10:35:40 -0300

    Proptypes, code style, cursor pointer

--- packages/fuselage/src/components/Tag/index.js
+++ packages/fuselage/src/components/Tag/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Box } from '../..';
 
 export function Tag({
-  variant = 'default',
+  variant = 'secondary',
   disabled,
   round,
+  onClick,
   ...props
 }) {
   return <Box
     is='div'
     componentClassName='rcx-tag'
-    mod-default={variant === 'default'}
+    mod-secondary={variant === 'secondary'}
     mod-primary={variant === 'primary'}
     mod-danger={variant === 'danger'}
     mod-disabled={!!disabled}
     mod-round={!!round}
+    mod-clickable={!!onClick}
+    onClick={onClick}
     {...props}
   />;
 }
+
+Tag.propTypes = {
+  variant: PropTypes.oneOf(['secondary', 'primary', 'danger']),
+  round: PropTypes.bool,
+  disabled: PropTypes.bool,
+};
