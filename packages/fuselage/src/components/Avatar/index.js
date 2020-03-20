406554b7a31d96b60133126c649a6857c0471c08
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-03-20 14:31:43 -0300

    feat: add stack avatar (#174)

--- packages/fuselage/src/components/Avatar/index.js
+++ packages/fuselage/src/components/Avatar/index.js
@@ -7,19 +7,36 @@ const AvatarContext = createContext({
   baseUrl: '',
 });
 
-export function Avatar({ title, url, size = 'x32', ...props }) {
+export function Avatar({ title, rounded, url, size = 'x32', ...props }) {
   const { baseUrl } = useContext(AvatarContext);
 
-  return <Box is='figure' componentClassName='rcx-avatar' aria-label={title} mod-size={size} {...props}>
-    <Box is='img' componentClassName='rcx-avatar__element' src={`${ baseUrl }${ url }`} mod-size={size} />
+  return <Box
+    is='figure'
+    componentClassName='rcx-avatar'
+    aria-label={title}
+    mod-size={size}
+    {...props}
+  >
+    <Box
+      is='img'
+      componentClassName='rcx-avatar__element'
+      src={`${ baseUrl }${ url }`}
+      mod-size={size}
+      mod-rounded={rounded}
+    />
   </Box>;
 }
 
 Avatar.Context = AvatarContext;
 
 Avatar.propTypes = {
+  rounded: PropTypes.bool,
   url: PropTypes.string,
   title: PropTypes.string,
   is: PropTypes.elementType,
   size: PropTypes.string,
 };
+
+const Stack = ({ children }) => <Box componentClassName='rcx-avatar-stack'>{React.Children.toArray(children).reverse()}</Box>;
+
+Avatar.Stack = Stack;
