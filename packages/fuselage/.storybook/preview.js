0cad807ea3df9ab4b174dee8cc97ea872b897c50
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-03-26 16:16:38 -0300

    Fix stories order

--- packages/fuselage/.storybook/preview.js
+++ packages/fuselage/.storybook/preview.js
@@ -15,6 +15,12 @@ addParameters({
   grid: {
     cellSize: 4,
   },
+  options: {
+    showRoots: true,
+    storySort: ([, a], [, b]) => {
+      return a.kind.localeCompare(b.kind);
+    },
+  },
   viewport: {
     viewports: {
       xs: {
