9b300ccd2bdb48c8b7bc11696e171e962cfad23b
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-04-01 11:53:58 -0300

    Now using Box props instead of Flex component

--- packages/fuselage/src/components/SideBar/Item/room.js
+++ packages/fuselage/src/components/SideBar/Item/room.js
@@ -13,57 +13,36 @@ const viewModeToAvatarSizeMap = {
 export function SidebarItemRoom({ viewMode = 'medium', avatar, status, roomType, lastMessage, title, unreadCount, ...props }) {
   const extendedView = viewMode === 'extended';
 
-  return <Flex.Container direction='row' {...props}>
-    <Box is='div' componentClassName='rcx-sidebar__item' mod-unread={!!unreadCount} {...props}>
-      <Margins inline='x4'>
-        <Flex.Item align='center'>
-          <Avatar size={viewModeToAvatarSizeMap[viewMode]} {...avatar}/>
-        </Flex.Item>
-      </Margins>
+  return <Box is='div' display='flex' flexDirection='row' componentClassName='rcx-sidebar__item' mod-unread={!!unreadCount} {...props}>
 
+    <Avatar mi='x4' alignSelf='center' size={viewModeToAvatarSizeMap[viewMode]} {...avatar}/>
 
-      <Flex.Container direction={extendedView ? 'column' : 'row'} justifyContent='space-between'>
-        <Box is='div' componentClassName='rcx-sidebar__item__text'>
+    <Box is='div' display='flex' flexDirection={extendedView ? 'column' : 'row'} justifyContent='space-between' componentClassName='rcx-sidebar__item__text'>
 
-          <Flex.Container direction='row' alignItems='center' justifyContent='space-between'>
-            <Box is='div'>
+      <Box is='div' display='flex' flexDirection='row' alignItems='center' justifyContent='space-between'>
+        <Box display='flex' flexDirection='row' alignItems='center'>
+          <Margins inline='x4'>
+            {roomType === 'd' && <UserStatus status={status} />}
+            {roomType !== 'd' && <Icon size={16} name={roomType === 'p' ? 'lock' : 'hashtag'} />}
 
-              <Flex.Container direction='row' alignItems='center'>
-                <Box>
-
-                  <Margins inline='x4'>
-                    <Flex.Container alignItems='center'>
-                      {roomType === 'd' && <UserStatus status={status} />}
-                      {roomType !== 'd' && <Icon size={16} name={roomType === 'p' ? 'lock' : 'hashtag'} />}
-                    </Flex.Container>
-
-                    {viewMode !== 'condensed' && <Box textStyle='p1' componentClassName='rcx-sidebar__item__title'>{title}</Box>}
-                    {viewMode === 'condensed' && <Box textStyle='c1' componentClassName='rcx-sidebar__item__title'>{title}</Box>}
-                  </Margins>
-
-                </Box>
-              </Flex.Container>
-
-              <Margins inline='x4'>
-                {extendedView && <Box is='div' textStyle='micro' textColor='hint'>{lastMessage.time}</Box>}
-              </Margins>
-            </Box>
-          </Flex.Container>
-
-          {(unreadCount || extendedView) && <Flex.Container direction='row' justifyContent='space-between'>
-            <Box is='div'>
-              <Margins inline='x4'>
-                {extendedView && <Box textStyle='p1' componentClassName='rcx-sidebar__item__description'>{`${ lastMessage.username }: ${ lastMessage.text }`}</Box>}
-                {unreadCount && <Flex.Item align='center'><Badge style={{ width: 'fit-content', display: 'block' }} variant={unreadCount.hasMention ? 'primary' : undefined}>{unreadCount.count}</Badge></Flex.Item>}
-              </Margins>
-            </Box>
-          </Flex.Container>}
-
+            {viewMode !== 'condensed' && <Box textStyle='p1' componentClassName='rcx-sidebar__item__title'>{title}</Box>}
+            {viewMode === 'condensed' && <Box textStyle='c1' componentClassName='rcx-sidebar__item__title'>{title}</Box>}
+          </Margins>
         </Box>
-      </Flex.Container>
+
+        {extendedView && <Box is='div' mi='x4' textStyle='micro' textColor='hint'>{lastMessage.time}</Box>}
+      </Box>
+
+      {(unreadCount || extendedView) && <Box is='div' display='flex' flexDirection='row' justifyContent='space-between'>
+        <Margins inline='x4'>
+          {extendedView && <Box textStyle='p1' componentClassName='rcx-sidebar__item__description'>{`${ lastMessage.username }: ${ lastMessage.text }`}</Box>}
+          {unreadCount && <Badge alignSelf='center' variant={unreadCount.hasMention ? 'primary' : 'ghost'}>{unreadCount.count}</Badge>}
+        </Margins>
+      </Box>}
 
     </Box>
-  </Flex.Container>;
+
+  </Box>;
 }
 
 SidebarItemRoom.propTypes = {
