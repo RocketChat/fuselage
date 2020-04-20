4d6200b291ca543f750e789898b74b1ee67b21e6
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-04-20 01:37:11 -0300

    Remove text-styles module

--- packages/fuselage/.storybook/helpers.js
+++ packages/fuselage/.storybook/helpers.js
@@ -8,13 +8,13 @@ export function PropsVariationSection({ component: Component, common = {}, xAxis
       <Box is='tr'>
         <Box is='th' />
         {Object.keys(xAxis).map((xVariation, key) =>
-          <Box key={key} is='th' textColor='hint' textStyle='caption'>{xVariation}</Box>)}
+          <Box key={key} is='th' textColor='hint' textStyle='c1'>{xVariation}</Box>)}
       </Box>
     </Box>
     <Box is='tbody'>
       {Object.entries(yAxis).map(([yVariation, yProps], y) => (
         <Box key={y} is='tr'>
-          <Box is='th' textColor='hint' textStyle='caption'>{yVariation}</Box>
+          <Box is='th' textColor='hint' textStyle='c1'>{yVariation}</Box>
           {Object.values(xAxis).map((xProps, x) => <Box key={x} is='td' style={{ margin: 0, padding: '0.5rem 1rem' }}>
             <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
               <Component {...common} {...xProps} {...yProps} />
