8b805c6dd1057d1b44ae92984878bbc97c1a963e
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-04-21 23:40:34 -0300

    Direct only specific input props to input element

--- packages/fuselage/src/components/ToggleSwitch/index.js
+++ packages/fuselage/src/components/ToggleSwitch/index.js
@@ -1,28 +1,66 @@
+import PropTypes from 'prop-types';
 import React, { forwardRef } from 'react';
 
 import { Label } from '../Label';
 import { Box } from '../Box';
 
 export const ToggleSwitch = forwardRef(function ToggleSwitch({
-  className,
-  hidden,
-  invisible,
-  style,
-  title,
-  onClick,
+  autoComplete,
+  checked,
+  defaultChecked,
+  disabled,
+  form,
+  id,
+  name,
+  required,
+  tabIndex,
+  value,
+  qa,
+  'data-qa': dataQa,
+  onChange,
+  onInput,
+  onInvalid,
   ...props
 }, ref) {
-  return <Box
-    is={Label}
-    componentClassName='rcx-toggle-switch'
-    className={className}
-    hidden={hidden}
-    invisible={invisible}
-    style={style}
-    title={title}
-    onClick={onClick}
-  >
-    <Box is='input' componentClassName='rcx-toggle-switch__input' ref={ref} type='checkbox' {...props} />
+  return <Box is={Label} componentClassName='rcx-toggle-switch' {...props}>
+    <Box
+      is='input'
+      componentClassName='rcx-toggle-switch__input'
+      autoComplete={autoComplete}
+      checked={checked}
+      defaultChecked={defaultChecked}
+      disabled={disabled}
+      form={form}
+      id={id}
+      name={name}
+      required={required}
+      tabIndex={tabIndex}
+      type='checkbox'
+      value={value}
+      data-qa={dataQa || qa}
+      ref={ref}
+      onChange={onChange}
+      onInput={onInput}
+      onInvalid={onInvalid}
+    />
     <Box is='i' componentClassName='rcx-toggle-switch__fake' aria-hidden='true' />
   </Box>;
 });
+
+ToggleSwitch.propTypes = {
+  autoComplete: PropTypes.string,
+  checked: PropTypes.bool,
+  defaultChecked: PropTypes.bool,
+  disabled: PropTypes.bool,
+  form: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  required: PropTypes.bool,
+  tabIndex: PropTypes.number,
+  value: PropTypes.string,
+  qa: PropTypes.string,
+  'data-qa': PropTypes.string,
+  onChange: PropTypes.func,
+  onInput: PropTypes.func,
+  onInvalid: PropTypes.func,
+};
