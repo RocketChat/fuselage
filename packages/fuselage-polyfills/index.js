76e90703a555ab8133acc16478375d68d41002f7
Gabriel Henriques <gabriel.henriques@rocket.chat>
2020-05-15 11:12:48 -0300

    Added clipboard polyfill

--- packages/fuselage-polyfills/index.js
+++ packages/fuselage-polyfills/index.js
@@ -1,3 +1,5 @@
 require('focus-visible');
+var clipboard = require('clipboard-polyfill');
 var ResizeObserver = require('@juggle/resize-observer');
 window.ResizeObserver = window.ResizeObserver || ResizeObserver.ResizeObserver;
+navigator.clipboard = navigator.clipboard || clipboard;
