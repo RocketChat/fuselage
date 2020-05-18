d2d8782b256d662b44faa60da256888e3c7f8625
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-05-18 20:08:17 -0300

    Update packages/fuselage-polyfills/index.js
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

--- packages/fuselage-polyfills/index.js
+++ packages/fuselage-polyfills/index.js
@@ -1,5 +1,3 @@
 require('focus-visible');
-var clipboard = require('clipboard-polyfill');
-var ResizeObserver = require('@juggle/resize-observer');
-window.ResizeObserver = window.ResizeObserver || ResizeObserver.ResizeObserver;
-navigator.clipboard = navigator.clipboard || clipboard;
+window.ResizeObserver = window.ResizeObserver || require('@juggle/resize-observer').ResizeObserver;
+navigator.clipboard = navigator.clipboard || require('clipboard-polyfill');
