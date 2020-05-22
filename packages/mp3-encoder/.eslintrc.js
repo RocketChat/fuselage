a97a9d1a0205d2a2a3bb706c17cca85bb4f2224a
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-05-22 17:24:17 -0300

    Fix jest configuration

--- packages/mp3-encoder/.eslintrc.js
+++ packages/mp3-encoder/.eslintrc.js
@@ -1,64 +1,54 @@
 module.exports = {
-  extends: ['@rocket.chat/eslint-config'],
-  parser: 'babel-eslint',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    '@rocket.chat/eslint-config',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+    warnOnUnsupportedTypeScriptVersion: false,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      legacyDecorators: true,
+    },
+  },
+  plugins: ['@typescript-eslint'],
   rules: {
-    indent: ['error', 2],
+    'indent': ['error', 2, {
+      SwitchCase: 1,
+    }],
     'import/order': ['error', {
       'newlines-between': 'always',
       groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
     }],
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/indent': ['error', 2, {
+      SwitchCase: 1,
+    }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': [
+      'error',
+      'always'
+    ],
   },
-  overrides: [
-    {
-      files: [
-        '**/*.ts',
-        '**/*.tsx'
-      ],
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        '@rocket.chat/eslint-config'
-      ],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2018,
-        warnOnUnsupportedTypeScriptVersion: false,
-        ecmaFeatures: {
-          experimentalObjectRestSpread: true,
-          legacyDecorators: true,
-        },
-      },
-      plugins: ['@typescript-eslint'],
-      rules: {
-        '@typescript-eslint/ban-ts-ignore': 'off',
-        '@typescript-eslint/indent': [
-          'error',
-          'tab'
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.ts',
+          '.tsx'
         ],
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/interface-name-prefix': [
-          'error',
-          'always'
-        ],
-      },
-      env: {
-        browser: true,
-        commonjs: true,
-        es6: true,
-        node: true
-      },
-      settings: {
-        'import/resolver': {
-          node: {
-            extensions: [
-              '.js',
-              '.ts',
-              '.tsx'
-            ],
-          },
-        },
       },
     },
-  ],
+  },
 };
