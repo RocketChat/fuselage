48736c86fb27052bd8d8c3f44794be6bd1f58c18
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-06-03 17:54:45 -0300

    Add types declaration

--- packages/mp3-encoder/rollup.config.js
+++ packages/mp3-encoder/rollup.config.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
@@ -5,24 +7,34 @@ import typescript from '@rollup/plugin-typescript';
 
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: path.dirname(pkg.main),
+      entryFileNames: path.basename(pkg.main),
       format: 'cjs',
       sourcemap: true,
     },
-    {
+    plugins: [
+      external(),
+      typescript({ declaration: true, declarationDir: 'dist/' }),
+      resolve(),
+      commonjs(),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
       file: pkg.module,
       format: 'esm',
       sourcemap: true,
     },
-  ],
-  plugins: [
-    external(),
-    typescript(),
-    resolve(),
-    commonjs(),
-  ],
-};
+    plugins: [
+      external(),
+      typescript(),
+      resolve(),
+      commonjs(),
+    ],
+  },
+];
