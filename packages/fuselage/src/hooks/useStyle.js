20c320b10f42e6aa95213f862fc5475be9fa4cd4
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-03 02:19:49 -0300

    Improve useStyle

--- packages/fuselage/src/hooks/useStyle.js
+++ packages/fuselage/src/hooks/useStyle.js
@@ -1,6 +1,42 @@
-import { useClassNameMapping } from './useClassNameMapping';
+import {
+  createClassName,
+  escapeName,
+  transpile,
+  attachRules,
+} from '@rocket.chat/css-in-js';
+import { useDebugValue, useEffect, useMemo } from 'react';
 
-export const useStyle = (cssFn, ...args) => {
-  const mapClassName = useClassNameMapping(...args);
-  return mapClassName(cssFn);
+export const useStyle = (cssFn, arg) => {
+  const content = useMemo(() => (cssFn ? cssFn(arg) : undefined), [arg, cssFn]);
+
+  const className = useMemo(() => {
+    if (!content) {
+      return;
+    }
+
+    return content ? createClassName(content) : undefined;
+  }, [content]);
+
+  useDebugValue(className);
+
+  const transpiledContent = useMemo(() => {
+    if (!content || !className) {
+      return;
+    }
+
+    const escapedClassName = escapeName(className);
+    const transpiledContent = transpile(`.${ escapedClassName }`, content);
+
+    return transpiledContent;
+  }, [className, content]);
+
+  useEffect(() => {
+    if (!transpiledContent) {
+      return;
+    }
+
+    return attachRules(transpiledContent);
+  }, [transpiledContent]);
+
+  return className;
 };
