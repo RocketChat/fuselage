5f9c11e8bdd1cbd83af4d0bc6e23d984c2c4affd
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-08 15:39:27 -0300

    Add support for not only Box children in Margins

--- packages/fuselage/src/components/Margins/index.js
+++ packages/fuselage/src/components/Margins/index.js
@@ -1,19 +1,26 @@
 import React, { useCallback } from 'react';
+import { css } from '@rocket.chat/css-in-js';
 
-import { BoxTransforms, useComposedBoxTransform } from '../Box/transforms';
+import { appendClassName } from '../../helpers/appendClassName';
 import { createPropType } from '../../helpers/createPropType';
+import { patchChildren } from '../../helpers/patchChildren';
+import { useStyle } from '../../hooks/useStyle';
 import { margin } from '../../styleTokens';
+import { BoxTransforms, useComposedBoxTransform } from '../Box/transforms';
 
-function Margins({
-  children,
-  all,
-  block,
-  blockStart,
-  blockEnd,
-  inline,
-  inlineStart,
-  inlineEnd,
-}) {
+function Margins(props) {
+  const {
+    children,
+    className,
+    all,
+    block,
+    blockStart,
+    blockEnd,
+    inline,
+    inlineStart,
+    inlineEnd,
+  } = props;
+
   const transformFn = useCallback((props) => {
     if (all !== undefined && props.margin === undefined) {
       props.margin = all;
@@ -46,8 +53,30 @@ function Margins({
     return props;
   }, [all, block, blockEnd, blockStart, inline, inlineEnd, inlineStart]);
 
+  const marginsClassName = useStyle(css`
+    &:not(.rcx-box) {
+      ${ props.all && css`margin: ${ margin(props.all) };` }
+      ${ props.inline && css`margin-inline: ${ margin(props.inline) };` }
+      ${ props.inlineStart && css`margin-inline-start: ${ margin(props.inlineStart) };` }
+      ${ props.inlineEnd && css`margin-inline-end: ${ margin(props.inlineEnd) };` }
+      ${ props.block && css`margin-block: ${ margin(props.block) };` }
+      ${ props.blockStart && css`margin-block-start: ${ margin(props.blockStart) };` }
+      ${ props.blockEnd && css`margin-block-end: ${ margin(props.blockEnd) };` }
+    }
+  `, props);
+
+  const patchedChildren = patchChildren(children, (childProps) => ({
+    className: appendClassName(
+      appendClassName(
+        childProps.className,
+        marginsClassName,
+      ),
+      className,
+    ),
+  }));
+
   return <BoxTransforms.Provider
-    children={children}
+    children={patchedChildren}
     value={useComposedBoxTransform(transformFn)}
   />;
 }
--- packages/fuselage/src/components/Margins/spec.js
+++ packages/fuselage/src/components/Margins/spec.js
@@ -8,3 +8,16 @@ it('renders without crashing', () => {
   ReactDOM.render(<Margins />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('patches non-`Box` children', () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Margins all='10px'>
+    <div />
+  </Margins>, root);
+
+  const div = root.firstChild;
+  expect(getComputedStyle(div).marginLeft).toBe('10px');
+  expect(getComputedStyle(div).marginTop).toBe('10px');
+  expect(getComputedStyle(div).marginRight).toBe('10px');
+  expect(getComputedStyle(div).marginBottom).toBe('10px');
+});
