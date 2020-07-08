68249a83180fc9d6be3133ba3332baa141703196
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-08 15:39:07 -0300

    Skip non-React elements in patchElement

--- packages/fuselage/src/helpers/patchChildren.js
+++ packages/fuselage/src/helpers/patchChildren.js
@@ -1,4 +1,4 @@
-import { cloneElement } from 'react';
+import { cloneElement, isValidElement } from 'react';
 
 import { flattenChildren } from './flattenChildren';
 import { shallowEqual } from './shallowEqual';
@@ -7,6 +7,10 @@ export const patchChildren = (children, patch) => {
   let dirty = false;
 
   const newChildren = flattenChildren(children).map((child) => {
+    if (!isValidElement(child)) {
+      return child;
+    }
+
     const mergedProps = patch(child.props);
 
     if (shallowEqual(child.props, mergedProps)) {
