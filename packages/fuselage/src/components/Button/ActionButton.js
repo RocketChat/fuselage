3b368c7e02afa83107ed6ce1e90d7233222714fe
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-10-14 01:59:01 -0300

    Tiny and mini variations

--- packages/fuselage/src/components/Button/ActionButton.js
+++ packages/fuselage/src/components/Button/ActionButton.js
@@ -3,4 +3,20 @@ import React from 'react';
 import { Button } from '.';
 import { Icon } from '../Icon';
 
-export const ActionButton = ({ icon, ...props }) => <Button {...props} square small flexShrink={0}><Icon name={icon} size='x20'/></Button>;
+const getSize = ({ tiny, mini, small }) => {
+  if (mini) {
+    return 'x16';
+  }
+
+  if (tiny) {
+    return 'x20';
+  }
+
+  if (small) {
+    return 'x24';
+  }
+
+  return 'x20';
+};
+
+export const ActionButton = ({ icon, ...props }) => <Button {...props} square small flexShrink={0}><Icon name={icon} size={getSize(props)}/></Button>;
