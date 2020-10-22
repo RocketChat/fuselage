8a5d560c3fc4b26e537935b122c9f3c047ccec0e
Douglas Fabris <deefabris@gmail.com>
2020-10-22 16:23:19 -0300

    [NEW] Element.closest Polyfill (#305)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- packages/fuselage-polyfills/index.js
+++ packages/fuselage-polyfills/index.js
@@ -1,3 +1,4 @@
 require('focus-visible');
 window.ResizeObserver = window.ResizeObserver || require('@juggle/resize-observer').ResizeObserver;
 navigator.clipboard = navigator.clipboard || require('clipboard-polyfill');
+require('element-closest-polyfill');
