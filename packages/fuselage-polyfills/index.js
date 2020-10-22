0d9a220a830f4d2fc235b125e1bb33eb0062974d
dougfabris <deefabris@gmail.com>
2020-10-22 14:42:46 -0300

    Element.closest Polyfill added

--- packages/fuselage-polyfills/index.js
+++ packages/fuselage-polyfills/index.js
@@ -1,3 +1,4 @@
 require('focus-visible');
 window.ResizeObserver = window.ResizeObserver || require('@juggle/resize-observer').ResizeObserver;
 navigator.clipboard = navigator.clipboard || require('clipboard-polyfill');
+Element.closest = Element.closest || require('element-closest-polyfill');
