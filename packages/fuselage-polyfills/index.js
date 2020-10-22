ba386398f8197eb133ac2c03bed2a13f836be606
Guilherme Gazzo <guilhermegazzo@gmail.com>
2020-10-22 16:06:58 -0300

    Update index.js

--- packages/fuselage-polyfills/index.js
+++ packages/fuselage-polyfills/index.js
@@ -1,4 +1,4 @@
 require('focus-visible');
 window.ResizeObserver = window.ResizeObserver || require('@juggle/resize-observer').ResizeObserver;
 navigator.clipboard = navigator.clipboard || require('clipboard-polyfill');
-if(!Element.closest){ require('element-closest-polyfill') };
+require('element-closest-polyfill');
