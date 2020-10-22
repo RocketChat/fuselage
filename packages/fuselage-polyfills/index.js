42d41478678d74c730df9bcb30ab76e6b27314f1
gabriellsh <40830821+gabriellsh@users.noreply.github.com>
2020-10-22 18:32:57 -0300

    feat: Focus-within polyfill (#306)

--- packages/fuselage-polyfills/index.js
+++ packages/fuselage-polyfills/index.js
@@ -1,3 +1,4 @@
+require('focus-within-polyfill');
 require('focus-visible');
 window.ResizeObserver = window.ResizeObserver || require('@juggle/resize-observer').ResizeObserver;
 navigator.clipboard = navigator.clipboard || require('clipboard-polyfill');
