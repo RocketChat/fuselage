07afd1e06131b1281ee8a83e947899974df5a24d
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2020-11-05 01:05:35 -0300

    Remove Box component dependence from Tag component

--- packages/fuselage/src/components/Tag/index.js
+++ packages/fuselage/src/components/Tag/index.js
@@ -1,33 +1,46 @@
+import { css } from '@rocket.chat/css-in-js';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { Box } from '../..';
+import { appendClassName } from '../../helpers/appendClassName';
+import { prependClassName } from '../../helpers/prependClassName';
+import { useStyle } from '../../hooks/useStyle';
+import { fontScale } from '../../styleTokens';
+
+const useTagPropsToClass = (props) => {
+  props.className = prependClassName(props.className, 'rcx-tag');
+
+  if (props.disabled) {
+    props.className = prependClassName(props.className, 'rcx-tag--disabled');
+  }
+
+  props.className = props.variant
+    ? prependClassName(props.className, `rcx-tag--${ props.variant }`)
+    : prependClassName(props.className, 'rcx-tag--secondary');
+  delete props.variant;
+
+  const newClassName = useStyle(css`
+    font-size: ${ fontScale(props.fontScale)?.fontSize } !important;
+    font-weight: ${ fontScale(props.fontScale)?.fontWeight } !important;
+    letter-spacing: ${ fontScale(props.fontScale)?.letterSpacing } !important;
+    line-height: ${ fontScale(props.fontScale)?.lineHeight } !important;
+  `);
+  delete props.fontScale;
+
+  props.className = appendClassName(props.className, newClassName);
+
+  return props;
+};
 
 export function Tag({
-  disabled,
-  round,
-  variant = 'secondary',
-  onClick,
   ...props
 }) {
-  return <Box
-    is='span'
-    rcx-tag
-    rcx-tag--warning={variant === 'warning'}
-    rcx-tag--secondary={variant === 'secondary'}
-    rcx-tag--primary={variant === 'primary'}
-    rcx-tag--danger={variant === 'danger'}
-    rcx-tag--ghost={variant === 'ghost'}
-    rcx-tag--disabled={!!disabled}
-    rcx-tag--round={!!round}
-    rcx-tag--clickable={!!onClick}
-    onClick={onClick}
-    {...props}
-  />;
+  props = useTagPropsToClass(props);
+
+  return <span {...props} />;
 }
 
 Tag.propTypes = {
   disabled: PropTypes.bool,
-  round: PropTypes.bool,
   variant: PropTypes.oneOf(['secondary', 'primary', 'danger', 'warning', 'ghost']),
 };
