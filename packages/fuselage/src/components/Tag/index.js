541f76c4bd43d8c6038238e97b6d3a1ae8c12bbf
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-11-05 23:33:56 -0300

    Remove Tag hook

--- packages/fuselage/src/components/Tag/index.js
+++ packages/fuselage/src/components/Tag/index.js
@@ -1,43 +1,20 @@
-import { css } from '@rocket.chat/css-in-js';
+
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { appendClassName } from '../../helpers/appendClassName';
 import { prependClassName } from '../../helpers/prependClassName';
-import { useStyle } from '../../hooks/useStyle';
-import { fontScale } from '../../styleTokens';
-
-const useTagPropsToClass = (props) => {
-  props.className = prependClassName(props.className, 'rcx-tag');
-
-  if (props.disabled) {
-    props.className = prependClassName(props.className, 'rcx-tag--disabled');
-  }
-
-  props.className = props.variant
-    ? prependClassName(props.className, `rcx-tag--${ props.variant }`)
-    : prependClassName(props.className, 'rcx-tag--secondary');
-  delete props.variant;
-
-  const newClassName = useStyle(css`
-    font-size: ${ fontScale(props.fontScale)?.fontSize } !important;
-    font-weight: ${ fontScale(props.fontScale)?.fontWeight } !important;
-    letter-spacing: ${ fontScale(props.fontScale)?.letterSpacing } !important;
-    line-height: ${ fontScale(props.fontScale)?.lineHeight } !important;
-  `);
-  delete props.fontScale;
-
-  props.className = appendClassName(props.className, newClassName);
-
-  return props;
-};
 
 export function Tag({
+  small,
+  medium,
+  className,
+  variant = 'secondary',
+  disabled,
   ...props
 }) {
-  props = useTagPropsToClass(props);
+  const modifiers = [variant, small && 'small', medium && 'medium', disabled && 'disabled'].map((modifier) => `rcx-tag--${ modifier }`).join(' ');
 
-  return <span {...props} />;
+  return <span className={prependClassName(className, `rcx-tag ${ modifiers }`)} {...props} />;
 }
 
 Tag.propTypes = {
