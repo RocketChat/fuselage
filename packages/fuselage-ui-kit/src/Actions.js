11fa6ea747e1ef700598cd6c54e3a8fc9e4511a6
dougfabris <deefabris@gmail.com>
2020-11-09 16:36:26 -0300

    wip

--- packages/fuselage-ui-kit/src/Actions.js
+++ packages/fuselage-ui-kit/src/Actions.js
@@ -1,19 +1,12 @@
-import {
-  Button,
-  Margins,
-  Flex,
-  Box,
-} from '@rocket.chat/fuselage';
-import {
-  BLOCK_CONTEXT,
-} from '@rocket.chat/ui-kit';
+import { Button, Margins, Flex, Box } from '@rocket.chat/fuselage';
+import { BLOCK_CONTEXT } from '@rocket.chat/ui-kit';
 import React, { useState } from 'react';
 
 import { Block } from './Block';
 
 const getStyle = ({ type }) => {
   switch (type) {
-    case 'button':// ELEMENT_TYPES.BUTTON :
+    case 'button': // ELEMENT_TYPES.BUTTON :
       return 'auto';
     default:
       return '45%';
@@ -22,12 +15,22 @@ const getStyle = ({ type }) => {
 
 export const Actions = ({ blockId, appId, elements, parser }) => {
   const [showMoreVisible, setShowMoreVisible] = useState(
-    () => elements.length > 5,
+    () => elements.length > 5
   );
   const renderedElements = (showMoreVisible
     ? elements.slice(0, 5)
     : elements
-  ).map((element, i) => <Margins key={i} all='x8'><Flex.Item basis={getStyle(element)}>{parser.renderActions({ blockId, appId, ...element }, BLOCK_CONTEXT.ACTION, parser)}</Flex.Item></Margins>);
+  ).map((element, i) => (
+    <Margins key={i} all='x8'>
+      <Flex.Item basis={getStyle(element)}>
+        {parser.renderActions(
+          { blockId, appId, ...element },
+          BLOCK_CONTEXT.ACTION,
+          parser
+        )}
+      </Flex.Item>
+    </Margins>
+  ));
 
   const handleShowMoreClick = () => {
     setShowMoreVisible(false);
@@ -37,7 +40,9 @@ export const Actions = ({ blockId, appId, elements, parser }) => {
     <Block>
       <Box display='flex' flexWrap='wrap' m='neg-x8'>
         {renderedElements}
-        {showMoreVisible && (<Button onClick={handleShowMoreClick}>Show more...</Button>)}
+        {showMoreVisible && (
+          <Button onClick={handleShowMoreClick}>Show more...</Button>
+        )}
       </Box>
     </Block>
   );
