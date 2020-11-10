fea9f7677acdc23a4b590bddb96823b020b97e91
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2020-11-09 22:59:26 -0300

    feat: Remove Box dependence in Avatar component

--- packages/fuselage/src/components/Avatar/index.js
+++ packages/fuselage/src/components/Avatar/index.js
@@ -3,6 +3,7 @@ import React, { useContext, createContext } from 'react';
 import flattenChildren from 'react-keyed-flatten-children';
 
 import { createPropType } from '../../helpers/createPropType';
+import { prependClassName } from '../../helpers/prependClassName';
 import { size } from '../../styleTokens';
 import { Box } from '../Box';
 
@@ -10,34 +11,40 @@ const AvatarContext = createContext({
   baseUrl: '',
 });
 
+const usePropsToStyle = (props) => {
+  props.style = {
+    width: props.size ? size(props.size) : size('x36'),
+    height: props.size ? size(props.size) : size('x36'),
+  };
+
+  return props;
+};
+
 export function Avatar({
   title,
   rounded = false,
   url,
-  size = 'x36',
+  className,
   ...props
 }) {
   const { baseUrl } = useContext(AvatarContext);
+  props = usePropsToStyle(props);
+  className = prependClassName(props.className, 'rcx-box rcx-box--full rcx-avatar');
+  rounded = rounded ? 'rcx-avatar__element--rounded' : '';
 
-  return <Box
-    is='figure'
-    rcx-avatar
-    aria-label={title}
-    {...props}
-    width={size}
-    height={size}
-    verticalAlign='middle'
-  >
-    <Box
-      is='img'
+  return (
+    <figure
+      className={className}
+      aria-label={title}
       {...props}
-      rcx-avatar__element
-      rcx-avatar__element--rounded={rounded}
-      src={`${ baseUrl }${ url }`}
-      width={size}
-      height={size}
-    />
-  </Box>;
+    >
+      <img
+        className={`${ className }__element ${ rounded }`}
+        src={`${ baseUrl }${ url }`}
+        {...props}
+      />
+    </figure>
+  );
 }
 
 Avatar.Context = AvatarContext;
