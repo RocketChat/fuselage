ad8942a17cca2aa553a999b453157e1af55d5cc2
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-11-17 00:08:31 -0300

    Fix Review

--- packages/fuselage/src/components/Avatar/index.js
+++ packages/fuselage/src/components/Avatar/index.js
@@ -1,66 +1,60 @@
-import { css } from '@rocket.chat/css-in-js';
 import PropTypes from 'prop-types';
-import React, { useContext, createContext } from 'react';
+import React from 'react';
 import flattenChildren from 'react-keyed-flatten-children';
 
-import { appendClassName } from '../../helpers/appendClassName';
-import { createPropType } from '../../helpers/createPropType';
 import { prependClassName } from '../../helpers/prependClassName';
-import { useStyle } from '../../hooks/useStyle';
-import { size } from '../../styleTokens';
-import { Box } from '../Box';
 
-const AvatarContext = createContext({
-  baseUrl: '',
-});
-
-const usePropsToClass = (props) => {
-  const style = useStyle(css`
-    width: ${props.size ? size(props.size) : size('x36')};
-    height: ${props.size ? size(props.size) : size('x36')};
-  `);
-
-  props.className = appendClassName(props.className, style);
-};
-
-const usePropsToImgClass = (props) => {
-  return props.className.replace('rcx-avatar', 'rcx-avatar__element');
-};
-
-export function Avatar({ title, rounded = false, url, ...props }) {
-  const { baseUrl } = useContext(AvatarContext);
-
+export function Avatar({
+  title,
+  size = 'x36',
+  rounded = false,
+  url,
+  ...props
+}) {
   props.className = prependClassName(
     props.className,
-    'rcx-box rcx-box--full rcx-avatar'
+    ['rcx-box rcx-box--full rcx-avatar', size && `rcx-avatar--${size}`]
+      .filter(Boolean)
+      .join(' ')
   );
-
-  usePropsToClass(props);
-
-  const imgClass = usePropsToImgClass(props);
-
-  rounded = rounded ? 'rcx-avatar__element--rounded' : '';
+  const innerClass = [
+    'rcx-avatar__element',
+    rounded && 'rcx-avatar__element--rounded',
+    size && `rcx-avatar--${size}`,
+  ]
+    .filter(Boolean)
+    .join(' ');
 
   return (
     <figure aria-label={title} {...props}>
-      <img src={`${baseUrl}${url}`} className={`${imgClass} ${rounded}`} />
+      <img src={`${url}`} className={`${innerClass}`} />
     </figure>
   );
 }
 
-Avatar.Context = AvatarContext;
-
 Avatar.propTypes = {
   rounded: PropTypes.bool,
-  size: createPropType(size),
+  size: PropTypes.oneOf([
+    'x16',
+    'x18',
+    'x20',
+    'x24',
+    'x28',
+    'x32',
+    'x36',
+    'x48',
+    'x124',
+    'x200',
+    'x332',
+  ]),
   title: PropTypes.string,
   url: PropTypes.string.isRequired,
 };
 
-const AvatarStack = ({ children, ...props }) => (
-  <Box rcx-avatar-stack {...props}>
+const AvatarStack = ({ children, className, ...props }) => (
+  <div className={prependClassName('rcx-avatar-stack', className)} {...props}>
     {flattenChildren(children).reverse()}
-  </Box>
+  </div>
 );
 
 Avatar.Stack = AvatarStack;
