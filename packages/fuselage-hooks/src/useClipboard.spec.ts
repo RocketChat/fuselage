81e257153be3a43e846bfa6ad8467eb812d8f150
dougfabris <deefabris@gmail.com>
2020-12-01 18:56:57 -0300

    wip

--- packages/fuselage-hooks/src/useClipboard.spec.ts
+++ packages/fuselage-hooks/src/useClipboard.spec.ts
@@ -1,33 +1,97 @@
-import { createElement, FunctionComponent, StrictMode } from 'react';
+import { createElement, useEffect, FunctionComponent, StrictMode } from 'react';
 import { render } from 'react-dom';
-import { act, isElement } from 'react-dom/test-utils';
+import { act, Simulate } from 'react-dom/test-utils';
 
 import { useClipboard } from './useClipboard';
 
 describe('useClipboard hook', () => {
-  it('check if TestComponent is an Element', () => {
-    const TestComponent: FunctionComponent = () => {
-      return null;
-    };
+  it('check hook properties', async () => {
+    Object.assign(navigator, {
+      clipboard: {
+        writeText: () => {
+          return new Promise((resolve) => {
+            return resolve();
+          });
+        },
+      },
+    });
 
-    isElement(TestComponent);
-  });
+    const onCopySuccess = jest.fn(() => {
+      console.log('Gabriel');
+      expect.anything();
+    });
+    const onCopyError = jest.fn(() => {
+      expect.anything();
+    });
 
-  it('check hook properties', () => {
     let hookObject: { copy: () => void; hasCopied: boolean };
+    let myRef;
+    let resolveCopied;
+    const hasTestCopied = new Promise((resolve) => {
+      resolveCopied = resolve;
+    });
+
     const TestComponent: FunctionComponent = () => {
-      hookObject = useClipboard('Lorem Ipsum');
-      return null;
+      hookObject = useClipboard('Lorem Ipsum Indolor Dolor', {
+        onCopySuccess,
+        onCopyError,
+      });
+
+      act(() => {
+        useEffect(() => {
+          if (hookObject.hasCopied) {
+            resolveCopied(true);
+          }
+        });
+      });
+
+      return createElement('div', {
+        onClick: hookObject.copy,
+        ref: (node) => {
+          myRef = node;
+        },
+      });
     };
 
+    let element;
     act(() => {
+      element = createElement(TestComponent);
       render(
-        createElement(StrictMode, {}, createElement(TestComponent)),
+        createElement(StrictMode, {}, element),
         document.createElement('div')
       );
     });
 
     expect(hookObject).toHaveProperty('copy');
     expect(hookObject).toHaveProperty('hasCopied');
+
+    act(() => {
+      Simulate.click(myRef);
+    });
+
+    // expect(hookObject.copy).toHaveBeenCalledTimes(1);
+    // expect(onCopySuccess).toHaveBeenCalledTimes(1);
+    expect(await hasTestCopied).toBe(true);
+    expect.assertions(5);
+
+    // expect(onCopySuccess).toBeCalledWith(
+    //   expect.objectContaining({ type: 'click' })
+    // );
+
+    Object.assign(navigator, {
+      clipboard: {
+        writeText: () => {
+          return new Promise((reject) => {
+            return reject('error');
+          });
+        },
+      },
+    });
+
+    act(() => {
+      Simulate.click(myRef);
+    });
+
+    expect(onCopyError).toHaveBeenCalledTimes(1);
   });
 });
