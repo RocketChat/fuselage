accd0213a056133a4d6434b632a08554f0ea2ad7
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-12-12 01:24:49 -0300

    Adjust some Storybook files

--- packages/fuselage/.storybook/helpers.js
+++ packages/fuselage/.storybook/helpers.js
@@ -2,28 +2,53 @@ import React from 'react';
 
 import { Box } from '../src';
 
-export function PropsVariationSection({ component: Component, common = {}, xAxis = {}, yAxis = {} }) {
-  return <Box is='table' marginBlock='x16' marginInline='auto' style={{ borderCollapse: 'collapse' }}>
-    <Box is='thead'>
-      <Box is='tr'>
-        <Box is='th' />
-        {Object.keys(xAxis).map((xVariation, key) =>
-          <Box key={key} is='th' color='hint' fontScale='c1'>{xVariation}</Box>)}
+export function PropsVariationSection({
+  component: Component,
+  common = {},
+  xAxis = {},
+  yAxis = {},
+}) {
+  return (
+    <Box
+      is='table'
+      marginBlock='x16'
+      marginInline='auto'
+      style={{ borderCollapse: 'collapse' }}
+    >
+      <Box is='thead'>
+        <Box is='tr'>
+          <Box is='th' />
+          {Object.keys(xAxis).map((xVariation, key) => (
+            <Box key={key} is='th' color='hint' fontScale='c1'>
+              {xVariation}
+            </Box>
+          ))}
+        </Box>
+      </Box>
+      <Box is='tbody'>
+        {Object.entries(yAxis).map(([yVariation, yProps], y) => (
+          <Box key={y} is='tr'>
+            <Box is='th' color='hint' fontScale='c1'>
+              {yVariation}
+            </Box>
+            {Object.values(xAxis).map((xProps, x) => (
+              <Box
+                key={x}
+                is='td'
+                margin='none'
+                paddingBlock='x8'
+                paddingInline='x16'
+              >
+                <Box display='flex' alignItems='center' justifyContent='center'>
+                  <Component {...common} {...xProps} {...yProps} />
+                </Box>
+              </Box>
+            ))}
+          </Box>
+        ))}
       </Box>
     </Box>
-    <Box is='tbody'>
-      {Object.entries(yAxis).map(([yVariation, yProps], y) => (
-        <Box key={y} is='tr'>
-          <Box is='th' color='hint' fontScale='c1'>{yVariation}</Box>
-          {Object.values(xAxis).map((xProps, x) => <Box key={x} is='td' margin='none' paddingBlock='x8' paddingInline='x16'>
-            <Box display='flex' alignItems='center' justifyContent='center'>
-              <Component {...common} {...xProps} {...yProps} />
-            </Box>
-          </Box>)}
-        </Box>
-      ))}
-    </Box>
-  </Box>;
+  );
 }
 
 export const exampleAvatar = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAA
