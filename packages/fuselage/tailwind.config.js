75b9e00a7124557f711bab6bc28085410e882b9d
Ram <irraju@gmail.com>
2021-01-25 00:25:14 -0600

    tailwind integration

--- packages/fuselage/tailwind.config.js
+++ packages/fuselage/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
-    content: ['app/**/*.html'],
+    content: ['app/**/*.html', 'src/**/*.js'],
   },
   theme: {
     extend: {
