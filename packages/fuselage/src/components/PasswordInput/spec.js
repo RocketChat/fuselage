b52fdf2aaf3d5978cdaf39c9ff39bf6acf261fed
Adithya Vardhan <64399555+im-adithya@users.noreply.github.com>
2021-01-26 22:59:36 +0530

    Update spec.js

--- packages/fuselage/src/components/PasswordInput/spec.js
+++ packages/fuselage/src/components/PasswordInput/spec.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { PasswordInput } from '.';
+import { PasswordInput, PasswordInputToggle } from '.';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<PasswordInput />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<PasswordInputToggle />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
