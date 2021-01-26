e30e7236dd1a1adc334ff1b9375e71f2b581abe7
Adithya Vardhan <64399555+im-adithya@users.noreply.github.com>
2021-01-26 22:58:47 +0530

    Update index.js

--- packages/fuselage/src/components/PasswordInput/index.js
+++ packages/fuselage/src/components/PasswordInput/index.js
@@ -1,7 +1,23 @@
 import React, { forwardRef } from 'react';
 
 import { InputBox } from '../InputBox';
+import { Icon } from '../Icon';
 
 export const PasswordInput = forwardRef(function PasswordInput(props, ref) {
   return <InputBox type='password' ref={ref} {...props} />;
+});
+
+export const PasswordInputToggle = forwardRef(function PasswordInput(props, ref) {
+  const [hidden, setHidden] = useState(true);
+  const handleToggle = () => {
+    setHidden(!hidden);
+  }
+  return (
+    <InputBox
+      type={hidden ? 'password' : 'text'}
+      addon={<Icon name={hidden ? 'eye' : 'eye-off'} size='x20' onClick={handleToggle} />}
+      ref={ref}
+      {...props}
+    />
+  );
 });
