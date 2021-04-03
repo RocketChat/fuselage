087cf53fb28e4893264822e7b0964dc0710f7c46
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-04-02 21:02:38 -0300

    remove from StatusBullet component prop size

--- packages/fuselage/src/components/StatusBullet/index.js
+++ packages/fuselage/src/components/StatusBullet/index.js
@@ -3,14 +3,11 @@ import React from 'react';
 
 export const StatusBullet = ({
   status = 'loading',
-  size,
   className = '',
   ...props
 }) => (
   <span
-    className={`rcx-box rcx-box--full rcx-status-bullet rcx-status-bullet--${status} ${
-      size === 'small' ? 'rcx-status-bullet--small' : ''
-    } ${className}`}
+    className={`rcx-box rcx-box--full rcx-status-bullet rcx-status-bullet--${status} ${className}`}
     {...props}
   />
 );
