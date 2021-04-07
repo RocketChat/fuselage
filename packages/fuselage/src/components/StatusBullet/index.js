370e7280fe0952e69a3c41a6a463c9cc06d115ee
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-04-07 14:12:21 -0300

    keep small size

--- packages/fuselage/src/components/StatusBullet/index.js
+++ packages/fuselage/src/components/StatusBullet/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 
 export const StatusBullet = ({
   status = 'loading',
+  size,
   className = '',
   ...props
 }) => (
   <span
-    className={`rcx-box rcx-box--full rcx-status-bullet rcx-status-bullet--${status} ${className}`}
+    className={`rcx-box rcx-box--full rcx-status-bullet rcx-status-bullet--${status} ${size && size === 'small' ? 'rcx-status-bullet--small' : ''} ${className}`}
     {...props}
   />
 );
