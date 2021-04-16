0678d498744edb76fef3e993b350d17f0bac37ee
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-04-16 18:40:24 -0300

    Fix linting in smaller packages

--- packages/icons/scripts/icons.js
+++ packages/icons/scripts/icons.js
@@ -1,21 +1,25 @@
-const path = require('path');
-const { promisify } = require('util');
+const { basename, dirname, join } = require('path');
+const { relative } = require('path');
+const { inspect, promisify } = require('util');
 
 const glob = require('glob');
 
 const { fixBrokenSymlink } = require('./files');
-const { logStep } = require('./log');
 
 const getIconDescriptors = async (srcPath) => {
-  const step = logStep('Read icons');
+  const rootPath = join(__dirname, '..');
+  console.log(
+    'read',
+    inspect(relative(rootPath, join(srcPath, '**/*.svg')), { colors: true })
+  );
 
-  const paths = await promisify(glob)(path.join(srcPath, '**/*.svg'));
+  const paths = await promisify(glob)(join(srcPath, '**/*.svg'));
 
   const descriptors = (
     await Promise.all(
       paths.map(async (_path) => {
         const [, name, , type] = /^(.*?)(\.([a-z]+))?$/.exec(
-          path.basename(_path, '.svg')
+          basename(_path, '.svg')
         );
         _path = await fixBrokenSymlink(_path, srcPath);
 
@@ -28,11 +32,9 @@ const getIconDescriptors = async (srcPath) => {
     )
   ).sort(
     (a, b) =>
-      path.dirname(a.path).localeCompare(path.dirname(b.path)) ||
+      dirname(a.path).localeCompare(dirname(b.path)) ||
       a.name.localeCompare(b.name)
   );
-
-  step.resolve();
 
   return descriptors;
 };
