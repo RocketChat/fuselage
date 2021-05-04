932bddb0196d9a41ddc6b9b8a688df985575a37b
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-05-04 11:48:03 -0300

    Add more test cases do strong emphasis

--- packages/message-parser/tests/strongEmphasis.test.ts
+++ packages/message-parser/tests/strongEmphasis.test.ts
@@ -34,6 +34,82 @@ test.each([
       ]),
     ],
   ],
+  ['**', [paragraph([plain('**')])]],
+  ['* *', [paragraph([bold([plain(' ')])])]],
+  ['** *', [paragraph([plain('** *')])]],
+  ['** **', [paragraph([bold([plain(' ')])])]],
+  ['* Hello*', [paragraph([bold([plain(' Hello')])])]],
+  ['*Hello *', [paragraph([bold([plain('Hello ')])])]],
+  [
+    ':custom*emoji*case:',
+    [paragraph([plain(':custom'), bold([plain('emoji')]), plain('case:')])],
+  ],
+  [
+    'text*hello*text',
+    [paragraph([plain('text'), bold([plain('hello')]), plain('text')])],
+  ],
+  ['*hello*text', [paragraph([bold([plain('hello')]), plain('text')])]],
+  ['text*hello*', [paragraph([plain('text'), bold([plain('hello')])])]],
+  ['*Hel lo*', [paragraph([bold([plain('Hel lo')])])]],
+  ['*Hello*', [paragraph([bold([plain('Hello')])])]],
+  ['**Hello*', [paragraph([plain('*'), bold([plain('Hello')])])]],
+  ['*Hello**', [paragraph([bold([plain('Hello')]), plain('*')])]],
+  ['*Hello', [paragraph([plain('*Hello')])]],
+  ['Hello*', [paragraph([plain('Hello*')])]],
+  ['He*llo', [paragraph([plain('He*llo')])]],
+  [
+    '***Hello***',
+    [paragraph([plain('*'), bold([plain('Hello')]), plain('*')])],
+  ],
+  ['***Hello**', [paragraph([plain('*'), bold([plain('Hello')])])]],
+  [
+    '*Hello* this is dog',
+    [paragraph([bold([plain('Hello')]), plain(' this is dog')])],
+  ],
+  [
+    'Rocket cat says *Hello*',
+    [paragraph([plain('Rocket cat says '), bold([plain('Hello')])])],
+  ],
+  [
+    'He said *Hello* to her',
+    [paragraph([plain('He said '), bold([plain('Hello')]), plain(' to her')])],
+  ],
+  [
+    '**Hello** this is dog',
+    [paragraph([bold([plain('Hello')]), plain(' this is dog')])],
+  ],
+  [
+    'Rocket cat says **Hello**',
+    [paragraph([plain('Rocket cat says '), bold([plain('Hello')])])],
+  ],
+  [
+    'He said **Hello** to her',
+    [paragraph([plain('He said '), bold([plain('Hello')]), plain(' to her')])],
+  ],
+  [
+    'He was a**nn**oyed',
+    [paragraph([plain('He was a'), bold([plain('nn')]), plain('oyed')])],
+  ],
+  [
+    'There are two o in f*oo*tball',
+    [
+      paragraph([
+        plain('There are two o in f'),
+        bold([plain('oo')]),
+        plain('tball'),
+      ]),
+    ],
+  ],
+  [
+    '*__~bolditalicstrike~_*',
+    [
+      paragraph([
+        plain('*__'),
+        strike([plain('bolditalicstrike')]),
+        plain('_*'),
+      ]),
+    ],
+  ],
 ])('parses %p', (input, output) => {
   expect(parser(input)).toMatchObject(output);
 });
