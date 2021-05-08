a2ae266733a2fe8c94c081cb948d0faacdca21f0
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-05-08 01:36:52 -0300

    Fix and upgrade Storybook

--- packages/fuselage-ui-kit/babel.config.js
+++ packages/fuselage-ui-kit/babel.config.js
@@ -22,5 +22,10 @@ module.exports = (api) => ({
       },
     ],
   ],
-  plugins: ['@babel/plugin-transform-runtime'],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
 });
