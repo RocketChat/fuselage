5fb902cdc4517055daa37df33c14862fa0276c59
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-06-02 01:18:34 -0300

    include in RadioButton component d.ts file

---
+++ packages/fuselage/src/components/RadioButton/index.d.ts
@@ -0,0 +1,6 @@
+import { ComponentProps, ForwardRefExoticComponent } from 'react';
+
+import { Box } from '../Box';
+
+type RadioButtonProps = ComponentProps<typeof Box>;
+export const RadioButton: ForwardRefExoticComponent<RadioButtonProps>;
