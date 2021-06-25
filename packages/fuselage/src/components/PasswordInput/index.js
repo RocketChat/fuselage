762cef15d606b54417d6ff3d81969216e3734f45
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-06-25 10:19:58 -0300

    Invert toggle state icon

--- packages/fuselage/src/components/PasswordInput/index.js
+++ packages/fuselage/src/components/PasswordInput/index.js
@@ -1,21 +1,25 @@
-import React, { forwardRef, useState } from 'react';
+import { useToggle } from '@rocket.chat/fuselage-hooks';
+import React, { forwardRef } from 'react';
 
 import { Icon } from '../Icon';
 import { InputBox } from '../InputBox';
 
+// TODO: fix a11y issues
+
 export const PasswordInput = forwardRef(function PasswordInput(props, ref) {
-  const [hidden, setHidden] = useState(true);
-  const handleToggle = () => {
-    setHidden(!hidden);
+  const [hidden, toggle] = useToggle(true);
+  const handleAddonClick = () => {
+    toggle();
   };
+
   return (
     <InputBox
       type={hidden ? 'password' : 'text'}
       addon={
         <Icon
-          name={hidden ? 'eye' : 'eye-off'}
-          size='x20'
-          onClick={handleToggle}
+          name={hidden ? 'eye-off' : 'eye'}
+          size={20}
+          onClick={handleAddonClick}
         />
       }
       ref={ref}
