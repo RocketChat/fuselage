a6ce6aa63d54db25a9cc7293159e930579f464a6
Douglas Fabris <devfabris@gmail.com>
2021-07-07 01:37:42 -0300

    new: AwaitingConfirmationPage Component (#491)
    * new: awaiting confirmation page
    
    * fix: loki
    
    * Add args to story
    
    * Stretch BackgroundLayer beyond 100vh
    
    * Isolate translation strings
    
    * Handle i18n on jest tests
    
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

---
+++ packages/onboarding-ui/.jest/setup.ts
@@ -0,0 +1,14 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+beforeAll(async () => {
+  return i18next.use(initReactI18next).init({
+    fallbackLng: 'en',
+    debug: false,
+    resources: {
+      en: {
+        translation: await import('../.i18n/en.i18n.json'),
+      },
+    },
+  });
+});
