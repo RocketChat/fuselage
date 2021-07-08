0ebe6cc6ec796efd2b6a6d57c2927ac5355a40a8
dougfabris <devfabris@gmail.com>
2021-07-08 15:24:33 -0300

    fix: wrong export

--- packages/onboarding-ui/src/forms/AdminInfoForm/AdminInfoForm.spec.tsx
+++ packages/onboarding-ui/src/forms/AdminInfoForm/AdminInfoForm.spec.tsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom';
 
 import AdminInfoForm from './AdminInfoForm';
-import { onSubmit } from './AdminInfoForm.stories';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AdminInfoForm onSubmit={onSubmit} />, div);
+  ReactDOM.render(<AdminInfoForm onSubmit={() => undefined} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
