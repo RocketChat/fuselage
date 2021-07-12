57f3f68e34c9f75b63aea36d4323ac91d986ddd2
PriyaBihani <priyabihani.official@gmail.com>
2021-07-12 17:42:38 +0530

    inde.tsx refactor

--- packages/image-manipulation/src/index.tsx
+++ packages/image-manipulation/src/index.tsx
@@ -1,6 +1,5 @@
+import React, { useRef, useState, ComponentProps } from 'react';
 import { Box, ButtonGroup } from '@rocket.chat/fuselage';
-import React, { useRef, ComponentProps } from 'react';
-
 import { Crop, Text, Doodle, Check, Cross } from './components';
 import { Preview } from './components/Preview';
 
@@ -9,7 +8,9 @@ type ImageManipulationProps = ComponentProps<typeof Box> & {
 };
 
 const ImageManipulation = ({ imgSrc, ...props }: ImageManipulationProps) => {
-  const imageEle = useRef<typeof Box>(null);
+  const parentRef = useRef<typeof Box & HTMLDivElement>(null);
+  const imageRef = useRef<typeof Box & HTMLImageElement>(null);
+  const [selection, setSelection] = useState<null | string>();
 
   return (
     <Box
@@ -17,17 +18,34 @@ const ImageManipulation = ({ imgSrc, ...props }: ImageManipulationProps) => {
       display='flex'
       justifyContent='space-evenly'
       alignItems='center'
+      border='2px solid black'
+      ref={parentRef}
     >
-      <Preview imgSrc={imgSrc} ref={imageEle} maxWidth='90%' maxHeight='90%' />
-      <ButtonGroup alignSelf='flex-start' margin='5px'>
-        <Crop />
-        <Doodle />
-        <Text />
-      </ButtonGroup>
-      <ButtonGroup alignSelf='flex-start'>
-        <Check />
-        <Cross />
-      </ButtonGroup>
+      <Preview
+        imgSrc={imgSrc}
+        ref={imageRef}
+        display={
+          selection === null || typeof selection === 'undefined'
+            ? 'block'
+            : 'none'
+        }
+      />
+
+      {selection === null ||
+        (typeof selection === 'undefined' && (
+          <ButtonGroup alignSelf='flex-start' margin='5px'>
+            <Crop onClick={() => setSelection('crop')} />
+            <Doodle />
+            <Text />
+          </ButtonGroup>
+        ))}
+
+      {selection !== null && typeof selection !== 'undefined' && (
+        <ButtonGroup alignSelf='flex-start' margin='5px'>
+          <Check />
+          <Cross />
+        </ButtonGroup>
+      )}
     </Box>
   );
 };
