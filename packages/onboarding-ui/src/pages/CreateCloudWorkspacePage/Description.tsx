087f77dea132afbbb126f498a32fd40f3ea8f364
Gabriel Henriques <gabriel.henriques@rocket.chat>
2021-07-14 17:50:47 -0300

    New icon

--- packages/onboarding-ui/src/pages/CreateCloudWorkspacePage/Description.tsx
+++ packages/onboarding-ui/src/pages/CreateCloudWorkspacePage/Description.tsx
@@ -1,28 +1,30 @@
 import { Box } from '@rocket.chat/fuselage';
-import type { ReactElement } from 'react';
+import { ReactElement, useMemo } from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
 import { useTranslation } from 'react-i18next';
 
 import List from '../../common/List';
+import DescriptionIcon from './DescriptionIcon';
 
 const Description = (): ReactElement => {
   const { t } = useTranslation();
+
+  const icon = useMemo(
+    () => encodeURIComponent(renderToStaticMarkup(<DescriptionIcon />)),
+    []
+  );
+
   return (
     <div>
       <Box mbe='x40'>
-        <List color='alternative' spacing='x16'>
-          <List.Li icon='check'>
-            {t('page.cloudDescription.availability')}
-          </List.Li>
-          <List.Li icon='check'>{t('page.cloudDescription.auditing')}</List.Li>
-          <List.Li icon='check'>
-            {t('page.cloudDescription.integrations')}
-          </List.Li>
-          <List.Li icon='check'>{t('page.cloudDescription.ldap')}</List.Li>
-          <List.Li icon='check'>
-            {t('page.cloudDescription.omnichannel')}
-          </List.Li>
-          <List.Li icon='check'>{t('page.cloudDescription.sla')}</List.Li>
-          <List.Li icon='check'>{t('page.cloudDescription.push')}</List.Li>
+        <List color='alternative' spacing='x16' icon={icon}>
+          <List.Li>{t('page.cloudDescription.availability')}</List.Li>
+          <List.Li>{t('page.cloudDescription.auditing')}</List.Li>
+          <List.Li>{t('page.cloudDescription.integrations')}</List.Li>
+          <List.Li>{t('page.cloudDescription.ldap')}</List.Li>
+          <List.Li>{t('page.cloudDescription.omnichannel')}</List.Li>
+          <List.Li>{t('page.cloudDescription.sla')}</List.Li>
+          <List.Li>{t('page.cloudDescription.push')}</List.Li>
         </List>
       </Box>
       <Box fontScale='micro' color='info'>
