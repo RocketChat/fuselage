fad1fc8ea181da5e7ae0d84db5091b0352686463
Gabriel Henriques <gabriel.henriques@rocket.chat>
2021-08-02 18:11:06 -0300

    Fix emailInput types

--- packages/fuselage/src/components/EmailInput/index.d.ts
+++ packages/fuselage/src/components/EmailInput/index.d.ts
@@ -2,5 +2,8 @@ import { ComponentProps, ForwardRefExoticComponent } from 'react';
 
 import { Box } from '../Box';
 
-type EmailInputProps = ComponentProps<typeof Box>;
+type EmailInputProps = ComponentProps<typeof Box> & {
+  addon?: ReactNode;
+  error?: string;
+};
 export const EmailInput: ForwardRefExoticComponent<EmailInputProps>;
