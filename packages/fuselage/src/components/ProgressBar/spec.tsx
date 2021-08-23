2351b5fa6651e46907c824c73f641032b947da41
gabriellsh <40830821+gabriellsh@users.noreply.github.com>
2021-08-23 18:15:37 -0300

    improve(fuselage): Allow custom color on Progress Bar (#520)
    * allow to change color
    
    * optional animation
    
    * Change helper name
    
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

--- packages/fuselage/src/components/ProgressBar/spec.tsx
+++ packages/fuselage/src/components/ProgressBar/spec.tsx
@@ -5,6 +5,6 @@ import { ProgressBar } from '../..';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ProgressBar />, div);
+  ReactDOM.render(<ProgressBar percentage={0} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
