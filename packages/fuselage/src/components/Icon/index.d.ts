b80f800f669b24b6cfce975866b7d7448f4db73a
Gabriel Henriques <gabriel.henriques@rocket.chat>
2021-09-03 16:00:57 -0300

    Fix missing Icon prop type

--- packages/fuselage/src/components/Icon/index.d.ts
+++ packages/fuselage/src/components/Icon/index.d.ts
@@ -4,5 +4,6 @@ import { Box } from '../Box';
 
 type IconProps = Omit<ComponentProps<typeof Box>, 'size'> & {
   size?: ComponentProps<typeof Box>['width'];
+  name: string;
 };
 export const Icon: ForwardRefExoticComponent<IconProps>;
