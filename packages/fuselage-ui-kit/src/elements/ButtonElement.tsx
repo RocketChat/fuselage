2fa0a3ec1f481f6c01b8b910376ee48a72aca484
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-09-16 17:51:52 -0300

    Refactor ButtonElement

--- packages/fuselage-ui-kit/src/elements/ButtonElement.tsx
+++ packages/fuselage-ui-kit/src/elements/ButtonElement.tsx
@@ -1,34 +1,59 @@
 import { Button, Throbber } from '@rocket.chat/fuselage';
-import {
-  BlockContext,
-  ButtonElement as ButtonElementProps,
-} from '@rocket.chat/ui-kit';
-import { BaseSurfaceRenderer } from '@rocket.chat/ui-kit/dist/esm/rendering/BaseSurfaceRenderer';
-import React, { FC } from 'react';
+import * as UiKit from '@rocket.chat/ui-kit';
+import React, { ReactElement } from 'react';
 
 import { useUiKitState } from '../hooks/useUiKitState';
 
-const ButtonElement: FC<{
-  element: ButtonElementProps;
-  context?: BlockContext;
-  parser: BaseSurfaceRenderer<FC>;
-}> = ({ element, context, parser }) => {
+type ButtonElementProps = {
+  element: UiKit.ButtonElement;
+  context: UiKit.BlockContext;
+  parser: UiKit.SurfaceRenderer<ReactElement>;
+};
+
+const ButtonElement = ({
+  element,
+  context,
+  parser,
+}: ButtonElementProps): ReactElement => {
   const [{ loading }, action] = useUiKitState(element, context);
+
+  if (element.url) {
+    return (
+      <Button
+        is='a'
+        target='_blank'
+        href={element.url}
+        disabled={loading}
+        primary={element.style === 'primary'}
+        danger={element.style === 'danger'}
+        minWidth='4ch'
+        small
+        onClick={action}
+      >
+        {loading ? (
+          <Throbber />
+        ) : (
+          parser.renderTextObject(element.text, 0, context)
+        )}
+      </Button>
+    );
+  }
+
   return (
     <Button
-      is={element.url ? 'a' : undefined}
-      target={element.url ? '_blank' : undefined}
-      href={element.url}
       disabled={loading}
       primary={element.style === 'primary'}
       danger={element.style === 'danger'}
       minWidth='4ch'
       small
       value={element.value}
-      // data-group={element.groupId}
       onClick={action}
     >
-      {loading ? <Throbber /> : parser.plainText(element.text, context)}
+      {loading ? (
+        <Throbber />
+      ) : (
+        parser.renderTextObject(element.text, 0, context)
+      )}
     </Button>
   );
 };
