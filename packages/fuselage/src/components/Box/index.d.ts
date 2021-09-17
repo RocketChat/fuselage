2a0ed22f46eedf8e10839b703645af1f202eb398
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-09-17 20:24:52 -0300

    Add typings for Flex and Grid

--- packages/fuselage/src/components/Box/index.d.ts
+++ packages/fuselage/src/components/Box/index.d.ts
@@ -149,5 +149,6 @@ type BoxProps = PropsWithChildren<{
 export const Box: ForwardRefExoticComponent<BoxProps>;
 
 export { default as AnimatedVisibility } from './AnimatedVisibility';
+export { default as Flex } from './Flex';
 export { default as Position, PositionAnimated } from './Position';
 export { default as Scrollable } from './Scrollable';
