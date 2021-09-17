942c7c7155a456f0720b2745787888f25e8c061d
dougfabris <devfabris@gmail.com>
2021-09-17 11:17:05 -0300

    fix: jest config

--- packages/logo/jest.config.js
+++ packages/logo/jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   errorOnDeprecated: true,
-  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts?(x)'],
+  testEnvironment: 'jsdom',
   globals: {
     'ts-jest': {
       tsconfig: {
