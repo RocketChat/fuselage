bc0cbce69589b9a056d797a03b78d7cd06423aaa
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-09-18 01:32:56 -0300

    fix(message-parser): Fix Url and Escaped Markdown (#537)

--- packages/message-parser/tests/url.test.ts
+++ packages/message-parser/tests/url.test.ts
@@ -3,6 +3,20 @@ import { link, paragraph, plain } from '../src/utils';
 
 test.each([
   [
+    'https://pt.wikipedia.org/wiki/Condi%C3%A7%C3%A3o_de_corrida#:~:text=Uma%20condi%C3%A7%C3%A3o%20de%20corrida%20%C3%A9,sequ%C3%AAncia%20ou%20sincronia%20doutros%20eventos',
+    [
+      paragraph([
+        link(
+          'https://pt.wikipedia.org/wiki/Condi%C3%A7%C3%A3o_de_corrida#:~:text=Uma%20condi%C3%A7%C3%A3o%20de%20corrida%20%C3%A9,sequ%C3%AAncia%20ou%20sincronia%20doutros%20eventos'
+        ),
+      ]),
+    ],
+  ],
+  [
+    'https://pt.wikipedia.org/',
+    [paragraph([link('https://pt.wikipedia.org/')])],
+  ],
+  [
     'https://www.npmjs.com/package/@rocket.chat/message-parser',
     [
       paragraph([
@@ -38,10 +52,12 @@ test.each([
   ],
   ['https://localhost:3000#', [paragraph([link('https://localhost:3000#')])]],
   ['https://localhost:3000?', [paragraph([link('https://localhost:3000?')])]],
+  ['https://localhost:3000/', [paragraph([link('https://localhost:3000/')])]],
   [
     'ftp://user:pass@localhost:21/etc/hosts',
     [paragraph([link('ftp://user:pass@localhost:21/etc/hosts')])],
   ],
+  ['ssh://test@test.test', [paragraph([link('ssh://test@test.test')])]],
 ])('parses %p', (input, output) => {
   expect(parser(input)).toMatchObject(output);
 });
