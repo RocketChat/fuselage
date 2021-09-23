02f8206ea66b5d121709346a524fb8e73ec1a5f0
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-09-23 13:16:05 -0300

    Create new app

---
+++ playground/message-parser-playground/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  displayName: "..-playground-message-parser-playground",
+  preset: "../../jest.preset.js",
+  transform: {
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
+    "^.+\\.[tj]sx?$": "babel-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory:
+    "../../coverage/packages/../playground/message-parser-playground",
+};
---
+++ playground/message-parser-playground/src/app/app.tsx
@@ -0,0 +1,8 @@
+import { ReactComponent as Logo } from "./logo.svg";
+import star from "./star.svg";
+
+export function App() {
+  return <h1>Welcome to message-parser-playground!</h1>;
+}
+
+export default App;
