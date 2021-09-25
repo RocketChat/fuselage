6220ca371e3bd9110c94bb64db454de5c8d905d6
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-09-25 19:57:10 -0300

    Add write-version-module

--- packages/fuselage-ui-kit/.bin/update-version.js
+++
@@ -1,8 +0,0 @@
-const { version } = require('../package.json');
-const { writeFileSync } = require('fs');
-const { join } = require('path');
-
-writeFileSync(
-  join(__dirname, '../src/version.ts'),
-  `export default '${version}';\n`
-);
