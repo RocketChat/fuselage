c172cab9a2ee33269cc572660b8df4848a905c9f
ujorgeleite <jorgefernandodamasio@gmail.com>
2021-10-14 08:43:04 -0300

    chore: refactor title

--- packages/onboarding-ui/src/pages/RequestTrialPage/TitleRequestTrial.tsx
+++ packages/onboarding-ui/src/pages/RequestTrialPage/TitleRequestTrial.tsx
@@ -1,19 +1,15 @@
 import colors from '@rocket.chat/fuselage-tokens/colors.json';
 import type { ReactElement } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans } from 'react-i18next';
 
 import { Title } from './RequestTrial.styles';
 
-const TitleRequestTrial = (): ReactElement => {
-  const { t } = useTranslation();
-  const title = t('page.requestTrial.title').split('**');
-  return (
-    <Title fontColor={colors.n900}>
-      <Trans i18nKey="page.requestTrial.title">
-        Request a <Title fontColor={colors.b500}>30-day Trial</Title>
-      </Trans>
-    </Title>
-  );
-};
+const TitleRequestTrial = (): ReactElement => (
+  <Title fontColor={colors.n900}>
+    <Trans i18nKey='page.requestTrial.title'>
+      Request a <Title fontColor={colors.b500}>30-day Trial</Title>
+    </Trans>
+  </Title>
+);
 
 export default TitleRequestTrial;
