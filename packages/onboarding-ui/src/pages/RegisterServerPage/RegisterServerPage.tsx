1cfef1c8400fdcbdd7659a90d72510b9ce12304b
ujorgeleite <jorgefernandodamasio@gmail.com>
2021-10-14 10:57:38 -0300

    feat: add new style brand on register server page

--- packages/onboarding-ui/src/pages/RegisterServerPage/RegisterServerPage.tsx
+++ packages/onboarding-ui/src/pages/RegisterServerPage/RegisterServerPage.tsx
@@ -1,10 +1,13 @@
+import colors from '@rocket.chat/fuselage-tokens/colors.json';
 import type { ReactElement } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
+import { Trans } from 'react-i18next';
 
 import BackgroundLayer from '../../common/BackgroundLayer';
 import FormPageLayout from '../../common/FormPageLayout';
 import RegisterServerForm from '../../forms/RegisterServerForm';
 import type { RegisterServerPayload } from '../../forms/RegisterServerForm/RegisterServerForm';
+import { Title } from './RegisterServer.styles';
 
 type RegisterServerPageProps = {
   currentStep: number;
@@ -14,9 +17,23 @@ type RegisterServerPageProps = {
   onBackButtonClick: () => void;
 };
 
+const titleRegisterServer = () => (
+  <Title fontColor={colors.n900}>
+    <Trans i18nKey='page.form.title'>
+      Let's
+      <Title fontColor={colors.b500}>Launch</Title>
+      Your Workspace
+    </Trans>
+  </Title>
+);
+
 const RegisterServerPage = (props: RegisterServerPageProps): ReactElement => (
   <BackgroundLayer>
-    <FormPageLayout>
+    <FormPageLayout
+      title={titleRegisterServer()}
+      justifyContent='center'
+      paddingEnd='135'
+    >
       <RegisterServerForm {...props} />
     </FormPageLayout>
   </BackgroundLayer>
