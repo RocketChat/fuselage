7c9cd06b463223292cc8fe2f9e21961b2a73a4de
ujorgeleite <jorgefernandodamasio@gmail.com>
2021-10-15 11:37:55 -0300

    feat: add new design

--- packages/onboarding-ui/src/pages/CloudAccountEmailPage/CloudAccountEmailPage.tsx
+++ packages/onboarding-ui/src/pages/CloudAccountEmailPage/CloudAccountEmailPage.tsx
@@ -1,10 +1,23 @@
+import colors from '@rocket.chat/fuselage-tokens/colors.json';
 import type { ReactElement } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
+import { Trans } from 'react-i18next';
 
 import BackgroundLayer from '../../common/BackgroundLayer';
 import FormPageLayout from '../../common/FormPageLayout';
 import CloudAccountEmailForm from '../../forms/CloudAccountEmailForm';
 import type { CloudAccountEmailPayload } from '../../forms/CloudAccountEmailForm/CloudAccountEmailForm';
+import { Title } from './CloudAccountEmailPage.styles';
+
+const titleCloudEmailPages = () => (
+  <Title fontColor={colors.n900}>
+    <Trans i18nKey='page.form.title'>
+      Let's
+      <Title fontColor={colors.b500}>Launch</Title>
+      Your Workspace
+    </Trans>
+  </Title>
+);
 
 type CloudAccountEmailPageProps = {
   currentStep: number;
@@ -20,7 +33,11 @@ const CloudAccountEmailPage = (
   props: CloudAccountEmailPageProps
 ): ReactElement => (
   <BackgroundLayer>
-    <FormPageLayout>
+    <FormPageLayout
+      title={titleCloudEmailPages()}
+      justifyContent='center'
+      paddingEnd='135'
+    >
       <CloudAccountEmailForm {...props} />
     </FormPageLayout>
   </BackgroundLayer>
