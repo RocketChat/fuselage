ea82666be7e204165fb616644cbd0f73a247bd69
Jorge Leite <jorgefernandodamasio@gmail.com>
2021-10-21 15:01:26 -0300

    Update packages/onboarding-ui/src/common/PlanFeatureIcon.tsx
    chore: change to color token name
    
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- packages/onboarding-ui/src/common/PlanFeatureIcon.tsx
+++ packages/onboarding-ui/src/common/PlanFeatureIcon.tsx
@@ -8,7 +8,7 @@ const DescriptionIcon = (): ReactElement => (
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
   >
-    <path d='M1 7L5 11L15 1' stroke='#1F2329' stroke-width='2' />
+    <path d='M1 7L5 11L15 1' stroke='currentColor' stroke-width='2' />
   </svg>
 );
 
