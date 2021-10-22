a1ed3696e7aff1d0a90d1912bdf0240e0f9e1140
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-22 11:05:05 -0300

    Refactor Badge smoke test

--- packages/fuselage/src/components/Badge/Badge.spec.tsx
+++ packages/fuselage/src/components/Badge/Badge.spec.tsx
@@ -1,10 +1,8 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import { Badge } from '.';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Badge />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<Badge />);
 });
