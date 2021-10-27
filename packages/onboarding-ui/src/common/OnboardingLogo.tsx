4fd477dabd219d9217a6687395862c448e430397
ujorgeleite <jorgefernandodamasio@gmail.com>
2021-10-27 10:22:43 -0300

    fix: update logo tagtitle color to white

--- packages/onboarding-ui/src/common/OnboardingLogo.tsx
+++ packages/onboarding-ui/src/common/OnboardingLogo.tsx
@@ -3,7 +3,7 @@ import { RocketChatLogo } from '@rocket.chat/logo';
 import type { ReactElement } from 'react';
 
 export const OnboardingLogo = ({ tag }: { tag?: string }): ReactElement => (
-  <Box width='100%' maxWidth={224}>
+  <Box width='100%' maxWidth={224} color='white'>
     <RocketChatLogo tagTitle={tag} />
   </Box>
 );
