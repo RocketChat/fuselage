a464b1a9480a9ed758eb8b5516afaa9826bec7ad
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-31 13:31:46 -0300

    Refactor NumberInput tests

--- packages/fuselage/src/components/NumberInput/NumberInput.spec.tsx
+++ packages/fuselage/src/components/NumberInput/NumberInput.spec.tsx
@@ -1,10 +1,8 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import { NumberInput } from '.';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NumberInput />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<NumberInput />);
 });
