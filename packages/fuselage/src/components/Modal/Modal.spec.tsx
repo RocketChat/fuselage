cb7adaca14dfc39576edb3a5d3e7cc3e8ed8e5d7
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-31 13:31:07 -0300

    Refactor Modal tests

--- packages/fuselage/src/components/Modal/Modal.spec.tsx
+++ packages/fuselage/src/components/Modal/Modal.spec.tsx
@@ -1,10 +1,8 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { Modal } from '../..';
+import { Modal } from '.';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Modal />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<Modal />);
 });
