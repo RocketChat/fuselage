902178b777ef99cdfce2ceec1cb34302786ef303
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-31 11:22:00 -0300

    Refactor CheckBox tests

--- packages/fuselage/src/components/CheckBox/CheckBox.spec.tsx
+++ packages/fuselage/src/components/CheckBox/CheckBox.spec.tsx
@@ -1,10 +1,8 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { CheckBox } from '../..';
+import { CheckBox } from '.';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CheckBox />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<CheckBox />);
 });
