2c78a580dda93b7f972ae0bd30119a42c72662fa
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-31 11:22:33 -0300

    Refactor Chevron tests

--- packages/fuselage/src/components/Chevron/Chevron.spec.tsx
+++ packages/fuselage/src/components/Chevron/Chevron.spec.tsx
@@ -1,10 +1,8 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { Chevron } from '../..';
+import { Chevron } from '.';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Chevron />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<Chevron />);
 });
--- packages/fuselage/src/components/Divider/Divider.spec.tsx
+++ packages/fuselage/src/components/Divider/Divider.spec.tsx
@@ -1,10 +1,8 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { Divider } from '../..';
+import { Divider } from '.';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Divider />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<Divider />);
 });
