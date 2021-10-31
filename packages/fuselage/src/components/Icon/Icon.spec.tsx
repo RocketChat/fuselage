1c42f1b8b2ccd7612aa4b51e312baa59a6d3fde8
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-31 11:41:41 -0300

    Refactor Icon tests

--- packages/fuselage/src/components/Icon/Icon.spec.tsx
+++ packages/fuselage/src/components/Icon/Icon.spec.tsx
@@ -1,10 +1,8 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { Icon } from '../..';
+import { Icon } from '.';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Icon name='chat' />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<Icon name='chat' />);
 });
