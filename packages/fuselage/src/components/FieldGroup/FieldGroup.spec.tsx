acb36a06813fd8bb5bb98d4dfc07e172944156cf
Tasso Evangelista <tasso.evangelista@rocket.chat>
2021-10-31 11:39:02 -0300

    Refactor FieldGroup tests

--- packages/fuselage/src/components/FieldGroup/FieldGroup.spec.tsx
+++ packages/fuselage/src/components/FieldGroup/FieldGroup.spec.tsx
@@ -1,10 +1,8 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { FieldGroup } from '../..';
+import { FieldGroup } from '.';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<FieldGroup />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<FieldGroup />);
 });
