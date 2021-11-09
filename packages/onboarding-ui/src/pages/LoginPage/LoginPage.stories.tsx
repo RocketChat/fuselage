c29bac4595342b9ab7f937c8e5161ff7fe47296f
ujorgeleite <jorgefernandodamasio@gmail.com>
2021-11-09 16:16:50 -0300

    feat: add a passwordLess storie

--- packages/onboarding-ui/src/pages/LoginPage/LoginPage.stories.tsx
+++ packages/onboarding-ui/src/pages/LoginPage/LoginPage.stories.tsx
@@ -12,7 +12,14 @@ export default {
     actions: { argTypesRegex: '^on.*' },
     layout: 'fullscreen',
   },
+  args: { isPasswordLessFlow: false },
 } as Meta<Args>;
 
 export const _LoginPage: Story<Args> = (args) => <LoginPage {...args} />;
-_LoginPage.storyName = 'LoginPage';
+export const _LoginPasswordLessPage: Story<Args> = (args) => (
+  <LoginPage {...args} />
+);
+_LoginPage.storyName = 'CloudDefaultLoginPage';
+_LoginPasswordLessPage.storyName = 'CloudPasswordLessLoginPage';
+
+_LoginPasswordLessPage.args = { isPasswordLessFlow: true };
