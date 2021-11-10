9e6a9986d14db26b9d5bdadef25453211261bc29
dougfabris <devfabris@gmail.com>
2021-11-10 09:32:48 -0300

    fix: LoginPage test

--- packages/onboarding-ui/src/pages/LoginPage/LoginPage.spec.tsx
+++ packages/onboarding-ui/src/pages/LoginPage/LoginPage.spec.tsx
@@ -6,18 +6,11 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <LoginPage
-      loginPasswordLessProps={{
-        initialValues: undefined,
-        onChangeForm: () => undefined,
-        onSubmit: () => undefined,
-      }}
-      loginDefaultProps={{
-        initialValues: undefined,
-        onSendLoginLinkForm: () => undefined,
-        onResetPassword: () => undefined,
-        onSubmit: () => undefined,
-      }}
-      isPasswordLessFlow={false}
+      initialValues={undefined}
+      onChangeForm={() => undefined}
+      onResetPassword={() => undefined}
+      onSubmit={() => undefined}
+      isPasswordLess={false}
       onCreateAccount={() => undefined}
     />,
     div
