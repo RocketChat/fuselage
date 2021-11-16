ae560de7da5c956774cc1568fcbc189c7be52694
ujorgeleite <jorgefernandodamasio@gmail.com>
2021-11-16 17:05:13 -0300

    refactor: refactor listItem

--- packages/onboarding-ui/src/common/List/ListItem.tsx
+++ packages/onboarding-ui/src/common/List/ListItem.tsx
@@ -1,19 +1,22 @@
 import { Box, Icon } from '@rocket.chat/fuselage';
 import type { ComponentProps, ReactElement, ReactNode } from 'react';
 
-const Title = ({
+type FontScale = 'h1' | 's1' | 's2' | 'p1' | 'p2' | 'c1' | 'c2' | 'micro';
+const ListItem = ({
   children,
   icon,
   iconColor = 'success',
+  fontScale = 'c1',
 }: {
   children: ReactNode;
   icon?: string;
   iconColor?: ComponentProps<typeof Icon>['color'];
+  fontScale?: FontScale;
 }): ReactElement => (
-  <Box is='li' fontScale='c1' color='inherit'>
-    {icon && <Icon name={icon} color={iconColor} size='x12' mie='x4' />}
+  <Box is='li' fontScale={fontScale} color='inherit'>
+    {icon && <Icon name={icon} color={iconColor} size='x16' mie='x4' />}
     {children}
   </Box>
 );
 
-export default Title;
+export default ListItem;
