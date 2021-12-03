65b19841bc3ac22d658d4b08c2508536bd13c711
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-12-03 02:21:26 -0300

    Chore/sidebar (#581)
    Co-authored-by: Martin Schoeler <martin.schoeler@rocket.chat>

--- packages/fuselage/src/components/Sidebar/Section.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Title = (props) => (
-  <div className='rcx-box rcx-box--full rcx-sidebar-title' {...props} />
-);
-
-export default {
-  Title,
-};
---
+++ packages/fuselage/src/components/Sidebar/TopBar.tsx
@@ -0,0 +1,88 @@
+import React, { ComponentProps, FC } from 'react';
+
+import { Box } from '../Box';
+import { Divider } from '../Divider';
+import { SidebarAction, SidebarActions } from './SidebarActions';
+
+const Avatar = { size: 'x24' };
+
+const TopBar: FC<{ className: string }> & {
+  Section: typeof TopBarSection;
+  ToolBox: typeof TopBarToolBox;
+  Avatar: typeof TopBarAvatar;
+  Actions: typeof TopBarActions;
+  Action: typeof TopBarAction;
+  Divider: typeof TopBarDivider;
+  Title: typeof TopBarTitle;
+  Wrapper: typeof TopBarWrapper;
+} = ({ className, ...props }) => (
+  <div
+    className={['rc-box rc-box--full rcx-sidebar-topbar', className]
+      .filter(Boolean)
+      .join(' ')}
+    {...props}
+  />
+);
+
+const TopBarWrapper: FC = ({ children }) => (
+  <div
+    className='rc-box rc-box--full rcx-sidebar-topbar__wrapper'
+    children={children}
+  />
+);
+
+export const TopBarToolBox: FC<{ className: string }> = ({
+  children,
+  className,
+  ...props
+}) => (
+  <TopBar
+    className={['rcx-sidebar-topbar--toolbox', className]
+      .filter(Boolean)
+      .join(' ')}
+    {...props}
+  >
+    <TopBarWrapper children={children} />
+    <TopBarDivider />
+  </TopBar>
+);
+
+export const TopBarSection: FC<{ className: string }> = ({
+  className,
+  children,
+  ...props
+}) => (
+  <TopBar
+    className={['rcx-sidebar-topbar--section', className]
+      .filter(Boolean)
+      .join(' ')}
+    {...props}
+  >
+    <TopBarWrapper children={children} />
+    <TopBarDivider />
+  </TopBar>
+);
+
+export const TopBarAvatar = Avatar;
+export const TopBarActions = SidebarActions;
+export const TopBarAction: FC<ComponentProps<typeof SidebarAction>> = (
+  props
+) => <SidebarAction ghost {...props} />;
+
+export const TopBarDivider: FC = () => <Divider mbs='neg-x2' mbe={0} />;
+
+export const TopBarTitle: FC = (props) => (
+  <Box rcx-sidebar-top-bar__title withTruncatedText {...props} />
+);
+
+TopBar.Section = TopBarSection;
+TopBar.ToolBox = TopBarToolBox;
+TopBar.Wrapper = TopBarWrapper;
+TopBar.Avatar = TopBarAvatar;
+TopBar.Actions = TopBarActions;
+TopBar.Action = TopBarAction;
+TopBar.Divider = TopBarDivider;
+TopBar.Title = TopBarTitle;
+TopBar.Wrapper = TopBarWrapper;
+
+export default TopBar;
