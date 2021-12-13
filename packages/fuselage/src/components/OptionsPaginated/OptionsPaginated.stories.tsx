903af03f0c82bfad571142e25ff3dc32346fb2a6
juliajforesti <juliajforesti@gmail.com>
2021-12-13 19:36:10 -0300

    Convert OptionsPaginated stories.

---
+++ packages/fuselage/src/components/OptionsPaginated/OptionsPaginated.stories.tsx
@@ -0,0 +1,62 @@
+import { Title, Description, Primary, Stories } from '@storybook/addon-docs';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+
+import { OptionsPaginated, CheckOption } from '.';
+import { Box } from '..';
+import Option from '../Options/Option';
+
+const options = Array.from(new Array(90)).map((_, i) => ({
+  value: i + 1,
+  label: `a teste ${i + 1}`,
+  selected: i === 0,
+}));
+
+export default {
+  title: 'Misc/Options/OptionsPaginated_new',
+  component: OptionsPaginated,
+  parameters: {
+    docs: {
+      description: {
+        component: 'An input for selection of options.',
+      },
+      page: () => (
+        <>
+          <Title />
+          <Description />
+          <Primary />
+          <Stories title={''} />
+        </>
+      ),
+    },
+  },
+} as ComponentMeta<typeof OptionsPaginated>;
+
+const Template: ComponentStory<typeof OptionsPaginated> = (args) => (
+  <Box position='relative' maxWidth={250}>
+    <OptionsPaginated {...args} ref={React.createRef()} options={options} />
+  </Box>
+);
+
+export const Default: ComponentStory<typeof OptionsPaginated> = Template.bind(
+  {}
+);
+Default.args = {
+  cursor: 1,
+};
+export const OptionStory: ComponentStory<typeof OptionsPaginated> =
+  Template.bind({});
+OptionStory.args = {
+  cursor: 1,
+  renderItem: Option,
+};
+OptionStory.storyName = 'Option';
+
+export const CheckOptionStory: ComponentStory<typeof OptionsPaginated> =
+  Template.bind({});
+CheckOptionStory.args = {
+  cursor: 0,
+  value: [1],
+  renderItem: CheckOption,
+};
+CheckOptionStory.storyName = 'CheckOption';
