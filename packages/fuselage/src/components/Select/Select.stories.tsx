606101c3c70a4a1cf8b3aaf5ed361af48b7462c7
juliajforesti <juliajforesti@gmail.com>
2021-12-13 20:20:35 -0300

    Convert SearchInput and Select stories.

---
+++ packages/fuselage/src/components/Select/Select.stories.tsx
@@ -0,0 +1,112 @@
+import {
+  Title,
+  Description,
+  Primary,
+  Stories,
+  ArgsTable,
+} from '@storybook/addon-docs';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+
+import { Select, SelectFiltered } from '../..';
+
+const options = Array.from(new Array(10)).map((_, i) => [
+  i + 1,
+  `a teste ${i + 1}`,
+]);
+const optionsEllipses = [
+  [11, 'Very very very very very very very very very large text'],
+  ...options,
+];
+
+export default {
+  title: 'Forms/Select_new',
+  component: Select,
+  parameters: {
+    docs: {
+      description: {
+        component: 'An input for selection of options.',
+      },
+      page: () => (
+        <>
+          <Title />
+          <Description />
+          <Primary />
+          <Stories title={''} />
+          <ArgsTable />
+        </>
+      ),
+    },
+  },
+} as ComponentMeta<typeof Select>;
+
+const Template: ComponentStory<typeof Select> = (args) => <Select {...args} />;
+
+export const Default: ComponentStory<typeof Select> = Template.bind({});
+Default.args = {
+  width: '250px',
+  placeholder: 'Placeholder here...',
+  options,
+};
+
+export const Error: ComponentStory<typeof Select> = Template.bind({});
+Error.args = {
+  width: '250px',
+  error: true,
+  placeholder: 'Placeholder here...',
+  options,
+};
+
+export const Disabled: ComponentStory<typeof Select> = Template.bind({});
+Disabled.args = {
+  width: '250px',
+  disabled: true,
+  placeholder: 'Placeholder here...',
+  options,
+};
+
+export const NoPlaceholder: ComponentStory<typeof Select> = Template.bind({});
+NoPlaceholder.args = {
+  width: '250px',
+  options,
+};
+
+const TemplateWithFilter: ComponentStory<typeof Select> = (args) => (
+  <SelectFiltered {...args} />
+);
+
+export const SelectWithFilter: ComponentStory<typeof Select> =
+  TemplateWithFilter.bind({});
+SelectWithFilter.args = {
+  width: '250px',
+  placeholder: 'Placeholder here...',
+  options,
+};
+
+export const SelectWithFilterAndEllipses: ComponentStory<typeof Select> =
+  TemplateWithFilter.bind({});
+SelectWithFilterAndEllipses.args = {
+  width: '250px',
+  placeholder: 'Placeholder here...',
+  options: optionsEllipses,
+  value: 11,
+};
+
+export const SelectWithFilterAndError: ComponentStory<typeof Select> =
+  TemplateWithFilter.bind({});
+SelectWithFilterAndError.args = {
+  width: '250px',
+  error: true,
+  placeholder: 'Placeholder here...',
+  options,
+};
+
+export const SelectWithFilterAndDisabled: ComponentStory<typeof Select> =
+  TemplateWithFilter.bind({});
+SelectWithFilterAndDisabled.args = {
+  width: '250px',
+  error: true,
+  disabled: true,
+  placeholder: 'Placeholder here...',
+  options,
+};
