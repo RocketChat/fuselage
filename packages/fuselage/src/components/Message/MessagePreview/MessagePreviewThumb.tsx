0233566e433de05bdcb3d18221d60725d0dbadc7
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-12-13 15:37:11 -0300

    loki

--- packages/fuselage/src/components/Message/MessagePreview/MessagePreviewThumb.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC } from 'react';
-
-export const MessagePreviewThumb: FC<React.HTMLAttributes<HTMLDivElement>> = (
-  props
-) => <div className='rcx-message-generic-preview__thumb' {...props} />;
--- packages/fuselage/src/components/Message/index.tsx
+++ packages/fuselage/src/components/Message/index.tsx
@@ -1,7 +1,6 @@
 export * from './Message';
 export * from './MessageDivider';
 export * from './MessageMetrics';
-export * from './MessagePreview';
 export * from './MessageReactions';
 export * from './MessageToolbox';
 export * from './ThreadMessage';
