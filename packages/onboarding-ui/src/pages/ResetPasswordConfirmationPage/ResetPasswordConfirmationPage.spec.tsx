3e36e38cfd785fb5b7bf29683dc2c1a464a75f9c
ujorgeleite <jorgefernandodamasio@gmail.com>
2021-12-15 13:49:27 -0300

    feat: update reset password confirmation

--- packages/onboarding-ui/src/pages/ResetPasswordConfirmationPage/ResetPasswordConfirmationPage.spec.tsx
+++ packages/onboarding-ui/src/pages/ResetPasswordConfirmationPage/ResetPasswordConfirmationPage.spec.tsx
@@ -1,12 +1,9 @@
 import ReactDOM from 'react-dom';
 
-import ConfirmationPage from './ConfirmationPage';
+import ResetPasswordConfirmationPage from './ResetPasswordConfirmationPage';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(
-    <ConfirmationPage title='Title' subTitle='Description' />,
-    div
-  );
+  ReactDOM.render(<ResetPasswordConfirmationPage />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
