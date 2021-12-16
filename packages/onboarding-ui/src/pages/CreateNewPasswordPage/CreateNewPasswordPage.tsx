7ca5dfef85805382be9a5f67847a8111446130de
Jorge Leite <jorgefernandodamasio@gmail.com>
2021-12-16 13:19:38 -0300

    feat(onboarding-ui): add reset password flow (#585)
    * feat: add reset password flow
    
    * chore: update loki screenshots
    
    * chore: fix lint
    
    * feat: update reset password confirmation
    
    * chore: update loki screenshots
    
    * chore: update reset password confirmation export
    
    * fix: review
    
    * chore: update loki screenshots
    
    Co-authored-by: dougfabris <devfabris@gmail.com>

---
+++ packages/onboarding-ui/src/pages/CreateNewPasswordPage/CreateNewPasswordPage.tsx
@@ -0,0 +1,50 @@
+import { Box } from '@rocket.chat/fuselage';
+import type { ReactElement } from 'react';
+import type { SubmitHandler, Validate } from 'react-hook-form';
+import { Trans, useTranslation } from 'react-i18next';
+
+import ActionLink from '../../common/ActionLink';
+import BackgroundLayer from '../../common/BackgroundLayer';
+import FormPageLayout from '../../common/FormPageLayout';
+import type { FormPageLayoutStyleProps } from '../../common/Types';
+import CreateNewPasswordForm from '../../forms/CreateNewPassword';
+import type { CreateNewPasswordPayload } from '../../forms/CreateNewPassword/CreateNewPassword';
+
+type CreateNewPasswordPageProps = {
+  initialValues?: CreateNewPasswordPayload;
+  validatePassword: Validate<string>;
+  validatePasswordConfirmation: Validate<string>;
+  onSubmit: SubmitHandler<CreateNewPasswordPayload>;
+  onLogin: () => void;
+};
+
+const pageLayoutStyleProps: FormPageLayoutStyleProps = {
+  justifyContent: 'center',
+};
+
+const ResetPasswordPage = ({
+  onLogin,
+  ...props
+}: CreateNewPasswordPageProps): ReactElement => {
+  const { t } = useTranslation();
+  return (
+    <BackgroundLayer>
+      <FormPageLayout
+        title={t('page.createPasswordPage.title')}
+        styleProps={pageLayoutStyleProps}
+      >
+        <CreateNewPasswordForm {...props} />
+        <Box fontScale='p3' pbs='x40'>
+          <Trans i18nKey='component.wantToLogin'>
+            Want to log in?
+            <ActionLink fontScale='p3' onClick={onLogin}>
+              Go to login
+            </ActionLink>
+          </Trans>
+        </Box>
+      </FormPageLayout>
+    </BackgroundLayer>
+  );
+};
+
+export default ResetPasswordPage;
