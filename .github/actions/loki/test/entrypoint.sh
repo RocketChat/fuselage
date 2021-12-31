729c2a83f07261e479b79c5688ca84a4015bfbab
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-12-31 18:00:46 -0300

    loki

--- .github/actions/loki/test/entrypoint.sh
+++ .github/actions/loki/test/entrypoint.sh
@@ -6,4 +6,3 @@ yarn run loki:test \
   --chromeFlags='--headless --no-sandbox --disable-gpu --disable-features=VizDisplayCompositor' \
   --verboseRenderer \
   --requireReference \
-  --reactUri file:$INPUT_REACT_URI
