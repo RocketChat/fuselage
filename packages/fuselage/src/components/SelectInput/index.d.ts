97be8dbb70aaa0b8e0bc170a6bcf67b562105945
Guilherme Gazzo <guilhermegazzo@gmail.com>
2022-01-04 17:36:22 -0300

    lint

--- packages/fuselage/src/components/SelectInput/index.d.ts
+++ packages/fuselage/src/components/SelectInput/index.d.ts
@@ -1,16 +1,17 @@
 import { ComponentProps, ForwardRefExoticComponent, ReactNode } from 'react';
 
+import { InputBox } from '..';
 import { Box } from '../Box';
 
-type SelectOptions = readonly (readonly [string, string])[];
+type SelectInputOptions = readonly (readonly [string, string])[];
 
 type SelectInputProps = Omit<ComponentProps<typeof Box>, 'onChange'> & {
   error?: string;
-  options: SelectOptions;
-  onChange: (value: SelectOptions[number][0]) => void;
+  options: SelectInputOptions;
+  onChange: (value: SelectInputOptions[number][0]) => void;
   htmlSize?: number;
   addon: ReactNode;
 };
 
 export const SelectInput: ForwardRefExoticComponent<SelectInputProps>;
-export const SelectInputOption;
+export const SelectInputOption: typeof InputBox.Option;
