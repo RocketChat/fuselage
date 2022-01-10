2547e748e1e934969c32cb9b7809518f3aafa6f0
juliajforesti <juliajforesti@gmail.com>
2022-01-10 12:10:07 -0300

    Convert StatusBullet.js to .ts

--- packages/fuselage/src/components/StatusBullet/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { AllHTMLAttributes, FC } from 'react';
-
-type StatusBulletProps = {
-  status?: 'loading' | 'online' | 'busy' | 'away' | 'offline';
-  size?: 'small' | 'large';
-} & Omit<AllHTMLAttributes<HTMLElement>, 'size'>;
-export const StatusBullet: FC<StatusBulletProps>;
