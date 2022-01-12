99096269558821ba136e3b74c2ce4b46f240cb9c
juliajforesti <juliajforesti@gmail.com>
2022-01-12 15:37:25 -0300

    Convert TelephoneInput component from .js to .tsx

--- packages/fuselage/src/components/TelephoneInput/index.js
+++
@@ -1,7 +0,0 @@
-import React, { forwardRef } from 'react';
-
-import { InputBox } from '../InputBox';
-
-export const TelephoneInput = forwardRef(function TelephoneInput(props, ref) {
-  return <InputBox type='tel' ref={ref} {...props} />;
-});
