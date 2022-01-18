088abb52f598637e9830a0743fdda769a7f67d69
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2022-01-18 14:18:19 -0300

    chore (fuselage): Convert components from .js to .tsx (#617)
    Co-authored-by: Guilherme Gazzo <guilhermegazzo@gmail.com>

--- packages/fuselage/src/components/Modal/ModalClose.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { ActionButton } from '../Button';
-
-export const ModalClose = (props) => (
-  <ActionButton small ghost flexShrink={0} {...props} icon='cross' />
-);
