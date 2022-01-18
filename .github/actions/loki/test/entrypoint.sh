7c9305b6eb06190887698d92f23ef54acb5ea7c7
dougfabris <devfabris@gmail.com>
2022-01-18 13:54:00 -0300

    chore: package.json

--- .github/actions/loki/test/entrypoint.sh
+++ .github/actions/loki/test/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh -ex
 
-mv package.json package.json-old
-cat package.json-old | sed s/\.docker/.app/ > package.json
+for pkg in packages/*/package.json; do
+  cd "$(dirname $pkg)"
+  mv package.json package.json-old
+  cat package.json-old | sed s/\.docker/.app/ > package.json
+  cd -
+done
 
 yarn run loki:test-ci --concurrency=1
 
-mv package.json-old package.json
+for pkg in packages/*/package.json; do
+  cd "$(dirname $pkg)"
+  mv package.json-old package.json
+  cd -
+done
