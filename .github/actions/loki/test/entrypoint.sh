5e5d09106c681fcb2be9b392fedf067bc4a25749
dougfabris <devfabris@gmail.com>
2022-01-18 11:44:08 -0300

    chore: update entrypoint and refs

--- .github/actions/loki/test/entrypoint.sh
+++ .github/actions/loki/test/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -ex
 
+mv package.json package.json-old
+cat package.json-old | sed s/\.docker/.app/ > package.json
+
 yarn run loki:test-ci --concurrency=1
+
+mv package.json-old package.json
