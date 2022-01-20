3015779d1901df5ae40c45a50f0feec9e8e8f3bd
Pedro Rorato <pedroberorato@gmail.com>
2022-01-20 15:13:00 -0300

    convert to ts

--- packages/fuselage/src/components/CodeSnippet/CodeSnippet.tsx
+++ packages/fuselage/src/components/CodeSnippet/CodeSnippet.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { ComponentProps, ReactElement } from 'react';
 
 import { Box } from '../Box';
 import { Button } from '../Button';
 
-const CodeSnippet = ({ code, handleClick, buttonText = 'Copy', ...props }) => (
+type CodeSnippetProps = ComponentProps<typeof Box> & {
+  code?: string;
+  buttonText?: string;
+  handleClick?: () => void;
+};
+
+const CodeSnippet = ({
+  code,
+  handleClick,
+  buttonText = 'Copy',
+  ...props
+}: CodeSnippetProps): ReactElement<CodeSnippetProps> => (
   <Box is='pre' rcx-code-snippet {...props}>
     <Box rcx-code-snippet__codebox>
       <code>{code}</code>
