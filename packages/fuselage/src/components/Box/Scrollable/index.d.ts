73d96e4fb2ddee15eca87245dfd5776c3f1dd2b6
juliajforesti <juliajforesti@gmail.com>
2022-01-20 10:24:55 -0300

    Convert Scrollable component from js to tsx

--- packages/fuselage/src/components/Box/Scrollable/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { ForwardRefExoticComponent, PropsWithChildren } from 'react';
-
-type ScrollableProps = PropsWithChildren<{
-  horizontal?: boolean;
-  vertical?: boolean;
-  onScrollContent?: (touching: { top: boolean }) => void;
-}>;
-const Scrollable: ForwardRefExoticComponent<ScrollableProps>;
-export default Scrollable;
