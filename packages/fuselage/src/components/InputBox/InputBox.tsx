c874f31429a88302b7d08a0b751f18eb5f501763
juliajforesti <juliajforesti@gmail.com>
2022-01-26 11:28:39 -0300

    fix ci

--- packages/fuselage/src/components/InputBox/InputBox.tsx
+++ packages/fuselage/src/components/InputBox/InputBox.tsx
@@ -11,7 +11,7 @@ import React, {
   useRef,
 } from 'react';
 
-import { Input, Addon, InputBoxSkeleton, Wrapper } from '.';
+import { Input, InputBoxAddon as Addon, InputBoxSkeleton, Wrapper } from '.';
 import { Box } from '../Box';
 
 type InputBoxProps = ComponentProps<typeof Box> & {
@@ -20,8 +20,6 @@ type InputBoxProps = ComponentProps<typeof Box> & {
   multiple?: boolean;
   error?: string;
   placeholder?: string;
-  Input?: ForwardRefExoticComponent<typeof Input>;
-  Skeleton?: ForwardRefExoticComponent<typeof InputBoxSkeleton>;
   type:
     | 'button'
     | 'checkbox'
@@ -50,113 +48,114 @@ type InputBoxProps = ComponentProps<typeof Box> & {
     | 'select';
 };
 
-export const InputBox: ForwardRefExoticComponent<InputBoxProps> = forwardRef(
-  function InputBox(
-    {
-      className,
-      addon,
-      error,
-      hidden,
-      invisible,
-      multiple,
-      placeholderVisible,
-      type,
-      onChange,
-      ...props
-    },
-    ref
-  ) {
-    const innerRef = useRef<HTMLObjectElement>(null);
-    const mergedRef = useMergedRefs(ref, innerRef);
+export const InputBox: ForwardRefExoticComponent<InputBoxProps> & {
+  Input?: ComponentProps<typeof Box>;
+  Skeleton?: ComponentProps<typeof InputBoxSkeleton>;
+} = forwardRef(function InputBox(
+  {
+    className,
+    addon,
+    error,
+    hidden,
+    invisible,
+    multiple,
+    placeholderVisible,
+    type,
+    onChange,
+    ...props
+  },
+  ref
+) {
+  const innerRef = useRef<HTMLObjectElement>(null);
+  const mergedRef = useMergedRefs(ref, innerRef);
 
-    useLayoutEffect(() => {
-      if (innerRef.current && innerRef.current.setCustomValidity) {
-        innerRef.current.setCustomValidity(error || '');
-      }
-    }, [error]);
+  useLayoutEffect(() => {
+    if (innerRef.current && innerRef.current.setCustomValidity) {
+      innerRef.current.setCustomValidity(error || '');
+    }
+  }, [error]);
 
-    useLayoutEffect(() => {
+  useLayoutEffect(() => {
+    if (addon && innerRef.current && innerRef.current.parentElement) {
+      innerRef.current.parentElement.classList.toggle(
+        'invalid',
+        !innerRef.current.checkValidity()
+      );
+    }
+  }, []);
+
+  const handleChange = useCallback(
+    (event, ...args) => {
       if (addon && innerRef.current && innerRef.current.parentElement) {
         innerRef.current.parentElement.classList.toggle(
           'invalid',
           !innerRef.current.checkValidity()
         );
       }
-    }, []);
 
-    const handleChange = useCallback(
-      (event, ...args) => {
-        if (addon && innerRef.current && innerRef.current.parentElement) {
-          innerRef.current.parentElement.classList.toggle(
-            'invalid',
-            !innerRef.current.checkValidity()
-          );
-        }
+      return onChange && onChange.call(event.currentTarget, event, ...args);
+    },
+    [addon, onChange]
+  );
 
-        return onChange && onChange.call(event.currentTarget, event, ...args);
-      },
-      [addon, onChange]
-    );
-
-    if (!addon) {
-      return (
-        <Input
-          is={
-            (type === 'textarea' && 'textarea') ||
-            (type === 'select' && 'select') ||
-            'input'
-          }
-          className={className}
-          cols={(type === 'textarea' && 1) || (type === 'select' && 0) || 0}
-          hidden={hidden}
-          invisible={invisible}
-          multiple={multiple}
-          ref={mergedRef}
-          htmlSize={
-            (type === 'textarea' && undefined) || (type === 'select' && 1) || 1
-          }
-          type={type === 'textarea' || type === 'select' ? undefined : type}
-          onChange={handleChange}
-          rcx-input-box--multiple={multiple}
-          rcx-input-box--placeholder-visible={placeholderVisible}
-          rcx-input-box--type={type}
-          {...props}
-        />
-      );
-    }
-
+  if (!addon) {
     return (
-      <Wrapper
-        className={[props.disabled && 'disabled', className]}
+      <Input
+        is={
+          (type === 'textarea' && 'textarea') ||
+          (type === 'select' && 'select') ||
+          'input'
+        }
+        className={className}
+        cols={(type === 'textarea' && 1) || (type === 'select' && 0) || 0}
         hidden={hidden}
         invisible={invisible}
-      >
-        <Input
-          is={
-            (type === 'textarea' && 'textarea') ||
-            (type === 'select' && 'select') ||
-            'input'
-          }
-          className={className}
-          cols={(type === 'textarea' && 1) || (type === 'select' && 0) || 0}
-          multiple={multiple}
-          ref={mergedRef}
-          htmlSize={
-            (type === 'textarea' && undefined) || (type === 'select' && 1) || 1
-          }
-          type={type === 'textarea' || type === 'select' ? undefined : type}
-          onChange={handleChange}
-          rcx-input-box--multiple={multiple}
-          rcx-input-box--placeholder-visible={placeholderVisible}
-          rcx-input-box--type={type}
-          rcx-input-box--undecorated
-          {...props}
-        />
-        <Addon children={addon} />
-      </Wrapper>
+        multiple={multiple}
+        ref={mergedRef}
+        htmlSize={
+          (type === 'textarea' && undefined) || (type === 'select' && 1) || 1
+        }
+        type={type === 'textarea' || type === 'select' ? undefined : type}
+        onChange={handleChange}
+        rcx-input-box--multiple={multiple}
+        rcx-input-box--placeholder-visible={placeholderVisible}
+        rcx-input-box--type={type}
+        {...props}
+      />
     );
   }
-);
+
+  return (
+    <Wrapper
+      className={[props.disabled && 'disabled', className]}
+      hidden={hidden}
+      invisible={invisible}
+    >
+      <Input
+        is={
+          (type === 'textarea' && 'textarea') ||
+          (type === 'select' && 'select') ||
+          'input'
+        }
+        className={className}
+        cols={(type === 'textarea' && 1) || (type === 'select' && 0) || 0}
+        multiple={multiple}
+        ref={mergedRef}
+        htmlSize={
+          (type === 'textarea' && undefined) || (type === 'select' && 1) || 1
+        }
+        type={type === 'textarea' || type === 'select' ? undefined : type}
+        onChange={handleChange}
+        rcx-input-box--multiple={multiple}
+        rcx-input-box--placeholder-visible={placeholderVisible}
+        rcx-input-box--type={type}
+        rcx-input-box--undecorated
+        {...props}
+      />
+      <Addon children={addon} />
+    </Wrapper>
+  );
+});
 
 InputBox.defaultProps = {
   type: 'text',
