20d76aaf272cff2ff84aa0c9a3ea682f25347ebd
juliajforesti <juliajforesti@gmail.com>
2022-01-31 18:06:42 -0300

    fix sidebar

--- packages/fuselage/src/components/Sidebar/index.tsx
+++ packages/fuselage/src/components/Sidebar/index.tsx
@@ -1,6 +1,21 @@
-import Sidebar from './Sidebar';
+import React, { ComponentProps, FC } from 'react';
 
-export default Sidebar;
+import { Box } from '..';
+import SidebarItem from './Item';
+import SidebarSection from './Section';
+import SidebarTopBar from './TopBar';
+
+export const Sidebar: FC<ComponentProps<typeof Box>> & {
+  TopBar?: typeof SidebarTopBar;
+  Item?: typeof SidebarItem;
+  Section?: typeof SidebarSection;
+} = (props) => <Box rcx-sidebar {...props} />;
+
+export default Object.assign(Sidebar, {
+  TopBar: SidebarTopBar,
+  Item: SidebarItem,
+  Section: SidebarSection,
+});
 
 export { default as SidebarItem } from './Item';
 export * from './Item';
@@ -9,5 +24,4 @@ export * from './Section';
 export { default as SidebarTopBar } from './TopBar';
 export * from './TopBar';
 
-export * from './SidebarActions';
 export * from './SidebarFooter';
