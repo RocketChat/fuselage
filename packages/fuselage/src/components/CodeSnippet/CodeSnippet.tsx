46fe245d6bc970ceb187edd33555a4731669a5cf
Pedro Berleze Rorato <41977327+PedroRorato@users.noreply.github.com>
2022-02-01 19:23:27 -0300

    feat(fuselage): Add loading Skeleton to CodeSnippet (#632)
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

--- packages/fuselage/src/components/CodeSnippet/CodeSnippet.tsx
+++ packages/fuselage/src/components/CodeSnippet/CodeSnippet.tsx
@@ -2,6 +2,7 @@ import React, { ComponentProps, ReactElement } from 'react';
 
 import { Box } from '../Box';
 import { Button } from '../Button';
+import { Skeleton } from '../Skeleton';
 
 type CodeSnippetProps = ComponentProps<typeof Box> & {
   children: string;
@@ -14,19 +15,29 @@ const CodeSnippet = ({
   onClick,
   buttonText = 'Copy',
   ...props
-}: CodeSnippetProps): ReactElement<CodeSnippetProps> => (
-  <Box is='pre' rcx-code-snippet {...props}>
-    <Box rcx-code-snippet__codebox>
-      <code>{children}</code>
-    </Box>
-    {onClick && (
-      <Box>
-        <Button small primary onClick={onClick}>
-          {buttonText}
-        </Button>
+}: CodeSnippetProps): ReactElement<CodeSnippetProps> => {
+  if (!children) {
+    return (
+      <Box is='pre' rcx-code-snippet {...props}>
+        <Skeleton w='100%' />
       </Box>
-    )}
-  </Box>
-);
+    );
+  }
+
+  return (
+    <Box is='pre' rcx-code-snippet {...props}>
+      <Box rcx-code-snippet__codebox>
+        <code>{children}</code>
+      </Box>
+      {onClick && children && (
+        <Box>
+          <Button small primary onClick={onClick}>
+            {buttonText}
+          </Button>
+        </Box>
+      )}
+    </Box>
+  );
+};
 
 export default CodeSnippet;
