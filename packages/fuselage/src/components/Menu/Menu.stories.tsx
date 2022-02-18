7c9eecf613b6b0fb4d6c2492d010168a98fd7227
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-02-18 17:50:04 -0300

    Revert "chore: duplicated menuOptions"
    This reverts commit b0cd0b9355a9f7d0c1cfe566fd76392dfbf68b58.

--- packages/fuselage/src/components/Menu/Menu.stories.tsx
+++ packages/fuselage/src/components/Menu/Menu.stories.tsx
@@ -3,7 +3,6 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import React from 'react';
 
 import { Box, Menu } from '..';
-import { menuOptions } from '../../../.storybook/helpers';
 import { Icon } from '../Icon';
 
 export default {
@@ -27,7 +26,26 @@ const Template: ComponentStory<typeof Menu> = (args) => (
 
 export const simple = Template.bind({});
 simple.args = {
-  options: menuOptions,
+  options: {
+    makeAdmin: {
+      label: (
+        <Box display='flex' alignItems='center'>
+          <Icon mie='x4' name='key' size='x16' />
+          Make Admin
+        </Box>
+      ),
+      action: action('makeAdmin.action'),
+    },
+    delete: {
+      label: (
+        <Box display='flex' alignItems='center' color='danger'>
+          <Icon mie='x4' name='trash' size='x16' />
+          Delete
+        </Box>
+      ),
+      action: action('delete.action'),
+    },
+  },
 };
 
 export const complex = Template.bind({});
