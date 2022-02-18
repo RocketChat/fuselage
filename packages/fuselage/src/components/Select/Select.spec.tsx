9ee50b16ac598f72f5549ad80d7b446f26233ac7
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-02-18 20:43:19 -0300

    Fix failing test

--- packages/fuselage/src/components/Select/Select.spec.tsx
+++ packages/fuselage/src/components/Select/Select.spec.tsx
@@ -1,8 +1,11 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { withResizeObserverMock } from 'testing-utils/mocks/withResizeObserverMock';
 
 import { Select } from '.';
 import { options } from './Select.stories';
+
+withResizeObserverMock();
 
 it('renders without crashing', () => {
   render(<Select options={options} onChange={() => undefined} />);
