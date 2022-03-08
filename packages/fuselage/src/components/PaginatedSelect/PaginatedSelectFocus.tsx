14109eedac39f5cb75b8394158ffcec1a37df2e8
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-03-08 16:49:36 -0300

    fix(fuselage): `PaginatedSelectFiltered` filter state (#661)
    * fix
    
    * Simplify stories
    
    * Split into subcomponents
    
    * Rename directory
    
    Co-authored-by: Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>

---
+++ packages/fuselage/src/components/PaginatedSelect/PaginatedSelectFocus.tsx
@@ -0,0 +1,25 @@
+import type { ComponentProps, Ref } from 'react';
+import React, { forwardRef } from 'react';
+
+import { Box } from '../Box';
+
+type PaginatedSelectFocusProps = ComponentProps<typeof Box>;
+
+const PaginatedSelectFocus = forwardRef(function PaginatedSelectFocus(
+  props: PaginatedSelectFocusProps,
+  ref: Ref<HTMLButtonElement>
+) {
+  return (
+    <Box
+      ref={ref}
+      fontScale='p2m'
+      color='hint'
+      rcx-select__focus
+      is='button'
+      type='button'
+      {...props}
+    />
+  );
+});
+
+export default PaginatedSelectFocus;
