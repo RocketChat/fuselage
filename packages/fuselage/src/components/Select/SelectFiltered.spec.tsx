4002b6f9d3e4e2072f895375ab55b955320e0d95
Tasso Evangelista <tasso.evangelista@rocket.chat>
2022-03-11 20:34:50 -0300

    Add tests for `SelectFiltered`

--- packages/fuselage/src/components/Select/SelectFiltered.spec.tsx
+++ packages/fuselage/src/components/Select/SelectFiltered.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import { withResizeObserverMock } from 'testing-utils/mocks/withResizeObserverMock';
 
@@ -8,4 +8,94 @@ withResizeObserverMock();
 
 it('renders without crashing', () => {
   render(<SelectFiltered options={[]} onChange={() => undefined} />);
+});
+
+it('renders with options', async () => {
+  const handleChange = jest.fn();
+
+  render(
+    <SelectFiltered
+      options={[
+        ['1', 'Item #1'],
+        ['2', 'Item #2'],
+      ]}
+      onChange={handleChange}
+    />
+  );
+
+  fireEvent.click(screen.getByRole('combobox'));
+
+  await waitFor(() =>
+    expect(screen.queryByRole('listbox')).toBeInTheDocument()
+  );
+
+  fireEvent.mouseDown(screen.getByRole('option', { name: 'Item #2' }));
+
+  expect(handleChange).toHaveBeenCalledTimes(1);
+  expect(handleChange).toHaveBeenCalledWith('2');
+});
+
+it('accepts filter', async () => {
+  const handleSetFilter = jest.fn();
+
+  render(
+    <SelectFiltered
+      options={[
+        ['1', 'Item #1'],
+        ['2', 'Item #2'],
+      ]}
+      onChange={jest.fn()}
+      setFilter={handleSetFilter}
+    />
+  );
+
+  fireEvent.input(screen.getByRole('textbox'), { target: { value: '#1' } });
+
+  expect(handleSetFilter).toHaveBeenCalledTimes(1);
+  expect(handleSetFilter).toHaveBeenCalledWith('#1');
+});
+
+it('does NOT filters options if controlled', async () => {
+  render(
+    <SelectFiltered
+      filter='Item #1'
+      options={[
+        ['1', 'Item #1'],
+        ['2', 'Item #2'],
+      ]}
+      onChange={jest.fn()}
+    />
+  );
+
+  fireEvent.click(screen.getByRole('combobox'));
+
+  await waitFor(() =>
+    expect(screen.queryByRole('listbox')).toBeInTheDocument()
+  );
+
+  expect(screen.queryByRole('option', { name: 'Item #2' })).toBeInTheDocument();
+});
+
+it('does filters options if uncontrolled', async () => {
+  render(
+    <SelectFiltered
+      options={[
+        ['1', 'Item #1'],
+        ['2', 'Item #2'],
+      ]}
+      onChange={jest.fn()}
+    />
+  );
+
+  fireEvent.click(screen.getByRole('combobox'));
+
+  fireEvent.input(screen.getByRole('textbox'), { target: { value: '#1' } });
+
+  await waitFor(() =>
+    expect(screen.queryByRole('listbox')).toBeInTheDocument()
+  );
+
+  expect(
+    screen.queryByRole('option', { name: 'Item #2' })
+  ).not.toBeInTheDocument();
 });
