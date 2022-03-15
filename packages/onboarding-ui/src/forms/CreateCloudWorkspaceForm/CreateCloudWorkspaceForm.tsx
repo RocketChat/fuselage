f42dcf96f0386158079ea03240039218fbf0dc89
Pedro Rorato <pedroberorato@gmail.com>
2022-03-15 19:32:35 -0300

    fix remove TODO comments

--- packages/onboarding-ui/src/forms/CreateCloudWorkspaceForm/CreateCloudWorkspaceForm.tsx
+++ packages/onboarding-ui/src/forms/CreateCloudWorkspaceForm/CreateCloudWorkspaceForm.tsx
@@ -110,15 +110,11 @@ const CreateCloudWorkspaceForm = ({
           </Field.Label>
           <Field.Row>
             <WorkspaceUrlInput
-              // TODO: refactor WorkspaceUrlInput error={errors?.workspaceURL?.type || undefined}
               domain={domain}
               {...register('workspaceURL', {
                 required: true,
                 validate: validateUrl,
               })}
-              // TODO: refactor WorkspaceUrlInput placeholder={t(
-              //   'form.createCloudWorkspace.fields.workspaceUrl.placeholder'
-              // )}
             />
           </Field.Row>
           {errors.workspaceURL?.type === 'required' && (
