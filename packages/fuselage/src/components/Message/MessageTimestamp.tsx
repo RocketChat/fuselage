1aaa0ace850f4b75d9cdf324326a6ba9ccff0418
juliajforesti <juliajforesti@gmail.com>
2022-04-01 09:43:56 -0300

    title optional

--- packages/fuselage/src/components/Message/MessageTimestamp.tsx
+++ packages/fuselage/src/components/Message/MessageTimestamp.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 import React from 'react';
 
-type MessageTimestampProps = { children: ReactNode; title: string };
+type MessageTimestampProps = { children: ReactNode; title?: string };
 
 export const MessageTimestamp = (props: MessageTimestampProps) => (
   <span className='rcx-box rcx-box--full rcx-message-header__time' {...props} />
