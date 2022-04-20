7cdf6b7539f80216eb23f3baa738859926aac72a
gabriellsh <40830821+gabriellsh@users.noreply.github.com>
2022-04-20 19:18:57 -0300

    fix(fuselage): Missing type on system message (#706)
    * Add onClick
    
    * add isSelected
    
    * import style
    
    * Fix for real now

--- packages/fuselage/src/components/Message/MessageSystem/MessageSystem.tsx
+++ packages/fuselage/src/components/Message/MessageSystem/MessageSystem.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import type { ReactNode, MouseEvent as ReactMouseEvent } from 'react';
 import React from 'react';
 
 import './MessageSystem.styles.scss';
@@ -6,15 +6,23 @@ import './MessageSystem.styles.scss';
 type MessageSystemProps = {
   children?: ReactNode;
   title?: string;
+  isSelected?: boolean;
+  onClick?: (e: ReactMouseEvent<HTMLDivElement, MouseEvent>) => void;
 };
 
 export const MessageSystem = ({
   children,
   title,
+  isSelected,
   ...rest
 }: MessageSystemProps) => (
   <div
-    className='rcx-box rcx-box--full rcx-message-system'
+    className={[
+      'rcx-box rcx-box--full rcx-message-system',
+      isSelected && 'rcx-message-system--selected',
+    ]
+      .filter(Boolean)
+      .join(' ')}
     title={title}
     {...rest}
   >
