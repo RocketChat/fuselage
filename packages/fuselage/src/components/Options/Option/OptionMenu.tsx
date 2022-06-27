8a12c8d67396340b29872a59cf0050cbbd23280d
gabriellsh <40830821+gabriellsh@users.noreply.github.com>
2022-06-27 14:18:26 -0300

    feat(icons): New icons (#755)
    * Add icons
    
    * Fix optionmenu type

--- packages/fuselage/src/components/Options/Option/OptionMenu.tsx
+++ packages/fuselage/src/components/Options/Option/OptionMenu.tsx
@@ -1,9 +1,9 @@
-import type { ReactNode } from 'react';
+import type { HTMLAttributes, ReactNode } from 'react';
 import React from 'react';
 
 type OptionMenuProps = {
   children?: ReactNode;
-};
+} & HTMLAttributes<HTMLDivElement>;
 
 const OptionMenu = (props: OptionMenuProps) => (
   <div className='rcx-box--animated rcx-option__menu-wrapper' {...props} />
