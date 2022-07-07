49c082c32995ca102bec03acc192fe7272eedb0c
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2022-07-07 14:22:31 -0300

    fix: IconButton disabled color and background (#762)

--- packages/fuselage/src/components/Button/IconButton.stories.tsx
+++ packages/fuselage/src/components/Button/IconButton.stories.tsx
@@ -35,36 +35,66 @@ export default {
 export const _IconButton: ComponentStory<typeof IconButton> = () => (
   <IconButton icon='balloon' />
 );
+export const _IconButtonDisabled: ComponentStory<typeof IconButton> = () => (
+  <IconButton icon='balloon' disabled />
+);
+
 export const _IconButtonMini: ComponentStory<typeof IconButton> = () => (
   <IconButton icon='balloon' mini />
 );
-export const _IconButtonSecondary: ComponentStory<typeof IconButton> = () => (
-  <IconButton icon='balloon' secondary />
-);
+
 export const _IconButtonInfo: ComponentStory<typeof IconButton> = () => (
   <IconButton icon='balloon' info />
 );
+export const _IconButtonInfoDisabled: ComponentStory<
+  typeof IconButton
+> = () => <IconButton icon='balloon' info disabled />;
+
 export const _IconButtonSecondaryInfo: ComponentStory<
   typeof IconButton
 > = () => <IconButton icon='balloon' secondary info />;
+export const _IconButtonSecondaryInfoDisabled: ComponentStory<
+  typeof IconButton
+> = () => <IconButton icon='balloon' secondary info disabled />;
 
 export const _IconButtonSuccess: ComponentStory<typeof IconButton> = () => (
   <IconButton icon='balloon' success />
 );
+export const _IconButtonSuccessDisabled: ComponentStory<
+  typeof IconButton
+> = () => <IconButton icon='balloon' success disabled />;
+
 export const _IconButtonSecondarySuccess: ComponentStory<
   typeof IconButton
 > = () => <IconButton icon='balloon' secondary success />;
+export const _IconButtonSecondarySuccessDisabled: ComponentStory<
+  typeof IconButton
+> = () => <IconButton icon='balloon' secondary success disabled />;
 
 export const _IconButtonWarning: ComponentStory<typeof IconButton> = () => (
   <IconButton icon='balloon' warning />
 );
+export const _IconButtonWarningDisabled: ComponentStory<
+  typeof IconButton
+> = () => <IconButton icon='balloon' warning disabled />;
+
 export const _IconButtonSecondaryWarning: ComponentStory<
   typeof IconButton
 > = () => <IconButton icon='balloon' secondary warning />;
+export const _IconButtonSecondaryWarningDisabled: ComponentStory<
+  typeof IconButton
+> = () => <IconButton icon='balloon' secondary warning disabled />;
 
 export const _IconButtonDanger: ComponentStory<typeof IconButton> = () => (
   <IconButton icon='balloon' danger />
 );
+export const _IconButtonDangerDisabled: ComponentStory<
+  typeof IconButton
+> = () => <IconButton icon='balloon' danger disabled />;
+
 export const _IconButtonSecondaryDanger: ComponentStory<
   typeof IconButton
 > = () => <IconButton icon='balloon' secondary danger />;
+export const _IconButtonSecondaryDangerDisabled: ComponentStory<
+  typeof IconButton
+> = () => <IconButton icon='balloon' secondary danger disabled />;
