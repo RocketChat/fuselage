9887bd53e6b3504e83d7d8f0e35ed4c590f57e19
dougfabris <devfabris@gmail.com>
2022-07-29 14:27:22 -0300

    improve: modal component

---
+++ packages/fuselage/src/components/Modal/ModalFooterControllers.tsx
@@ -0,0 +1,14 @@
+import type { ComponentProps } from 'react';
+import React from 'react';
+
+import { ButtonGroup } from '..';
+
+export type ModalFooterControllersProps = ComponentProps<typeof ButtonGroup>;
+
+export const ModalFooterControllers = ({
+  children,
+}: ModalFooterControllersProps) => (
+  <ButtonGroup medium align='end'>
+    {children}
+  </ButtonGroup>
+);
