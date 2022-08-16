ffb3bdcb6cb270d7a98d3f680e6c56d5df8c87a8
juliajforesti <juliajforesti@gmail.com>
2022-08-16 09:36:38 -0300

    Update Checkbox tests

--- packages/fuselage/src/components/CheckBox/CheckBox.spec.tsx
+++ packages/fuselage/src/components/CheckBox/CheckBox.spec.tsx
@@ -4,19 +4,29 @@ import React from 'react';
 
 import * as stories from './CheckBox.stories';
 
-const { Default } = composeStories(stories);
+const { Default, Indeterminate } = composeStories(stories);
 
 describe('[CheckBox Component]', () => {
   it('renders without crashing', () => {
     render(<Default />);
   });
+  it('changes style of element as checkbox is checked', () => {
+    const { container } = render(<Default />);
+    const checkbox = getByRole(container, 'checkbox') as HTMLInputElement;
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toEqual(true);
+  });
   it('changes style of element as checkbox is checked/unchecked', () => {
     const { container } = render(<Default />);
     const checkbox = getByRole(container, 'checkbox') as HTMLInputElement;
-    expect(checkbox.checked).toEqual(false);
     fireEvent.click(checkbox);
     expect(checkbox.checked).toEqual(true);
     fireEvent.click(checkbox);
     expect(checkbox.checked).toEqual(false);
+  });
+  it('displays checkbox indeterminate correctly', () => {
+    const { container } = render(<Indeterminate />);
+    const checkbox = getByRole(container, 'checkbox') as HTMLInputElement;
+    expect(checkbox.indeterminate).toEqual(true);
   });
 });
