4bcf92fc08e5d7c9be4c1dc15d8d515c052d1711
Pedro Rorato <pedroberorato@gmail.com>
2022-08-17 16:47:02 -0300

    feat: separate storybook testing from component testing

--- packages/fuselage/src/components/Badge/Badge.spec.tsx
+++ packages/fuselage/src/components/Badge/Badge.spec.tsx
@@ -2,38 +2,57 @@ import { composeStories } from '@storybook/testing-react';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
+import { Badge } from './Badge';
 import * as stories from './Badge.stories';
 
-const { Default, Disabled } = composeStories(stories);
+const { Default, Primary, Secondary, Danger, Warning, Disabled } =
+  composeStories(stories);
 
 describe('[Badge Component]', () => {
-  it('renders without crashing', () => {
-    render(<Default />);
+  describe('Story renders without crashing', () => {
+    it('Default', () => {
+      render(<Default />);
+    });
+    it('Primary', () => {
+      render(<Primary />);
+    });
+    it('Secondary', () => {
+      render(<Secondary />);
+    });
+    it('Danger', () => {
+      render(<Danger />);
+    });
+    it('Warning', () => {
+      render(<Warning />);
+    });
+    it('Disabled', () => {
+      render(<Disabled />);
+    });
   });
 
   it('should display children', () => {
-    render(<Default>Children</Default>);
+    render(<Badge>Children</Badge>);
     screen.getByText('Children');
   });
 
   it('should have class according to variant', () => {
-    const { container } = render(<Default variant='primary' />);
+    const { container } = render(<Badge variant='primary' />);
     expect(container.querySelector('.rcx-badge--primary')).toBeInTheDocument();
   });
 
   it('should have class passed as property', () => {
-    const { container } = render(<Default className='test-class' />);
+    const { container } = render(<Badge className='test-class' />);
     expect(container.querySelector('.test-class')).toBeInTheDocument();
   });
 
   it('should show title when hovered', () => {
-    const { container } = render(<Default title='test-title' />);
+    const { container } = render(<Badge title='test-title' />);
     const badgeTitle = container.querySelector('span');
     expect(badgeTitle?.getAttribute('title')).toEqual('test-title');
   });
 
   it('should have disabled class if it has the disabled property', () => {
-    const { container } = render(<Disabled />);
+    const { container } = render(<Badge disabled />);
     expect(container.querySelector('.rcx-badge--disabled')).toBeInTheDocument();
   });
 });
