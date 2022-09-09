3e4c893562b7bb02536766c43bb85b95a96773f9
Guilherme Gazzo <guilherme@gazzo.xyz>
2022-09-09 13:03:35 -0300

    chore(layout): Added more form props

--- packages/layout/src/FormPageLayout/Form.tsx
+++ packages/layout/src/FormPageLayout/Form.tsx
@@ -1,23 +1,25 @@
 import { Tile } from '@rocket.chat/fuselage';
 import colors from '@rocket.chat/fuselage-tokens/colors.json';
-import type { ReactElement, FC } from 'react';
+import type { ReactElement, FC, FormHTMLAttributes } from 'react';
+import { forwardRef } from 'react';
 
-const Form: FC<{ onSubmit: () => void }> = ({
-  onSubmit,
-  children,
-}): ReactElement => (
-  <Tile
-    is='form'
-    backgroundColor={colors.white}
-    color={colors.n800}
-    padding='x40'
-    width='full'
-    maxWidth={576}
-    textAlign='left'
-    onSubmit={onSubmit}
-  >
-    {children}
-  </Tile>
+const Form = forwardRef<
+  HTMLElement,
+  FC<Omit<FormHTMLAttributes<HTMLFormElement>, 'is'>>
+>(
+  ({ ...props }, ref): ReactElement => (
+    <Tile
+      ref={ref}
+      is='form'
+      backgroundColor={colors.white}
+      color={colors.n800}
+      padding='x40'
+      width='full'
+      maxWidth={576}
+      textAlign='left'
+      {...props}
+    />
+  )
 );
 
 export default Form;
