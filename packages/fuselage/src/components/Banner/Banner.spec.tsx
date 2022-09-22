25c581a9b5ace956de1f46ea5b8ed50658104741
csuarez <carlos.suarez@rocket.chat>
2022-09-22 10:28:38 -0500

    test: Add test to check link rendering on banner

--- packages/fuselage/src/components/Banner/Banner.spec.tsx
+++ packages/fuselage/src/components/Banner/Banner.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { withResizeObserverMock } from 'testing-utils/mocks/withResizeObserverMock';
 
@@ -9,5 +9,14 @@ withResizeObserverMock();
 describe('[Banner Component]', () => {
   it('renders without crashing', () => {
     render(<Banner />);
+  });
+
+  it('renders with link', () => {
+    render(<Banner link='https://rocket.chat' linkText='More info' />);
+    expect(
+      screen.getByRole('link', {
+        name: /more info/i,
+      })
+    ).toBeInTheDocument();
   });
 });
