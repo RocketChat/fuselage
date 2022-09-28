afd3525f7382f5cf99f3e9b8f82e5d62d237315e
Hugo Costa <hugocarreiracosta@gmail.com>
2022-09-28 13:54:16 -0300

    Fixing URL/links parsing issues

--- packages/message-parser/src/grammar.pegjs
+++ packages/message-parser/src/grammar.pegjs
@@ -171,16 +171,9 @@ Space
   / "\t"
 
 anyText
-  = [\x20-\x27] /*     ! " # $ % & ' ( )   */
+  = [\x20-\x27] //     ! " # $ % & '
   / [\x2B-\x40] // + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @
   / [\x41-\x5A] // A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
-  / [\x61-\x7A] // a b c d e f g h i j k l m n o p q r s t u v w x y z
-  / nonascii
-
-SectionText
-  = [-]+
-  / [\x20-\x40] //   ! " # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @
-  / [\x41-\x60] // A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] ^ _ `
   / [\x61-\x7A] // a b c d e f g h i j k l m n o p q r s t u v w x y z
   / nonascii
 
@@ -281,9 +274,9 @@ LinkTitle2
 
 References
   = "[](" href:LinkRef ")" { return link(href); }
-  / "[" title:LinkTitle "](" href:LinkRef ")" { return link(href, title); }
+  / "[" title:LinkTitle* "](" href:LinkRef ")" { return link(href, title); }
   / "<" href:LinkRef "|" title:LinkTitle2 ">" {
-      return link(href, plain(title));
+      return link(href, [plain(title)]);
     }
 
 /* Macros */
@@ -301,32 +294,11 @@ escape
   = unicode
   / "\\" ch:[^\r\n\f0-9a-f]i { return ch; }
 
-nmstart
-  = [_a-z]i
-  / nonascii
-  / escape
-
-nmchar
-  = [_a-z0-9-]i
-  / nonascii
-  / escape
-
-string1
-  = "\"" chars:$([^\n\r\f\\"] / "\\" nl:nl { return ''; } / escape)* "\"" {
-      return chars;
-    }
-
-nl
-  = "\n"
-  / "\r\n"
-  / "\r"
-  / "\f"
-
-AutolinkedPhone = p:Phone { return link('tel:' + p.number, plain(p.text)); }
+AutolinkedPhone = p:Phone { return link('tel:' + p.number, [plain(p.text)]); }
 
 AutolinkedURL = u:URL { return link(u); }
 
-AutolinkedEmail = e:Email { return link('mailto:' + e, plain(e)); }
+AutolinkedEmail = e:Email { return link('mailto:' + e, [plain(e)]); }
 
 alpha = [a-zA-Z]
 
@@ -375,11 +347,11 @@ hexByte = a:hexdigit b:hexdigit { return parseInt(a + b, 16); }
 
 domainName
   = "localhost"
-  / $(domainNameLabel ("." domainChar domainNameLabel)+)
+  / $(domainNameLabel ("." (!digit domainChar) domainNameLabel)+)
 
 domainNameLabel = $(domainChar+ $("-" domainChar+)*)
 
-domainChar = !"\\" !"/" !"|" !">" !"<" !safe !extra !EndOfLine !Space .
+domainChar = !"\\" !"/" !"|" !">" !"<" !"%" !safe !extra !EndOfLine !Space .
 
 /**
  *
@@ -409,23 +381,8 @@ phonePrefix
  */
 
 URL
-  = $(
-    s:urlScheme
-      a:urlAuthority
-      p:urlPath?
-      q:urlQuery?
-      f:urlFragment?
-      g:urlPath?
-      h:urlQuery?
-  )
-  / $(
-    urlAuthorityHost
-      p:urlPath?
-      q:urlQuery?
-      f:urlFragment?
-      g:urlPath?
-      h:urlQuery?
-  )
+  = $(urlScheme urlAuthority urlBody)
+  / $(urlAuthorityHost urlBody)
 
 urlScheme
   = $(
@@ -464,6 +421,25 @@ urlScheme
       ":"
   )
 
+urlBody
+  = (
+    !Whitespace
+      (
+        anyText
+        / "*"
+        / "["
+        / "\/"
+        / "]"
+        / "^"
+        / "_"
+        / "`"
+        / "{"
+        / "}"
+        / "~"
+        / "("
+      )
+  )*
+
 urlAuthority = $("//" urlAuthorityUserInfo? urlAuthorityHost)
 
 urlAuthorityUserInfo = $(urlAuthorityUser (":" urlAuthorityPassword)? "@")
@@ -480,12 +456,6 @@ urlAuthorityHostName
 
 urlAuthorityPort
   = digits // TODO: from "0" to "65535"
-
-urlPath = $("/" $(!"?" !"/" !"#" !")" !">" !"|" !" " .)* urlPath*)
-
-urlQuery = $("?" $(alpha_digit / safe)*)
-
-urlFragment = $("#" $(alpha_digit / extra / safe)*)
 
 /**
  *
--- packages/message-parser/tests/link.test.ts
+++ packages/message-parser/tests/link.test.ts
@@ -12,7 +12,7 @@ import {
 test.each([
   [
     '<https://domain.com|Test>',
-    [paragraph([link('https://domain.com', plain('Test'))])],
+    [paragraph([link('https://domain.com', [plain('Test')])])],
   ],
 
   [
@@ -32,56 +32,57 @@ test.each([
     '[Link](https://domain.com/link?a=%28node_filesystem_avail_bytes%29)',
     [
       paragraph([
-        link(
-          'https://domain.com/link?a=%28node_filesystem_avail_bytes%29',
-          plain('Link')
-        ),
+        link('https://domain.com/link?a=%28node_filesystem_avail_bytes%29', [
+          plain('Link'),
+        ]),
       ]),
     ],
   ],
   ['[](https://rocket.chat)', [paragraph([link('https://rocket.chat')])]],
   [
     '[ ](https://rocket.chat)',
-    [paragraph([link('https://rocket.chat', plain(' '))])],
+    [paragraph([link('https://rocket.chat', [plain(' ')])])],
   ],
 
   [
     '[ test](https://rocket.chat)',
-    [paragraph([link('https://rocket.chat', plain(' test'))])],
+    [paragraph([link('https://rocket.chat', [plain(' test')])])],
   ],
   [
     '[ test ](https://rocket.chat)',
-    [paragraph([link('https://rocket.chat', plain(' test '))])],
+    [paragraph([link('https://rocket.chat', [plain(' test ')])])],
   ],
   [
     '[title](https://rocket.chat)',
-    [paragraph([link('https://rocket.chat', plain('title'))])],
+    [paragraph([link('https://rocket.chat', [plain('title')])])],
   ],
   [
     '[title](http://localhost)',
-    [paragraph([link('http://localhost', plain('title'))])],
+    [paragraph([link('http://localhost', [plain('title')])])],
   ],
   [
     '[title](http://localhost?testing=true)',
-    [paragraph([link('http://localhost?testing=true', plain('title'))])],
+    [paragraph([link('http://localhost?testing=true', [plain('title')])])],
   ],
   [
     '[**title**](https://rocket.chat)',
-    [paragraph([link('https://rocket.chat', bold([plain('title')]))])],
+    [paragraph([link('https://rocket.chat', [bold([plain('title')])])])],
   ],
   [
     '[~~title~~](https://rocket.chat)',
-    [paragraph([link('https://rocket.chat', strike([plain('title')]))])],
+    [paragraph([link('https://rocket.chat', [strike([plain('title')])])])],
   ],
   [
     '[__title__](https://rocket.chat)',
-    [paragraph([link('https://rocket.chat', italic([plain('title')]))])],
+    [paragraph([link('https://rocket.chat', [italic([plain('title')])])])],
   ],
   [
     '[__**~~title~~**__](https://rocket.chat)',
     [
       paragraph([
-        link('https://rocket.chat', italic([bold([strike([plain('title')])])])),
+        link('https://rocket.chat', [
+          italic([bold([strike([plain('title')])])]),
+        ]),
       ]),
     ],
   ],
@@ -101,7 +102,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
-          plain('Test')
+          [plain('Test')]
         ),
       ]),
     ],
@@ -112,7 +113,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
-          plain('title')
+          [plain('title')]
         ),
       ]),
     ],
@@ -123,7 +124,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
-          bold([plain('title')])
+          [bold([plain('title')])]
         ),
       ]),
     ],
@@ -134,7 +135,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
-          strike([plain('title')])
+          [strike([plain('title')])]
         ),
       ]),
     ],
@@ -145,7 +146,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
-          italic([plain('title')])
+          [italic([plain('title')])]
         ),
       ]),
     ],
@@ -156,7 +157,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
-          italic([bold([strike([plain('title')])])])
+          [italic([bold([strike([plain('title')])])])]
         ),
       ]),
     ],
@@ -167,7 +168,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351?query=test12-34',
-          plain('title')
+          [plain('title')]
         ),
       ]),
     ],
@@ -178,7 +179,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do?query=test12-34#Cases/dv/413244000073043351',
-          plain('title')
+          [plain('title')]
         ),
       ]),
     ],
@@ -189,7 +190,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351?query=test12-34&query2=abc123',
-          plain('title')
+          [plain('title')]
         ),
       ]),
     ],
@@ -200,7 +201,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases?query=test12-34&query2=abcd!e/dv/413244000073043351',
-          plain('title')
+          [plain('title')]
         ),
       ]),
     ],
@@ -211,7 +212,7 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases?query=test12-34&query2=abcd!e/dv/413244000073043351',
-          plain('title')
+          [plain('title')]
         ),
       ]),
     ],
@@ -222,28 +223,33 @@ test.each([
       paragraph([
         link(
           'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351?query=test12-34&query2=abcd!~-._%2B+',
-          plain('title')
+          [plain('title')]
         ),
       ]),
     ],
   ],
-  ['google.com', [paragraph([link('//google.com', plain('google.com'))])]],
+  ['google.com', [paragraph([link('//google.com', [plain('google.com')])])]],
   ['www.google.com', [paragraph([link('www.google.com')])]],
   ['rocket.chat:8080', [paragraph([link('rocket.chat:8080')])]],
   ['ShouldNotBeALink', [paragraph([plain('ShouldNotBeALink')])]],
   [
     'http:/ google.com',
-    [paragraph([plain('http:/ '), link('//google.com', plain('google.com'))])],
+    [
+      paragraph([
+        plain('http:/ '),
+        link('//google.com', [plain('google.com')]),
+      ]),
+    ],
   ],
   [
     '[custom](custom://google.com)',
-    [paragraph([link('custom://google.com', plain('custom'))])],
+    [paragraph([link('custom://google.com', [plain('custom')])])],
   ],
   [
     '[thing](https://www.thingiverse.com/thing:5451684)',
     [
       paragraph([
-        link('https://www.thingiverse.com/thing:5451684', plain('thing')),
+        link('https://www.thingiverse.com/thing:5451684', [plain('thing')]),
       ]),
     ],
   ],
@@ -255,10 +261,134 @@ test.each([
     '[telegram invite](https://t.me/joinchat/chatexample)',
     [
       paragraph([
-        link('https://t.me/joinchat/chatexample', plain('telegram invite')),
+        link('https://t.me/joinchat/chatexample', [plain('telegram invite')]),
       ]),
     ],
   ],
+  [
+    '[Test with **bold** element](https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351)',
+    [
+      paragraph([
+        link(
+          'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
+          [plain(`Test with `), bold([plain('bold')]), plain(` element`)]
+        ),
+      ]),
+    ],
+  ],
+  [
+    '[Test with *bold* element](https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351)',
+    [
+      paragraph([
+        link(
+          'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
+          [plain(`Test with `), bold([plain('bold')]), plain(` element`)]
+        ),
+      ]),
+    ],
+  ],
+  [
+    '[Test with _italic_ element](https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351)',
+    [
+      paragraph([
+        link(
+          'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
+          [plain(`Test with `), italic([plain('italic')]), plain(` element`)]
+        ),
+      ]),
+    ],
+  ],
+  [
+    '[Test with ~strike~ element](https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351)',
+    [
+      paragraph([
+        link(
+          'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
+          [plain(`Test with `), strike([plain('strike')]), plain(` element`)]
+        ),
+      ]),
+    ],
+  ],
+  [
+    '[Test with __**~~title~~**__ element](https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351)',
+    [
+      paragraph([
+        link(
+          'https://desk.rocket.chat/support/rocketchat/ShowHomePage.do#Cases/dv/413244000073043351',
+          [
+            plain(`Test with `),
+            italic([bold([strike([plain('title')])])]),
+            plain(` element`),
+          ]
+        ),
+      ]),
+    ],
+  ],
+  [
+    '[Github link with hash](https://github.com/RocketChat/Rocket.Chat/pull/26751/files#diff-c87b108ecf1ede549f8ede68eca840fbb330180b927df0b8a0b4df5d06cbd89b)',
+    [
+      paragraph([
+        link(
+          'https://github.com/RocketChat/Rocket.Chat/pull/26751/files#diff-c87b108ecf1ede549f8ede68eca840fbb330180b927df0b8a0b4df5d06cbd89b',
+          [plain(`Github link with hash`)]
+        ),
+      ]),
+    ],
+  ],
+  [
+    '[Github link with hash](https://github.com/RocketChat/Rocket.Chat/pull/26751/files#diff)',
+    [
+      paragraph([
+        link(
+          'https://github.com/RocketChat/Rocket.Chat/pull/26751/files#diff',
+          [plain(`Github link with hash`)]
+        ),
+      ]),
+    ],
+  ],
+  [
+    '[Github link without hash](https://github.com/RocketChat/Rocket.Chat/pull/26751/files)',
+    [
+      paragraph([
+        link('https://github.com/RocketChat/Rocket.Chat/pull/26751/files', [
+          plain(`Github link without hash`),
+        ]),
+      ]),
+    ],
+  ],
+  [
+    '[Link with special chars](https://github.com/RocketChat/Rocket.Chat*[/]^_`{}~)',
+    [
+      paragraph([
+        link('https://github.com/RocketChat/Rocket.Chat*[/]^_`{}~', [
+          plain(`Link with special chars`),
+        ]),
+      ]),
+    ],
+  ],
+  [
+    '[Google complex Link](https://www.google.com/url?rct=j&sa=t&url=https://ga.de/freizeit/region-erleben/bonn-und-region-tipps-fuers-wochenende-flohmarkt-rheinaue-weltkindertag-stadtfest_aid-53876987&ct=ga&cd=CAIyHDQ0NzEyYWE3MDA1MGNhNTQ6Y29tOmRlOkRFOlI&usg=AOvVaw3ySYrO9lM0iNSnk43gPVwZ)',
+    [
+      paragraph([
+        link(
+          'https://www.google.com/url?rct=j&sa=t&url=https://ga.de/freizeit/region-erleben/bonn-und-region-tipps-fuers-wochenende-flohmarkt-rheinaue-weltkindertag-stadtfest_aid-53876987&ct=ga&cd=CAIyHDQ0NzEyYWE3MDA1MGNhNTQ6Y29tOmRlOkRFOlI&usg=AOvVaw3ySYrO9lM0iNSnk43gPVwZ',
+          [plain(`Google complex Link`)]
+        ),
+      ]),
+    ],
+  ],
+  [
+    '[9gag](https://9gag.com/)',
+    [paragraph([link('https://9gag.com/', [plain(`9gag`)])])],
+  ],
+  ['[9gag](9gag.com)', [paragraph([link('9gag.com', [plain(`9gag`)])])]],
+  ['<9gag.com|9gag>', [paragraph([link('9gag.com', [plain(`9gag`)])])]],
+  ['9gag.com', [paragraph([link('9gag.com')])]],
+  ['[77.77%](77.77%)', [paragraph([plain('[77.77%](77.77%)')])]],
+  ['77.77%', [paragraph([plain('77.77%')])]],
+  ['[77.77](77.77)', [paragraph([plain('[77.77](77.77)')])]],
+  ['77.77', [paragraph([plain('77.77')])]],
+  ['test.9gag', [paragraph([plain('test.9gag')])]],
 ])('parses %p', (input, output) => {
   expect(parse(input)).toMatchObject(output);
 });
