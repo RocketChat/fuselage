9dc5465fb0a2132bc46dfe2e7ae64179cadcff03
dougfabris <devfabris@gmail.com>
2022-10-04 10:09:56 -0300

    fix: review

--- packages/fuselage/src/components/Modal/ModalHeroImage.tsx
+++ packages/fuselage/src/components/Modal/ModalHeroImage.tsx
@@ -5,16 +5,6 @@ import Box from '../Box';
 
 export type ModalHeroImageProps = ComponentProps<typeof Box>;
 
-export const ModalHeroImage = ({ title, ...props }: ModalHeroImageProps) => (
-  <Box
-    rcx-modal__hero-image
-    aria-label={title}
-    title={title}
-    is='img'
-    width='100%'
-    height='auto'
-    maxHeight='x256'
-    mbe='x24'
-    {...props}
-  />
+export const ModalHeroImage = ({ ...props }: ModalHeroImageProps) => (
+  <Box rcx-modal__hero-image is='img' {...props} />
 );
