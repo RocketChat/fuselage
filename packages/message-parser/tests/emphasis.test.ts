d1d5337a9c030175dfb9e086d5893af9f40bf7ad
Hugo Costa <hugocarreiracosta@gmail.com>
2022-10-04 11:31:29 -0300

    fix(message-parser): Link/URL parser issues (#855)
    * feat(fuselage): Adds wordbreak in Box Component (#853)
    
    * Fixing URL/links parsing issues
    
    * restoring files from base branch
    
    * replacing backstick with single quotes
    
    Co-authored-by: Douglas Fabris <devfabris@gmail.com>

--- packages/message-parser/tests/emphasis.test.ts
+++ packages/message-parser/tests/emphasis.test.ts
@@ -16,10 +16,9 @@ test.each([
     [
       paragraph([
         italic([
-          link(
-            'https://gist.github.com/24dddfa97bef58f46ac2ce0f80c58ba4',
-            plain('A brand new Gist')
-          ),
+          link('https://gist.github.com/24dddfa97bef58f46ac2ce0f80c58ba4', [
+            plain('A brand new Gist'),
+          ]),
         ]),
       ]),
     ],
