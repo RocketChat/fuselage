0b5e64b4dd13850fe3bc06d91c95b8003ec3ff74
Filipe Marins <filipemarins123@gmail.com>
2022-10-06 11:04:48 -0300

    refactor: add image preview variation

--- packages/fuselage/src/components/Message/MessageGenericPreview/MessageGenericPreviewImage.spec.tsx
+++ packages/fuselage/src/components/Message/MessageGenericPreview/MessageGenericPreviewImage.spec.tsx
@@ -10,10 +10,15 @@ describe('MessageGenericPreviewImage', () => {
 
   it('should render div with background image', () => {
     render(<MessageGenericPreviewImage url='test' width={200} height={200} />);
+    const previewImage = screen.getByTestId('preview-image');
 
-    expect(screen.getByTestId('preview-image')).toHaveStyle({
+    expect(previewImage).toHaveStyle({
       background: 'test',
     });
+    expect(previewImage).toHaveClass('rcx-message-generic-preview__preview');
+    expect(previewImage).not.toHaveClass(
+      'rcx-message-generic-preview__preview--image'
+    );
   });
 
   it('should render anchor with background image when has external url', () => {
@@ -32,5 +37,24 @@ describe('MessageGenericPreviewImage', () => {
     });
     expect(previewImage).toHaveAttribute('href', 'external');
     expect(previewImage).toHaveAttribute('target', '_blank');
+  });
+
+  it('should render image preview when pass imagePreview', () => {
+    render(
+      <MessageGenericPreviewImage
+        url='test'
+        width={200}
+        height={200}
+        imagePreview
+      />
+    );
+    const previewImage = screen.getByTestId('preview-image');
+
+    expect(previewImage).toHaveStyle({
+      background: 'test',
+    });
+    expect(previewImage).toHaveClass(
+      'rcx-message-generic-preview__preview--image'
+    );
   });
 });
--- packages/fuselage/src/components/Message/MessageGenericPreview/MessageGenericPreviewImage.tsx
+++ packages/fuselage/src/components/Message/MessageGenericPreview/MessageGenericPreviewImage.tsx
@@ -7,6 +7,7 @@ type MessageGenericPreviewImageProps = {
   width: number;
   height: number;
   externalUrl?: string;
+  imagePreview?: boolean;
   className?: string;
 };
 
@@ -15,6 +16,7 @@ export const MessageGenericPreviewImage = ({
   width,
   height,
   externalUrl,
+  imagePreview,
   className,
   ...props
 }: MessageGenericPreviewImageProps) => {
@@ -22,14 +24,22 @@ export const MessageGenericPreviewImage = ({
     'data-testid': 'preview-image',
     'className': prependClassName(
       className,
-      'rcx-message-generic-preview__preview'
+      `rcx-message-generic-preview__preview ${
+        imagePreview && 'rcx-message-generic-preview__preview--image'
+      }`
     ),
     'style': { backgroundImage: `url(${url})`, maxWidth: '100%' },
     'children': <div style={{ paddingTop: `${(height / width) * 100}%` }} />,
   };
 
   return externalUrl ? (
-    <a href={externalUrl} target='_blank' {...componentProps} {...props} />
+    <a
+      href={externalUrl}
+      target='_blank'
+      rel='noopener noreferrer'
+      {...componentProps}
+      {...props}
+    />
   ) : (
     <div {...componentProps} {...props} />
   );
