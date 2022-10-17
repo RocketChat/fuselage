bc30953a2b34ad947e5ca872305a7eaeafa7dd45
Guilherme Gazzo <guilhermegazzo@gmail.com>
2022-10-17 14:28:41 -0300

    chore: __setThrowErrorOnInvalidToken__ (#874)

--- packages/fuselage/src/index.ts
+++ packages/fuselage/src/index.ts
@@ -1,6 +1,6 @@
 export * from './components';
 export * from './styleTokens';
-export { Palette } from './Theme';
+export { Palette, __setThrowErrorOnInvalidToken__ } from './Theme';
 
 if (process.env.VERSION) {
   console.log(`fuselage: ${process.env.VERSION}`);
