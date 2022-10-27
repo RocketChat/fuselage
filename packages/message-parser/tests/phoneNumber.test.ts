7e521bbc4a257fac3bea292f40220419c411fdc6
Hugo Costa <hugocarreiracosta@gmail.com>
2022-10-27 16:18:03 -0300

    feat(message-parser): Enabling Emphasis elements (bold, strike and italic) inside link label (#864)
    * fix: enabling emphasis inside link label
    
    * remove duplicate tests
    
    * replace test
    
    * fix
    
    Co-authored-by: guijun13 <guilhermejun13.grillo@gmail.com>
    Co-authored-by: Guilherme Jun Grillo <48109548+guijun13@users.noreply.github.com>
    Co-authored-by: Tasso Evangelista <tasso.evangelista@rocket.chat>

--- packages/message-parser/tests/phoneNumber.test.ts
+++ packages/message-parser/tests/phoneNumber.test.ts
@@ -4,27 +4,27 @@ import { link, paragraph, plain, bold } from '../src/utils';
 test.each([
   [
     '+07563546725',
-    [paragraph([link('tel:07563546725', plain('+07563546725'))])],
+    [paragraph([link('tel:07563546725', [plain('+07563546725')])])],
   ],
   [
     '+075-63546725',
-    [paragraph([link('tel:07563546725', plain('+075-63546725'))])],
+    [paragraph([link('tel:07563546725', [plain('+075-63546725')])])],
   ],
   [
     '+(075)-63546725',
-    [paragraph([link('tel:07563546725', plain('+(075)-63546725'))])],
+    [paragraph([link('tel:07563546725', [plain('+(075)-63546725')])])],
   ],
   [
     '+(075)63546725',
-    [paragraph([link('tel:07563546725', plain('+(075)63546725'))])],
+    [paragraph([link('tel:07563546725', [plain('+(075)63546725')])])],
   ],
   [
     '[here](+(075)63546725)',
-    [paragraph([link('tel:07563546725', plain('here'))])],
+    [paragraph([link('tel:07563546725', [plain('here')])])],
   ],
   [
     '[**here**](+(075)63546725)',
-    [paragraph([link('tel:07563546725', bold([plain('here')]))])],
+    [paragraph([link('tel:07563546725', [bold([plain('here')])])])],
   ],
 ])('parses %p', (input, output) => {
   expect(parse(input)).toMatchObject(output);
