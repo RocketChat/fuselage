0eb6fcd07e7e802e84098603538e284d4505d471
Filipe Marins <filipemarins123@gmail.com>
2022-11-01 06:02:26 -0300

    refactor: add message system variation

--- packages/fuselage/src/components/Message/ThreadMessage/ThreadMessageOrigin.tsx
+++ packages/fuselage/src/components/Message/ThreadMessage/ThreadMessageOrigin.tsx
@@ -3,11 +3,21 @@ import React from 'react';
 
 type ThreadMessageOriginProps = {
   children?: ReactNode;
+  system?: boolean;
 };
 
-export const ThreadMessageOrigin = (props: ThreadMessageOriginProps) => (
+export const ThreadMessageOrigin = ({
+  children,
+  system,
+}: ThreadMessageOriginProps) => (
   <div
-    className='rcx-box rcx-box--full rcx-message-thread__origin'
-    {...props}
-  />
+    className={[
+      'rcx-box rcx-box--full rcx-message-thread__origin',
+      system && 'rcx-box rcx-box--full rcx-message-thread__origin--system',
+    ]
+      .filter(Boolean)
+      .join(' ')}
+  >
+    {children}
+  </div>
 );
