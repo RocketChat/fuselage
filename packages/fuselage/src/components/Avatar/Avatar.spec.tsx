d56cd95cca20f88adc28666eb9e06a899e187fdf
yash-rajpal <rajpal.yash03@gmail.com>
2022-12-06 01:07:07 +0530

    add unit test

--- packages/fuselage/src/components/Avatar/Avatar.spec.tsx
+++ packages/fuselage/src/components/Avatar/Avatar.spec.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from '@storybook/testing-react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import * as stories from './Avatar.stories';
@@ -9,5 +9,11 @@ const { Default } = composeStories(stories);
 describe('[Avatar Component]', () => {
   it('renders without crashing', () => {
     render(<Default />);
+  });
+
+  it('should render emoji avatar', () => {
+    render(<Default emoji={{ children: '😄', name: '' }} />);
+
+    expect(screen.getAllByText('😄').length).toBeGreaterThanOrEqual(1);
   });
 });
