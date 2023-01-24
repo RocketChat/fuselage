984189eed6434578a11a304a97fcd5cb25cdcebf
dougfabris <devfabris@gmail.com>
2023-01-24 17:59:41 -0300

    chore: Option Skeleton

--- packages/fuselage/src/components/Option/OptionSkeleton.tsx
+++ packages/fuselage/src/components/Option/OptionSkeleton.tsx
@@ -2,17 +2,10 @@ import React from 'react';
 
 import { Skeleton } from '../Skeleton';
 import Option from './Option';
-import OptionAvatar from './OptionAvatar';
-import OptionContent from './OptionContent';
 
 const OptionSkeleton = () => (
   <Option>
-    <OptionAvatar>
-      <Skeleton variant='rect' width={28} height={28} />
-    </OptionAvatar>
-    <OptionContent>
-      <Skeleton width='100%' />
-    </OptionContent>
+    <Skeleton width='100%' />
   </Option>
 );
 
