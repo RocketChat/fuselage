e92418c70377b11577cad27ca8efe319e054ecc4
juliajforesti <juliajforesti@gmail.com>
2023-01-30 12:08:25 -0300

    Replace default calendar icon from InputBox type date with Addon

--- packages/fuselage/src/components/InputBox/InputBox.stories.tsx
+++ packages/fuselage/src/components/InputBox/InputBox.stories.tsx
@@ -43,6 +43,13 @@ Default.args = {
   onChange: action('change'),
 };
 
+export const Date: ComponentStory<typeof InputBox> = Template.bind({});
+Date.args = {
+  defaultValue: 'Value',
+  onChange: action('change'),
+  type: 'date',
+};
+
 export const WithAddon: ComponentStory<typeof InputBox> = Template.bind({});
 WithAddon.args = {
   addon: <Icon name='send' size='x20' />,
