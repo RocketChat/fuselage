b0cac69e7b3ea2736ed1bb17cf12d5657ab1dbaa
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-04-17 21:04:10 -0300

    Upgrade Rollup to overcome circular dependencies bugs

--- packages/mp3-encoder/rollup.config.js
+++ packages/mp3-encoder/rollup.config.js
@@ -1,12 +1,12 @@
-import path from 'path';
+const path = require('path');
 
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+const commonjs = require('@rollup/plugin-commonjs');
+const resolve = require('@rollup/plugin-node-resolve');
+const typescript = require('@rollup/plugin-typescript');
 
-import pkg from './package.json';
+const pkg = require('./package.json');
 
-export default [
+module.exports = [
   {
     input: 'src/index.ts',
     output: {
