0ed5e66ee10990c0a8a646afe53463a111fb55a9
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-04-18 01:54:42 -0300

    Convert `VBRSeekInfo.ts`

--- packages/mp3-encoder/.eslintrc.js
+++ packages/mp3-encoder/.eslintrc.js
@@ -8,6 +8,6 @@ module.exports = {
     'dist',
     'node_modules',
     'src/lame/*.js',
-    '!src/lame/VBRQuantize.js',
+    '!src/lame/VBRSeekInfo.js',
   ],
 };
---
+++ packages/mp3-encoder/src/lame/VBRSeekInfo.ts
@@ -0,0 +1,40 @@
+class VBRSeekInfo {
+  /**
+   * What we have seen so far.
+   */
+  sum = 0;
+
+  /**
+   * How many frames we have seen in this chunk.
+   */
+  seen = 0;
+
+  /**
+   * How many frames we want to collect into one chunk.
+   */
+  want = 0;
+
+  /**
+   * Actual position in our bag.
+   */
+  pos = 0;
+
+  /**
+   * Size of our bag.
+   */
+  size = 0;
+
+  /**
+   * Pointer to our bag.
+   */
+  bag = null;
+
+  nVbrNumFrames = 0;
+
+  nBytesWritten = 0;
+
+  /* VBR tag data */
+  TotalFrameSize = 0;
+}
+
+export default VBRSeekInfo;
