9ba3fac64aa3548afa501fa1f90fbba238bfc3a7
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-05-09 15:31:44 -0300

    fix(fuselage, fuselage-hooks): `Position` (#1045)
    Co-authored-by: Douglas Fabris <devfabris@gmail.com>

--- packages/fuselage/src/components/Position/Position.tsx
+++ packages/fuselage/src/components/Position/Position.tsx
@@ -6,7 +6,7 @@ import type {
   ReactPortal,
   ReactElement,
 } from 'react';
-import { useRef, useMemo, cloneElement, useState } from 'react';
+import { useRef, useMemo, cloneElement, useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 import type Box from '../Box';
@@ -50,6 +50,15 @@ const Position = ({
     document.body.appendChild(element);
     return element;
   });
+
+  useEffect(
+    () => () => {
+      if (portalContainer.childNodes.length === 0) {
+        document.body.removeChild(portalContainer);
+      }
+    },
+    [portalContainer]
+  );
 
   return createPortal(
     cloneElement(children, {
