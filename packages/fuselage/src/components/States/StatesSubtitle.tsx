8558795eca74898844b024efc16901b212366714
Douglas Fabris <devfabris@gmail.com>
2023-05-17 17:35:15 -0300

    chore(fuselage): Change `StatesTitle` to h3 (#1051)

--- packages/fuselage/src/components/States/StatesSubtitle.tsx
+++ packages/fuselage/src/components/States/StatesSubtitle.tsx
@@ -1,12 +1,14 @@
-import type { ReactNode } from 'react';
+import type { AllHTMLAttributes, ReactNode } from 'react';
 import React from 'react';
 
 type StatesSubtitleProps = {
   children?: ReactNode;
-};
+} & AllHTMLAttributes<HTMLDivElement>;
 
-const StatesSubtitle = ({ children }: StatesSubtitleProps) => (
-  <div className='rcx-states__subtitle'>{children}</div>
+const StatesSubtitle = ({ children, ...props }: StatesSubtitleProps) => (
+  <div {...props} className='rcx-states__subtitle'>
+    {children}
+  </div>
 );
 
 export default StatesSubtitle;
--- packages/fuselage/src/components/States/StatesSuggestion.tsx
+++ packages/fuselage/src/components/States/StatesSuggestion.tsx
@@ -1,12 +1,14 @@
-import type { ReactNode } from 'react';
+import type { ReactNode, AllHTMLAttributes } from 'react';
 import React from 'react';
 
 type StatesSuggestionProps = {
   children?: ReactNode;
-};
+} & AllHTMLAttributes<HTMLDivElement>;
 
-const StatesSuggestion = ({ children }: StatesSuggestionProps) => (
-  <div className='rcx-states__suggestion'>{children}</div>
+const StatesSuggestion = ({ children, ...props }: StatesSuggestionProps) => (
+  <div {...props} className='rcx-states__suggestion'>
+    {children}
+  </div>
 );
 
 export default StatesSuggestion;
