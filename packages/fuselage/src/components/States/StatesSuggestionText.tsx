fd14ec4242cad6f938eaef4849e7d24134c8ade4
dougfabris <devfabris@gmail.com>
2023-05-17 16:27:36 -0300

    chore: `StatesTitle` to h3

--- packages/fuselage/src/components/States/StatesSuggestionText.tsx
+++ packages/fuselage/src/components/States/StatesSuggestionText.tsx
@@ -1,12 +1,17 @@
-import type { ReactNode } from 'react';
+import type { AllHTMLAttributes, ReactNode } from 'react';
 import React from 'react';
 
 type StatesSuggestionTextProps = {
   children?: ReactNode;
-};
+} & AllHTMLAttributes<HTMLDivElement>;
 
-const StatesSuggestionText = ({ children }: StatesSuggestionTextProps) => (
-  <div className='rcx-states__suggestion-text'>{children}</div>
+const StatesSuggestionText = ({
+  children,
+  ...props
+}: StatesSuggestionTextProps) => (
+  <div {...props} className='rcx-states__suggestion-text'>
+    {children}
+  </div>
 );
 
 export default StatesSuggestionText;
--- packages/fuselage/src/components/States/StatesTitle.tsx
+++ packages/fuselage/src/components/States/StatesTitle.tsx
@@ -1,12 +1,14 @@
-import type { ReactNode } from 'react';
+import type { AllHTMLAttributes, ReactNode } from 'react';
 import React from 'react';
 
 type StatesTitleProps = {
   children?: ReactNode;
-};
+} & AllHTMLAttributes<HTMLHeadingElement>;
 
-const StatesTitle = ({ children }: StatesTitleProps) => (
-  <div className='rcx-states__title'>{children}</div>
+const StatesTitle = ({ children, ...props }: StatesTitleProps) => (
+  <h3 {...props} className='rcx-states__title'>
+    {children}
+  </h3>
 );
 
 export default StatesTitle;
