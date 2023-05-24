e2f74539bce690f7ec335e07d2cefcf26cfb8c42
Pedro Berleze Rorato <41977327+PedroRorato@users.noreply.github.com>
2023-05-24 00:05:26 -0300

    feat(fuselage): `Contextualbar` component (#749)
    Co-authored-by: dougfabris <devfabris@gmail.com>

---
+++ packages/fuselage/src/components/Contextualbar/ContextualbarSkeleton.tsx
@@ -0,0 +1,25 @@
+import type { ReactElement, ComponentProps } from 'react';
+import React, { memo } from 'react';
+
+import { Contextualbar, ContextualbarHeader } from '.';
+import { Box, Skeleton } from '..';
+
+const ContextualbarSkeleton = (
+  props: ComponentProps<typeof Box>
+): ReactElement => (
+  <Contextualbar {...props} width='100%'>
+    <ContextualbarHeader>
+      <Skeleton width='100%' />
+    </ContextualbarHeader>
+    <Box p='x24'>
+      <Skeleton mbe='x4' width='32px' height='32px' variant='rect' />
+      {Array(5)
+        .fill(5)
+        .map((_, index) => (
+          <Skeleton key={index} />
+        ))}
+    </Box>
+  </Contextualbar>
+);
+
+export default memo(ContextualbarSkeleton);
