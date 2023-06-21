3bea52ae330932e7667903f9ba34d0a2cc4df467
csuarez <carlos.suarez@rocket.chat>
2023-06-21 11:39:11 -0500

    fix: uikit tab component value type

--- packages/ui-kit/src/blocks/elements/TabNavigationElement.ts
+++ packages/ui-kit/src/blocks/elements/TabNavigationElement.ts
@@ -1,8 +1,9 @@
 import type { Actionable } from '../Actionable';
+import { TextObject } from '../TextObject';
 
 export type TabItem = {
   value: string;
-  text: string;
+  text: TextObject;
   disabled?: boolean;
   selected?: boolean;
 };
