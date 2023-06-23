687f1d6e23ef64d525d63f29058b256719ec7103
juliajforesti <juliajforesti@gmail.com>
2023-06-23 15:22:03 -0300

    ManyV2 forward className

--- packages/fuselage/src/components/Menu/V2/Menu.tsx
+++ packages/fuselage/src/components/Menu/V2/Menu.tsx
@@ -1,5 +1,5 @@
 import type { UsePositionOptions } from '@rocket.chat/fuselage-hooks';
-import type { ComponentProps } from 'react';
+import type { ComponentProps, ElementType } from 'react';
 import React, { useRef } from 'react';
 import type { AriaMenuProps } from 'react-aria';
 import {
@@ -24,11 +24,18 @@ interface MenuButtonProps<T> extends AriaMenuProps<T>, MenuTriggerProps {
   mini?: boolean;
   placement?: UsePositionOptions['placement'];
   title?: string;
+  /**
+   * A component that renders an IconButton
+   */
+  is?: ElementType;
+  className?: string;
 }
 const Menu = <T extends object>({
   icon = 'kebab',
   placement = 'bottom-start',
   title,
+  is: MenuButton = IconButton,
+  className,
   ...props
 }: MenuButtonProps<T>) => {
   const state = useMenuTriggerState(props);
@@ -51,11 +58,12 @@ const Menu = <T extends object>({
 
   return (
     <>
-      <IconButton
+      <MenuButton
         icon={icon}
         ref={trigger}
         small
         title={title}
+        className={className}
         {...mergeProps(pressProps)}
       />
       {state.isOpen && (
