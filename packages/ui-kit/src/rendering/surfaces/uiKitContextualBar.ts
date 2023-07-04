a5a5b1cb5f007849bc12a82055ac676f4a6db5d3
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-07-04 11:54:21 -0300

    Update call signature of surface renderers

--- packages/ui-kit/src/rendering/surfaces/uiKitContextualBar.ts
+++ packages/ui-kit/src/rendering/surfaces/uiKitContextualBar.ts
@@ -1,7 +1,5 @@
 import { createSurfaceRenderer } from '../createSurfaceRenderer';
 import type { ContextualBarSurfaceLayout } from './UiKitParserContextualBar';
 
-export const uiKitContextualBar = createSurfaceRenderer<
-  unknown,
-  ContextualBarSurfaceLayout[number]
->();
+export const uiKitContextualBar =
+  createSurfaceRenderer<ContextualBarSurfaceLayout[number]>();
