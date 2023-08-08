a88a8d5ea710a6da2530e604e91121d62ee64943
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-08-08 16:25:05 -0300

    ..

--- packages/fuselage/src/index.ts
+++ packages/fuselage/src/index.ts
@@ -4,7 +4,3 @@ export * from './components';
 export * from './styleTokens';
 
 export { Palette, __setThrowErrorOnInvalidToken__ } from './Theme';
-
-if (process.env.VERSION) {
-  console.log(`fuselage: ${process.env.VERSION}`);
-}
