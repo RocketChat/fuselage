c8279fced5abba0312c368fdc1752a3cfa26d53f
juliajforesti <juliajforesti@gmail.com>
2023-08-09 15:42:11 -0300

    replace tokens on Themes and styleTokens

--- packages/fuselage/src/Theme.ts
+++ packages/fuselage/src/Theme.ts
@@ -40,95 +40,91 @@ export const neutral = {
   n600: new Var('neutral-600', tokenColors.n600),
   n700: new Var('neutral-700', tokenColors.n700),
   n800: new Var('neutral-800', tokenColors.n800),
-  n850: new Var('neutral-850', tokenColors.n850),
   n900: new Var('neutral-900', tokenColors.n900),
 };
 
 const primary = {
-  p100: new Var('primary-100', tokenColors.p100),
-  p200: new Var('primary-200', tokenColors.p200),
-  p300: new Var('primary-300', tokenColors.p300),
-  p400: new Var('primary-400', tokenColors.p400),
-  p500: new Var('primary-500', tokenColors.p500),
-  p550: new Var('primary-550', tokenColors.p550),
-  p600: new Var('primary-600', tokenColors.p600),
-  p700: new Var('primary-700', tokenColors.p700),
-  p800: new Var('primary-800', tokenColors.p800),
-  p900: new Var('primary-900', tokenColors.p900),
+  p100: new Var('primary-100', tokenColors.b100),
+  p200: new Var('primary-200', tokenColors.b200),
+  p300: new Var('primary-300', tokenColors.b300),
+  p400: new Var('primary-400', tokenColors.b400),
+  p500: new Var('primary-500', tokenColors.b500),
+  p600: new Var('primary-600', tokenColors.b600),
+  p700: new Var('primary-700', tokenColors.b700),
+  p800: new Var('primary-800', tokenColors.b800),
+  p900: new Var('primary-900', tokenColors.b900),
 };
 
-const info = {
-  i100: new Var('info-100', tokenColors.i100),
-  i200: new Var('info-200', tokenColors.i200),
-  i300: new Var('info-300', tokenColors.i300),
-  i400: new Var('info-400', tokenColors.i400),
-  i500: new Var('info-500', tokenColors.i500),
-  i600: new Var('info-600', tokenColors.i600),
-  i700: new Var('info-700', tokenColors.i700),
-  i800: new Var('info-800', tokenColors.i800),
-  i900: new Var('info-900', tokenColors.i900),
-};
+// const info = {
+//   i100: new Var('info-100', tokenColors.i100),
+//   i200: new Var('info-200', tokenColors.i200),
+//   i300: new Var('info-300', tokenColors.i300),
+//   i400: new Var('info-400', tokenColors.i400),
+//   i500: new Var('info-500', tokenColors.i500),
+//   i600: new Var('info-600', tokenColors.i600),
+//   i700: new Var('info-700', tokenColors.i700),
+//   i800: new Var('info-800', tokenColors.i800),
+//   i900: new Var('info-900', tokenColors.i900),
+// };
 
 const success = {
-  s100: new Var('success-100', tokenColors.s100),
-  s200: new Var('success-200', tokenColors.s200),
-  s300: new Var('success-300', tokenColors.s300),
-  s400: new Var('success-400', tokenColors.s400),
-  s500: new Var('success-500', tokenColors.s500),
-  s600: new Var('success-600', tokenColors.s600),
-  s700: new Var('success-700', tokenColors.s700),
-  s800: new Var('success-800', tokenColors.s800),
-  s900: new Var('success-900', tokenColors.s900),
+  s100: new Var('success-100', tokenColors.g100),
+  s200: new Var('success-200', tokenColors.g200),
+  s300: new Var('success-300', tokenColors.g300),
+  s400: new Var('success-400', tokenColors.g400),
+  s500: new Var('success-500', tokenColors.g500),
+  s600: new Var('success-600', tokenColors.g600),
+  s700: new Var('success-700', tokenColors.g700),
+  s800: new Var('success-800', tokenColors.g800),
+  s900: new Var('success-900', tokenColors.g900),
 };
 
 const warning = {
-  w100: new Var('warning-100', tokenColors.w100),
-  w150: new Var('warning-150', tokenColors.w150),
-  w200: new Var('warning-200', tokenColors.w200),
-  w300: new Var('warning-300', tokenColors.w300),
-  w400: new Var('warning-400', tokenColors.w400),
-  w500: new Var('warning-500', tokenColors.w500),
-  w600: new Var('warning-600', tokenColors.w600),
-  w700: new Var('warning-700', tokenColors.w700),
-  w800: new Var('warning-800', tokenColors.w800),
-  w900: new Var('warning-900', tokenColors.w900),
+  w100: new Var('warning-100', tokenColors.y100),
+  w200: new Var('warning-200', tokenColors.y200),
+  w300: new Var('warning-300', tokenColors.y300),
+  w400: new Var('warning-400', tokenColors.y400),
+  w500: new Var('warning-500', tokenColors.y500),
+  w600: new Var('warning-600', tokenColors.y600),
+  w700: new Var('warning-700', tokenColors.y700),
+  w800: new Var('warning-800', tokenColors.y800),
+  w900: new Var('warning-900', tokenColors.y900),
 };
 
 const danger = {
-  d100: new Var('danger-100', tokenColors.d100),
-  d200: new Var('danger-200', tokenColors.d200),
-  d300: new Var('danger-300', tokenColors.d300),
-  d400: new Var('danger-400', tokenColors.d400),
-  d500: new Var('danger-500', tokenColors.d500),
-  d550: new Var('danger-550', tokenColors.d550),
-  d600: new Var('danger-600', tokenColors.d600),
-  d700: new Var('danger-700', tokenColors.d700),
-  d800: new Var('danger-800', tokenColors.d800),
-  d900: new Var('danger-900', tokenColors.d900),
+  d100: new Var('danger-100', tokenColors.r100),
+  d200: new Var('danger-200', tokenColors.r200),
+  d300: new Var('danger-300', tokenColors.r300),
+  d400: new Var('danger-400', tokenColors.r400),
+  d500: new Var('danger-500', tokenColors.r500),
+  d600: new Var('danger-600', tokenColors.r600),
+  d700: new Var('danger-700', tokenColors.r700),
+  d800: new Var('danger-800', tokenColors.r800),
+  d900: new Var('danger-900', tokenColors.r900),
 };
 
 const service1 = {
-  100: new Var('service-1-100', tokenColors['s1-100']),
-  200: new Var('service-1-200', tokenColors['s1-200']),
-  300: new Var('service-1-300', tokenColors['s1-300']),
-  400: new Var('service-1-400', tokenColors['s1-400']),
-  500: new Var('service-1-500', tokenColors['s1-500']),
-  600: new Var('service-1-600', tokenColors['s1-600']),
-  700: new Var('service-1-700', tokenColors['s1-700']),
-  800: new Var('service-1-800', tokenColors['s1-800']),
-  900: new Var('service-1-900', tokenColors['s1-900']),
+  100: new Var('service-1-100', tokenColors.o100),
+  200: new Var('service-1-200', tokenColors.o200),
+  300: new Var('service-1-300', tokenColors.o300),
+  400: new Var('service-1-400', tokenColors.o400),
+  500: new Var('service-1-500', tokenColors.o500),
+  600: new Var('service-1-600', tokenColors.o600),
+  700: new Var('service-1-700', tokenColors.o700),
+  800: new Var('service-1-800', tokenColors.o800),
+  900: new Var('service-1-900', tokenColors.o900),
 };
 
 const service2 = {
-  100: new Var('service-2-100', tokenColors['s2-100']),
-  200: new Var('service-2-200', tokenColors['s2-200']),
-  300: new Var('service-2-300', tokenColors['s2-300']),
-  400: new Var('service-2-400', tokenColors['s2-400']),
-  500: new Var('service-2-500', tokenColors['s2-500']),
-  600: new Var('service-2-600', tokenColors['s2-600']),
-  700: new Var('service-2-700', tokenColors['s2-700']),
-  800: new Var('service-2-800', tokenColors['s2-800']),
-  900: new Var('service-2-900', tokenColors['s2-900']),
+  100: new Var('service-2-100', tokenColors.p100),
+  200: new Var('service-2-200', tokenColors.p200),
+  300: new Var('service-2-300', tokenColors.p300),
+  400: new Var('service-2-400', tokenColors.p400),
+  500: new Var('service-2-500', tokenColors.p500),
+  600: new Var('service-2-600', tokenColors.p600),
+  700: new Var('service-2-700', tokenColors.p700),
+  800: new Var('service-2-800', tokenColors.p800),
+  900: new Var('service-2-900', tokenColors.p900),
 };
 
 export const surfaceColors = {
@@ -142,7 +138,7 @@ export const surfaceColors = {
   'surface-dark': neutral.n800.theme('surface-dark'),
   'surface-featured': service2['700'].theme('surface-featured'),
   'surface-featured-hover': service2['800'].theme('surface-featured-hover'),
-  'surface-overlay': neutral.n850.theme('surface-overlay'),
+  'surface-overlay': neutral.n800.theme('surface-overlay'),
   'surface-transparent': 'transparent',
 };
 
@@ -181,11 +177,11 @@ export const textIconColors = {
 type TextIconColors = keyof typeof textIconColors;
 
 export const statusBackgroundColors = {
-  'status-background-info': info.i200.theme('status-background-info'),
+  'status-background-info': primary.p200.theme('status-background-info'),
   'status-background-success': success.s200.theme('status-background-success'),
   'status-background-danger': danger.d200.theme('status-background-danger'),
   'status-background-warning': warning.w200.theme('status-background-warning'),
-  'status-background-warning-2': warning.w150.theme(
+  'status-background-warning-2': warning.w100.theme(
     'status-background-warning-2'
   ),
   'status-background-service-1': service1['200'].theme(
@@ -199,7 +195,7 @@ export const statusBackgroundColors = {
 type StatusBackgroundColors = keyof typeof statusBackgroundColors;
 
 export const statusColors = {
-  'status-font-on-info': info.i600.theme('status-font-on-info'),
+  'status-font-on-info': primary.p600.theme('status-font-on-info'),
   'status-font-on-success': success.s800.theme('status-font-on-success'),
   'status-font-on-warning': warning.w800.theme('status-font-on-warning'),
   'status-font-on-warning-2': neutral.n800.theme('status-font-on-warning-2'),
@@ -213,9 +209,9 @@ type StatusColors = keyof typeof statusColors;
 export const badgeBackgroundColors = {
   'badge-background-level-0': neutral.n400.theme('badge-background-level-0'),
   'badge-background-level-1': neutral.n600.theme('badge-background-level-1'),
-  'badge-background-level-2': primary.p550.theme('badge-background-level-2'),
+  'badge-background-level-2': primary.p500.theme('badge-background-level-2'),
   'badge-background-level-3': service1[500].theme('badge-background-level-3'),
-  'badge-background-level-4': danger.d550.theme('badge-background-level-4'),
+  'badge-background-level-4': danger.d500.theme('badge-background-level-4'),
 };
 
 type BadgeBackgroundColors = keyof typeof badgeBackgroundColors;
