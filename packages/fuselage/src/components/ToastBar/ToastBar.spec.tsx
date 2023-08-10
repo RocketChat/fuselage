3746a5c5cb1f440ad07527f3a99f9601e480e760
dougfabris <devfabris@gmail.com>
2023-08-10 13:04:02 -0300

    chore: a11y tweaks

--- packages/fuselage/src/components/ToastBar/ToastBar.spec.tsx
+++ packages/fuselage/src/components/ToastBar/ToastBar.spec.tsx
@@ -1,10 +1,20 @@
 import { render } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import React from 'react';
 
 import { ToastBar } from '.';
 
+expect.extend(toHaveNoViolations);
+
 describe('[ToastBar Component]', () => {
   it('renders without crashing', () => {
     render(<ToastBar />);
+  });
+
+  it('should have no a11y violations', async () => {
+    const { container } = render(<ToastBar />);
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
