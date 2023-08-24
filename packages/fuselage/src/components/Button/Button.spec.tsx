5d6475bf8d155b4cc8c26cd0d97bad81f4d5c5b4
juliajforesti <juliajforesti@gmail.com>
2023-08-24 16:23:57 -0300

    chore: a11y button tests

--- packages/fuselage/src/components/Button/Button.spec.tsx
+++ packages/fuselage/src/components/Button/Button.spec.tsx
@@ -1,10 +1,15 @@
 import { composeStories } from '@storybook/testing-react';
 import { render } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import React from 'react';
 
 import * as stories from './Button.stories';
+import * as iconButtonStories from './IconButton.stories';
 
 const { Default, AsIconButton } = composeStories(stories);
+const { _IconButton, _IconButtonInfo, _IconButtonSuccess } =
+  composeStories(iconButtonStories);
+expect.extend(toHaveNoViolations);
 
 describe('[Button Component]', () => {
   it('renders Button without crashing', () => {
@@ -13,5 +18,39 @@ describe('[Button Component]', () => {
 
   it('renders ActionButton without crashing', () => {
     render(<AsIconButton />);
+  });
+
+  it('should have no a11y violations', async () => {
+    const { container } = render(<Default />);
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
+
+describe('[IconButton Component]', () => {
+  it('renders IconButton without crashing', () => {
+    render(<AsIconButton />);
+  });
+
+  it('IconButton default should have no a11y violations', async () => {
+    const { container } = render(<_IconButton />);
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('IconButtonInfo should have no a11y violations', async () => {
+    const { container } = render(<_IconButtonInfo />);
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('IconButtonSuccess should have no a11y violations', async () => {
+    const { container } = render(<_IconButtonSuccess />);
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
--- packages/fuselage/src/components/ButtonGroup/ButtonGroup.spec.tsx
+++ packages/fuselage/src/components/ButtonGroup/ButtonGroup.spec.tsx
@@ -1,13 +1,22 @@
 import { composeStories } from '@storybook/testing-react';
 import { render } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import React from 'react';
 
 import * as stories from './ButtonGroup.stories';
 
 const { Default } = composeStories(stories);
+expect.extend(toHaveNoViolations);
 
 describe('[ButtonGroup Component]', () => {
   it('renders without crashing', () => {
     render(<Default />);
+  });
+
+  it('should have no a11y violations', async () => {
+    const { container } = render(<Default />);
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
