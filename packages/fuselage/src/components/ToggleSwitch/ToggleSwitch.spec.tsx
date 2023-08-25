24f51e02ae1ad8d3eb5341439403d1c9a160cf91
Douglas Fabris <devfabris@gmail.com>
2023-08-25 19:29:36 -0300

    chore(fuselage): `ToggleSwitch` best practices adjustments (#1146)

--- packages/fuselage/src/components/ToggleSwitch/ToggleSwitch.spec.tsx
+++ packages/fuselage/src/components/ToggleSwitch/ToggleSwitch.spec.tsx
@@ -1,10 +1,33 @@
+import { composeStories } from '@storybook/testing-react';
 import { render } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import React from 'react';
 
-import { ToggleSwitch } from '.';
+import * as stories from './ToggleSwitch.stories';
 
-describe('[ToggleSwitch Component]', () => {
-  it('renders without crashing', () => {
-    render(<ToggleSwitch />);
-  });
+expect.extend(toHaveNoViolations);
+
+const testCases = Object.values(composeStories(stories)).map((Story) => [
+  Story.storyName || 'Story',
+  Story,
+]);
+
+describe('[ToggleSwitch Rendering]', () => {
+  test.each(testCases)(
+    `renders %s without crashing`,
+    async (_storyname, Story) => {
+      const tree = render(<Story />);
+      expect(tree.baseElement).toMatchSnapshot();
+    }
+  );
+
+  test.each(testCases)(
+    '%s should have no a11y violations',
+    async (_storyname, Story) => {
+      const { container } = render(<Story />);
+
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    }
+  );
 });
--- packages/fuselage/src/components/ToggleSwitch/ToggleSwitch.stories.tsx
+++ packages/fuselage/src/components/ToggleSwitch/ToggleSwitch.stories.tsx
@@ -1,40 +1,27 @@
 import { action } from '@storybook/addon-actions';
-import {
-  Title,
-  Description,
-  Primary,
-  Stories,
-  ArgsTable,
-} from '@storybook/addon-docs';
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
 import React from 'react';
 
-import { ToggleSwitch } from '../..';
-import { PropsVariationSection } from '../../../.storybook/helpers';
+import { ToggleSwitch, Label } from '../..';
+import { DECORATOR_ID, DECORATOR_LABEL } from '../../../.storybook/helpers';
 
 export default {
   title: 'Inputs/ToggleSwitch',
   component: ToggleSwitch,
-  parameters: {
-    docs: {
-      description: {
-        component: 'An input for multi-line plain-text editing.',
-      },
-      page: () => (
-        <>
-          <Title />
-          <Description />
-          <Primary />
-          <ArgsTable />
-          <Stories title={''} />
-        </>
-      ),
-    },
-  },
+  decorators: [
+    (Story) => (
+      <>
+        <Story />
+        <Label mis='x4' htmlFor={DECORATOR_ID}>
+          {DECORATOR_LABEL}
+        </Label>
+      </>
+    ),
+  ],
 } as ComponentMeta<typeof ToggleSwitch>;
 
 const Template: ComponentStory<typeof ToggleSwitch> = (args) => (
-  <ToggleSwitch {...args} />
+  <ToggleSwitch id={DECORATOR_ID} {...args} />
 );
 
 export const Default: ComponentStory<typeof ToggleSwitch> = Template.bind({});
@@ -52,21 +39,3 @@ export const Disabled: ComponentStory<typeof ToggleSwitch> = Template.bind({});
 Disabled.args = {
   disabled: true,
 };
-
-export const States: ComponentStory<typeof ToggleSwitch> = () => (
-  <PropsVariationSection
-    component={ToggleSwitch}
-    common={{ onChange: action('change') }}
-    xAxis={{
-      checked: { checked: true },
-      unchecked: { checked: false },
-    }}
-    yAxis={{
-      default: {},
-      hover: { className: 'is-hovered' },
-      active: { className: 'is-active' },
-      focus: { className: 'is-focused' },
-      disabled: { disabled: true },
-    }}
-  />
-);
