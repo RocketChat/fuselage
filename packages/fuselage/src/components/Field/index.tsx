a7d9cbefcb246431781bf40756b779353fec5fd2
juliajforesti <juliajforesti@gmail.com>
2023-08-31 16:24:50 -0300

    fix: Field deprecation message only for assigned components

--- packages/fuselage/src/components/Field/index.tsx
+++ packages/fuselage/src/components/Field/index.tsx
@@ -16,14 +16,34 @@ export {
   FieldRow,
 };
 
-/**
- * @deprecated Avoid default usage, use named imports instead
- */
 export default Object.assign(Field, {
+  /**
+   * @deprecated Use named import `FieldLabel` instead
+   */
   Label: FieldLabel,
+  /**
+      * @deprecated Use named import `FieldDescription` instead
+
+   */
   Description: FieldDescription,
+  /**
+      * @deprecated Use named import `FieldRow` instead
+
+   */
   Row: FieldRow,
+  /**
+      * @deprecated Use named import `FieldError` instead
+
+   */
   Error: FieldError,
+  /**
+      * @deprecated Use named import `FieldHint` instead
+
+   */
   Hint: FieldHint,
+  /**
+      * @deprecated Use named import `FieldLink` instead
+
+   */
   Link: FieldLink,
 });
