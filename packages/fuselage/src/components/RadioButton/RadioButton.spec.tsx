7d7a7c76003a32593c25758db2b9bc968de8f487
juliajforesti <juliajforesti@gmail.com>
2023-09-11 19:14:53 -0300

    tests

--- packages/fuselage/src/components/RadioButton/RadioButton.spec.tsx
+++ packages/fuselage/src/components/RadioButton/RadioButton.spec.tsx
@@ -1,10 +1,67 @@
+import { composeStories } from '@storybook/testing-react';
 import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
 import React from 'react';
 
-import { RadioButton } from '.';
+import * as stories from './RadioButton.stories';
 
-describe('RadioButton Component', () => {
-  it('renders without crashing', () => {
-    render(<RadioButton />);
+const testCases = Object.values(composeStories(stories)).map((Story) => [
+  Story.storyName || 'Story',
+  Story,
+]);
+
+const { Default, Checked, Disabled } = composeStories(stories);
+
+describe('[RadioButton Rendering]', () => {
+  test.each(testCases)(
+    `renders %s without crashing`,
+    async (_storyname, Story) => {
+      const tree = render(<Story />);
+      expect(tree.baseElement).toMatchSnapshot();
+    }
+  );
+
+  test.each(testCases)(
+    '%s should have no a11y violations',
+    async (_storyname, Story) => {
+      const { container } = render(<Story />);
+
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    }
+  );
+});
+
+describe('[RadioButton Interacting]', () => {
+  it('changes style of element as radio button is checked', () => {
+    const { container } = render(<Default />);
+    const radioButton = container.querySelector(
+      'input[type="radio"]'
+    ) as HTMLInputElement;
+    radioButton.click();
+    expect(radioButton.checked).toEqual(true);
+  });
+  it('changes style of element as radio button is checked/unchecked', () => {
+    const { container } = render(<Default />);
+    const radioButton = container.querySelector(
+      'input[type="radio"]'
+    ) as HTMLInputElement;
+    radioButton.click();
+    radioButton.click();
+    expect(radioButton.checked).toEqual(false);
+  });
+  it('displays radio button with defaultChecked value correctly', () => {
+    const { container } = render(<Checked />);
+    const radioButton = container.querySelector(
+      'input[type="radio"]'
+    ) as HTMLInputElement;
+    expect(radioButton.defaultChecked).toEqual(true);
+  });
+  it('displays radio button disabled correctly', () => {
+    const { container } = render(<Disabled />);
+    const radioButton = container.querySelector(
+      'input[type="radio"]'
+    ) as HTMLInputElement;
+    expect(radioButton.disabled).toEqual(true);
   });
 });
