2c25e7575c76c44db33a63c17c437b09ab286d19
Tasso Evangelista <tasso.evangelista@rocket.chat>
2023-09-15 16:00:43 -0300

    fix(fuselage): Memory leak in `PaginatedMultiSelect` (#1169)

---
+++ packages/fuselage/src/components/PaginatedSelect/PaginatedMultiSelectFiltered.tsx
@@ -0,0 +1,74 @@
+import type { ComponentProps, Ref, FormEvent, ReactElement } from 'react';
+import React, { useCallback, forwardRef } from 'react';
+
+import type Box from '../Box';
+import Flex from '../Flex';
+import { InputBox } from '../InputBox';
+import { type OptionsPaginated } from '../OptionsPaginated';
+import PaginatedMultiSelect, {
+  type PaginatedMultiSelectOption,
+} from './PaginatedMultiSelect';
+
+type PaginatedMultiSelectFilteredProps = Omit<
+  ComponentProps<typeof Box>,
+  'onChange' | 'value' | 'filter'
+> & {
+  error?: boolean;
+  options: PaginatedMultiSelectOption[];
+  withTitle?: boolean;
+  placeholder?: string;
+  endReached?: (start?: number, end?: number) => void;
+  value?: PaginatedMultiSelectOption[];
+  onChange: (values: PaginatedMultiSelectOption[]) => void;
+  renderOptions?: (
+    props: ComponentProps<typeof OptionsPaginated>
+  ) => ReactElement | null;
+  anchor?: any;
+  filter?: string;
+  setFilter?: (value: string) => void;
+};
+
+export const PaginatedMultiSelectFiltered = ({
+  filter,
+  setFilter,
+  options,
+  placeholder,
+  ...props
+}: PaginatedMultiSelectFilteredProps) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const anchor = useCallback(
+    forwardRef(
+      (
+        {
+          children: _children,
+          filter,
+          ...props
+        }: ComponentProps<typeof InputBox>,
+        ref: Ref<HTMLInputElement>
+      ) => (
+        <Flex.Item grow={1}>
+          <InputBox.Input
+            ref={ref}
+            placeholder={placeholder}
+            value={filter}
+            onInput={(e: FormEvent<HTMLInputElement>) =>
+              setFilter?.(e.currentTarget.value)
+            }
+            {...props}
+            rcx-input-box--undecorated
+          />
+        </Flex.Item>
+      )
+    ),
+    []
+  );
+
+  return (
+    <PaginatedMultiSelect
+      filter={filter}
+      options={options}
+      {...props}
+      anchor={anchor}
+    />
+  );
+};
