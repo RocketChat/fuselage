f078758c7d96690d5e355326a3129fa4e1acea94
juliajforesti <juliajforesti@gmail.com>
2023-11-03 12:58:15 -0300

    feat: `FieldLabelInfo` component

--- packages/fuselage/src/components/Field/FieldLabelInfo.tsx
+++ packages/fuselage/src/components/Field/FieldLabelInfo.tsx
@@ -2,21 +2,24 @@ import type { ComponentPropsWithoutRef } from 'react';
 import React from 'react';
 
 import WithErrorWrapper from '../../helpers/WithErrorWrapper';
-import Box from '../Box';
-import { Label } from '../Label';
+import type { Icon } from '../Icon';
+import { LabelInfo } from '../Label/LabelInfo';
 import { FieldContext } from './Field';
 
-type FieldLabelProps = ComponentPropsWithoutRef<typeof Label>;
+type FieldLabelInfoProps = {
+  title: string;
+  id: string;
+} & Omit<ComponentPropsWithoutRef<typeof Icon>, 'name'>;
 
-export const FieldLabel = (props: FieldLabelProps) => {
-  const component = <Box is={Label} rcx-field__label {...props} />;
+export const FieldLabelInfo = (props: FieldLabelInfoProps) => {
+  const component = <LabelInfo {...props} />;
 
   if (process.env.NODE_ENV === 'development') {
     return (
       <WithErrorWrapper
         context={FieldContext}
         parentComponent='Field'
-        componentName={FieldLabel.name}
+        componentName={FieldLabelInfo.name}
       >
         {component}
       </WithErrorWrapper>
