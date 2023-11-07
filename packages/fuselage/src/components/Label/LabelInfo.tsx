73bd59458bee698ec70ceb28304e77dfb5034400
juliajforesti <juliajforesti@gmail.com>
2023-11-07 16:39:47 -0300

    chore: reorder info item with display flex

--- packages/fuselage/src/components/Label/LabelInfo.tsx
+++ packages/fuselage/src/components/Label/LabelInfo.tsx
@@ -10,7 +10,7 @@ type LabelInfoProps = {
 } & Omit<ComponentProps<typeof Icon>, 'name'>;
 
 export const LabelInfo = ({ title, id, ...props }: LabelInfoProps) => (
-  <Box is='span' mi={4} rcx-label__info>
+  <Box is='span' mi={2} rcx-label__info>
     <span hidden id={id}>
       {title}
     </span>
