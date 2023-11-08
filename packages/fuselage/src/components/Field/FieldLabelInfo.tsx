68674c502014e31b15cb81ab85d62677814a60fd
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2023-11-08 09:28:29 -0300

    feat(fuselage): create `LabelInfo` component (#1214)

--- packages/fuselage/src/components/Field/FieldLabelInfo.tsx
+++ packages/fuselage/src/components/Field/FieldLabelInfo.tsx
@@ -1,22 +1,21 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import type { ComponentProps } from 'react';
 import React from 'react';
 
 import WithErrorWrapper from '../../helpers/WithErrorWrapper';
-import Box from '../Box';
-import { Label } from '../Label';
+import { LabelInfo } from '../Label/LabelInfo';
 import { FieldContext } from './Field';
 
-type FieldLabelProps = ComponentPropsWithoutRef<typeof Label>;
+type FieldLabelInfoProps = ComponentProps<typeof LabelInfo>;
 
-export const FieldLabel = (props: FieldLabelProps) => {
-  const component = <Box is={Label} rcx-field__label {...props} />;
+export const FieldLabelInfo = (props: FieldLabelInfoProps) => {
+  const component = <LabelInfo {...props} />;
 
   if (process.env.NODE_ENV === 'development') {
     return (
       <WithErrorWrapper
         context={FieldContext}
         parentComponent='Field'
-        componentName={FieldLabel.name}
+        componentName={FieldLabelInfo.name}
       >
         {component}
       </WithErrorWrapper>
---
+++ packages/fuselage/src/components/Label/LabelInfo.tsx
@@ -0,0 +1,19 @@
+import type { ComponentProps } from 'react';
+import React from 'react';
+
+import Box from '../Box/Box';
+import { Icon } from '../Icon';
+
+type LabelInfoProps = {
+  title: string;
+  id?: string;
+} & Omit<ComponentProps<typeof Icon>, 'name'>;
+
+export const LabelInfo = ({ title, id, ...props }: LabelInfoProps) => (
+  <Box is='span' mi={2} rcx-label__info>
+    <span hidden id={id}>
+      {title}
+    </span>
+    <Icon {...props} name='info-circled' title={title} />
+  </Box>
+);
