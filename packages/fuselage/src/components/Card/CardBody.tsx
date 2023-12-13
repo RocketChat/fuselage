71db7dac3e1d51148ccd5b7fc182ce9f2fc1f3c0
juliajforesti <juliajforesti@gmail.com>
2023-12-13 09:59:40 -0300

    chore: `CardBody` props

--- packages/fuselage/src/components/Card/CardBody.tsx
+++ packages/fuselage/src/components/Card/CardBody.tsx
@@ -1,4 +1,4 @@
-import type { CSSProperties, ComponentProps } from 'react';
+import type { AllHTMLAttributes, CSSProperties, ComponentProps } from 'react';
 import React from 'react';
 
 import Box from '../Box/Box';
@@ -7,12 +7,13 @@ type CardBodyProps = {
   flexDirection?: CSSProperties['flexDirection'];
   height?: ComponentProps<typeof Box>['height'];
   children: React.ReactNode;
-};
+} & Omit<AllHTMLAttributes<HTMLElement>, 'is'>;
 
 const CardBody = ({
   children,
   flexDirection = 'row',
   height,
+  ...props
 }: CardBodyProps) => (
   <Box
     fontScale='p2m'
@@ -21,6 +22,7 @@ const CardBody = ({
     flexGrow={1}
     height={height}
     rcx-card__body
+    {...props}
   >
     {children}
   </Box>
