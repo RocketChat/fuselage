86d97419ab094e73bbbe4861705850c46bc55878
juliajforesti <juliajforesti@gmail.com>
2024-01-12 15:54:21 -0300

    chore: HTML attributes props

--- packages/fuselage/src/components/Bubble/Bubble.tsx
+++ packages/fuselage/src/components/Bubble/Bubble.tsx
@@ -1,5 +1,5 @@
 import type { Keys as IconName } from '@rocket.chat/icons';
-import type { ButtonHTMLAttributes, ReactNode } from 'react';
+import type { AllHTMLAttributes, ButtonHTMLAttributes, ReactNode } from 'react';
 import React from 'react';
 
 import { Icon } from '../Icon';
@@ -9,21 +9,26 @@ type BubbleProps = {
   onClick: () => void;
   icon?: IconName;
   onDismiss?: () => void;
-} & ButtonHTMLAttributes<HTMLButtonElement>;
+  contentProps?: ButtonHTMLAttributes<HTMLButtonElement>;
+  dismissProps?: ButtonHTMLAttributes<HTMLButtonElement>;
+} & AllHTMLAttributes<HTMLDivElement>;
 
 const Bubble = ({
   children,
   onClick,
   icon,
   onDismiss,
+  contentProps,
+  dismissProps,
   ...props
 }: BubbleProps) => (
   <div
     className={`rcx-bubble rcx-box rcx-box--full ${
       onDismiss ? 'rcx-bubble--group' : ''
     }`}
+    {...props}
   >
-    <button className='rcx-bubble--content' onClick={onClick} {...props}>
+    <button className='rcx-bubble--content' onClick={onClick} {...contentProps}>
       {icon && <Icon name={icon} size='x16' />}
       <span>{children}</span>
     </button>
@@ -32,7 +37,7 @@ const Bubble = ({
         aria-label={`Dismiss ${children}`}
         className='rcx-bubble--dismiss'
         onClick={onClick}
-        {...props}
+        {...dismissProps}
       >
         <Icon
           name='cross-small'
