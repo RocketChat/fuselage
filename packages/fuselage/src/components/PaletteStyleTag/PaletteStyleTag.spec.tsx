2cbdcee30f43baf2630792d714b7d1e822395498
juliajforesti <juliajforesti@gmail.com>
2024-02-21 18:03:32 -0300

    chore: update tests

--- packages/fuselage/src/components/PaletteStyleTag/PaletteStyleTag.spec.tsx
+++ packages/fuselage/src/components/PaletteStyleTag/PaletteStyleTag.spec.tsx
@@ -1,38 +1,25 @@
-import { composeStories } from '@storybook/testing-react';
 import { render } from '@testing-library/react';
 import React from 'react';
 
-import * as stories from './PaletteStyleTag.stories';
+import PaletteStyleTag from './PaletteStyleTag';
 import { dark } from './lib/paletteDark';
 import { light } from './lib/paletteLight';
 
-const testCases = Object.values(composeStories(stories)).map((Story) => [
-  Story.storyName || 'Story',
-  Story,
-]);
-
 describe('[PaletteStyleTag Rendering]', () => {
-  test.each(testCases)(
-    `renders %s without crashing`,
-    async (_storyname, Story) => {
-      const tree = render(<Story />);
-      expect(tree.baseElement).toMatchSnapshot();
-    }
-  );
+  const tree = render(<PaletteStyleTag theme='dark' />);
+  expect(tree.baseElement).toMatchSnapshot();
 });
-
-const { Light, Dark } = composeStories(stories);
 
 describe('[PaletteStyleTag colors]', () => {
   it('inserts the Light theme style tag', () => {
-    render(<Light />);
+    render(<PaletteStyleTag theme='light' />);
     const style = document.querySelector('#main-palette');
     expect(style?.innerHTML).toContain(
       `surface-light: ${light['surface-light']}`
     );
   });
   it('inserts the Dark theme style tag', () => {
-    render(<Dark />);
+    render(<PaletteStyleTag theme='dark' />);
     const style = document.querySelector('#main-palette');
     expect(style?.innerHTML).toContain(
       `surface-light: ${dark['surface-light']}`
