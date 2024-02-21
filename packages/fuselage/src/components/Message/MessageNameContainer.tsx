7d61a26778253cf2e022a7d9422fdbe3aaedd9f2
Douglas Fabris <devfabris@gmail.com>
2024-02-21 13:41:27 -0300

    fix(fuselage): `MessageNameContainer` missing forwardRef (#1291)

--- packages/fuselage/src/components/Message/MessageNameContainer.tsx
+++ packages/fuselage/src/components/Message/MessageNameContainer.tsx
@@ -1,11 +1,17 @@
 import type { AllHTMLAttributes } from 'react';
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 type MessageNameContainerProps = AllHTMLAttributes<HTMLSpanElement>;
 
-export const MessageNameContainer = (props: MessageNameContainerProps) => (
-  <span
-    className='rcx-box rcx-box--full rcx-message-header__name-container'
-    {...props}
-  />
-);
+export const MessageNameContainer = forwardRef<
+  HTMLSpanElement,
+  MessageNameContainerProps
+>(function MessageNameContainer(props, ref) {
+  return (
+    <span
+      ref={ref}
+      className='rcx-box rcx-box--full rcx-message-header__name-container'
+      {...props}
+    />
+  );
+});
