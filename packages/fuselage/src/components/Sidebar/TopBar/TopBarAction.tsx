29e520851a15be482eb8089274b77a0c44f2f823
dougfabris <devfabris@gmail.com>
2024-02-26 18:53:28 -0300

    chore: split components

---
+++ packages/fuselage/src/components/Sidebar/TopBar/TopBarAction.tsx
@@ -0,0 +1,13 @@
+import type { ComponentProps, Ref } from 'react';
+import React, { forwardRef } from 'react';
+
+import { SidebarAction } from '../SidebarActions';
+
+type TopBarActionProps = ComponentProps<typeof SidebarAction>;
+
+export const TopBarAction = forwardRef(function TopBarAction(
+  props: TopBarActionProps,
+  ref: Ref<HTMLElement>
+) {
+  return <SidebarAction ref={ref} {...props} />;
+});
---
+++ packages/fuselage/src/components/Sidebar/TopBar/TopBarSection.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { SidebarDivider } from '../SidebarDivider';
+import { TopBar } from './TopBar';
+import { TopBarWrapper } from './TopBarWrapper';
+
+type TopBarSectionProps = {
+  children?: React.ReactNode;
+  className?: string;
+};
+
+export const TopBarSection = ({
+  className,
+  children,
+  ...props
+}: TopBarSectionProps) => (
+  <TopBar
+    className={['rcx-sidebar-topbar--section', className]
+      .filter(Boolean)
+      .join(' ')}
+    {...props}
+  >
+    <TopBarWrapper children={children} />
+    <SidebarDivider />
+  </TopBar>
+);
