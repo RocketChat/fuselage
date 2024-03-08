4cf7b4faf10cb6c12baefa8a03bf13d989c03008
Niral Patel <patnir41@stanford.edu>
2024-03-08 15:23:30 -0500

    Remove hide for multiple autoselect components

--- packages/fuselage/src/components/AutoComplete/AutoComplete.tsx
+++ packages/fuselage/src/components/AutoComplete/AutoComplete.tsx
@@ -88,10 +88,10 @@ export function AutoComplete({
     } else {
       setSelected(getSelected(currentValue, options));
       onChange(currentValue);
+      hide();
     }
 
     setFilter('');
-    hide();
   });
 
   const handleRemove = useEffectEvent((event) => {
