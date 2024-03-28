b4eb263f8ff4a0dd24eed8e840ed75e6d9838e63
dougfabris <devfabris@gmail.com>
2024-03-28 17:17:50 -0300

    fix: `MessageMetrics` props types

--- packages/fuselage/src/components/Message/MessageMetrics/index.tsx
+++ packages/fuselage/src/components/Message/MessageMetrics/index.tsx
@@ -1,17 +1,22 @@
 import { MessageMetrics } from './MessageMetrics';
 import { MessageMetricsFollowing } from './MessageMetricsFollowing';
-import MessageMetricsItem from './MessageMetricsItem';
+import { MessageMetricsItem } from './MessageMetricsItem';
 import { MessageMetricsReply } from './MessageMetricsReply';
 
 export default Object.assign(MessageMetrics, {
+  /**
+   * @deprecated prefer using named imports
+   * */
   Reply: MessageMetricsReply,
+  /**
+   * @deprecated prefer using named imports
+   * */
   Item: MessageMetricsItem,
+  /**
+   * @deprecated prefer using named imports
+   * */
   Following: MessageMetricsFollowing,
 });
 
-export {
-  MessageMetrics,
-  MessageMetricsFollowing,
-  MessageMetricsItem,
-  MessageMetricsReply,
-};
+export { MessageMetrics, MessageMetricsFollowing, MessageMetricsReply };
+export * from './MessageMetricsItem';
