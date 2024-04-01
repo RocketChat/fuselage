71c22168458c51975b32170a53c4a835124bec2b
Douglas Fabris <devfabris@gmail.com>
2024-04-01 09:47:10 -0300

    fix(fuselage): `MessageMetrics` props types (#1342)

---
+++ packages/fuselage/src/components/Message/MessageMetrics/MessageMetrics.stories.tsx
@@ -0,0 +1,51 @@
+import { action } from '@storybook/addon-actions';
+import { Title, Primary } from '@storybook/addon-docs';
+import type { ComponentMeta } from '@storybook/react';
+import React from 'react';
+
+import {
+  MessageMetrics,
+  MessageMetricsReply,
+  MessageMetricsItem,
+  MessageMetricsFollowing,
+  MessageMetricsItemIcon,
+  MessageMetricsItemLabel,
+} from '.';
+import { BasicMessageTemplate } from '../helpers';
+
+export default {
+  title: 'Message/MessageMetrics',
+  component: MessageMetrics,
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Primary />
+        </>
+      ),
+    },
+  },
+} as ComponentMeta<typeof MessageMetrics>;
+
+const metrics = (
+  <MessageMetrics>
+    <MessageMetricsReply>Reply</MessageMetricsReply>
+    <MessageMetricsItem title='Replies'>
+      <MessageMetricsItemIcon name='thread' />
+      <MessageMetricsItemLabel>10</MessageMetricsItemLabel>
+    </MessageMetricsItem>
+    <MessageMetricsItem>
+      <MessageMetricsFollowing
+        title='Following'
+        name='bell'
+        onClick={action('click')}
+      />
+    </MessageMetricsItem>
+  </MessageMetrics>
+);
+
+export const Default = BasicMessageTemplate.bind({});
+Default.args = {
+  metrics,
+};
--- packages/fuselage/src/components/Message/MessageMetrics/index.tsx
+++ packages/fuselage/src/components/Message/MessageMetrics/index.tsx
@@ -4,14 +4,19 @@ import MessageMetricsItem from './MessageMetricsItem';
 import { MessageMetricsReply } from './MessageMetricsReply';
 
 export default Object.assign(MessageMetrics, {
+  /**
+   * @deprecated prefer using named imports
+   * */
   Reply: MessageMetricsReply,
+  /**
+   * @deprecated prefer using named imports
+   * */
   Item: MessageMetricsItem,
+  /**
+   * @deprecated prefer using named imports
+   * */
   Following: MessageMetricsFollowing,
 });
 
-export {
-  MessageMetrics,
-  MessageMetricsFollowing,
-  MessageMetricsItem,
-  MessageMetricsReply,
-};
+export { MessageMetrics, MessageMetricsFollowing, MessageMetricsReply };
+export * from './MessageMetricsItem';
