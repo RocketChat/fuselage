ffb2113531f63236296bd7cf2f2c6b3b96638426
juliajforesti <juliajforesti@gmail.com>
2024-04-03 16:51:22 -0300

    chore: `SidebarCollapser` component

--- packages/fuselage/src/components/SideBarV2/SideBarAccordionItem.tsx
+++ packages/fuselage/src/components/SideBarV2/SideBarAccordionItem.tsx
@@ -1,125 +1,14 @@
-import { useToggle, useUniqueId } from '@rocket.chat/fuselage-hooks';
-import type {
-  HTMLAttributes,
-  KeyboardEvent,
-  MouseEvent,
-  ReactNode,
-} from 'react';
+import type { ComponentProps } from 'react';
 import React from 'react';
 
-import { Chevron } from '../Chevron';
+import { SideBarCollapser } from './components/SideBarCollapser';
 
-type SideBarAccordionItemProps = {
-  children?: ReactNode;
-  className?: string;
-  defaultExpanded?: boolean;
-  disabled?: boolean;
-  expanded?: boolean;
-  tabIndex?: number;
-  title: ReactNode;
-  noncollapsible?: boolean;
-  badge?: ReactNode;
-} & HTMLAttributes<HTMLElement>;
+type SideBarAccordionItemProps = ComponentProps<typeof SideBarCollapser>;
 
-export const SideBarAccordionItem = function Item({
-  children,
-  className,
-  defaultExpanded,
-  disabled,
-  expanded: propExpanded,
-  tabIndex = 0,
-  title,
-  noncollapsible = !title,
-  badge,
-  ...props
-}: SideBarAccordionItemProps) {
-  const [stateExpanded, toggleStateExpanded] = useToggle(defaultExpanded);
-  const expanded = propExpanded || stateExpanded;
-
-  const panelExpanded = noncollapsible || expanded;
-
-  const titleId = useUniqueId();
-  const panelId = useUniqueId();
-
-  const handleClick = (e: MouseEvent<HTMLElement>) => {
-    if (disabled) {
-      return;
-    }
-    e.currentTarget?.blur();
-    toggleStateExpanded();
-  };
-
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if (disabled || event.currentTarget !== event.target) {
-      return;
-    }
-
-    if ([13, 32].includes(event.keyCode)) {
-      event.preventDefault();
-
-      if (event.repeat) {
-        return;
-      }
-
-      toggleStateExpanded();
-    }
-  };
-
-  const collapsibleProps = {
-    'aria-controls': panelId,
-    'aria-expanded': expanded ? 'true' : 'false',
-    'tabIndex': !disabled ? tabIndex : undefined,
-    'onClick': handleClick,
-    'onKeyDown': handleKeyDown,
-  } as const;
-
-  const nonCollapsibleProps = {
-    'aria-disabled': 'true',
-    'aria-expanded': 'true',
-    'aria-labelledby': titleId,
-  } as const;
-
-  const barProps = noncollapsible ? nonCollapsibleProps : collapsibleProps;
-
+export const SideBarAccordionItem = function Item(
+  props: SideBarAccordionItemProps
+) {
   return (
-    <section
-      className={['rcx-sidebar-v2__accordion-item', className && className]
-        .filter(Boolean)
-        .join(' ')}
-      {...props}
-    >
-      <div
-        role='button'
-        className={[
-          'rcx-sidebar-v2__accordion-item__bar',
-          'rcx-box--animated',
-          disabled && 'rcx-sidebar-v2__accordion-item__bar--disabled',
-        ]
-          .filter(Boolean)
-          .join(' ')}
-        {...barProps}
-      >
-        {!noncollapsible && <Chevron size='x18' up={expanded} />}
-        {title && (
-          <h4 className='rcx-sidebar-v2__accordion-item__title' id={titleId}>
-            {title}
-          </h4>
-        )}
-        {!expanded && badge && badge}
-      </div>
-      <div
-        className={[
-          'rcx-sidebar-v2__accordion-item__panel',
-          'rcx-box--animated',
-          panelExpanded && 'rcx-sidebar-v2__accordion-item__panel--expanded',
-        ]
-          .filter(Boolean)
-          .join(' ')}
-        id={panelId}
-        role='region'
-      >
-        {children}
-      </div>
-    </section>
+    <SideBarCollapser {...props} className='rcx-sidebar-v2__accordion-item' />
   );
 };
--- packages/fuselage/src/components/SideBarV2/SideBarCollapseGroup.tsx
+++ packages/fuselage/src/components/SideBarV2/SideBarCollapseGroup.tsx
@@ -1,107 +1,14 @@
-import { useToggle, useUniqueId } from '@rocket.chat/fuselage-hooks';
-import type { HTMLAttributes, MouseEvent } from 'react';
+import type { ComponentProps } from 'react';
 import React from 'react';
 
-import { Chevron } from '../Chevron';
+import { SideBarCollapser } from './components/SideBarCollapser';
 
-type SideBarCollapseGroupProps = {
-  expanded?: boolean;
-  defaultExpanded?: boolean;
-  tabIndex?: number;
-  title: string;
-  badge?: React.ReactNode;
-  actions?: React.ReactNode;
-} & HTMLAttributes<HTMLDivElement>;
+type SideBarCollapseGroupProps = ComponentProps<typeof SideBarCollapser>;
 
-export const SideBarCollapseGroup = ({
-  expanded: propExpanded,
-  defaultExpanded,
-  tabIndex = 0,
-  children,
-  title,
-  className,
-  badge,
-  ...props
-}: SideBarCollapseGroupProps) => {
-  const [stateExpanded, toggleStateExpanded] = useToggle(defaultExpanded);
-  const expanded = propExpanded || stateExpanded;
-
-  const titleId = useUniqueId();
-  const panelId = useUniqueId();
-
-  const handleClick = (e: MouseEvent<HTMLElement>) => {
-    e.currentTarget?.blur();
-    toggleStateExpanded();
-  };
-
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if (event.currentTarget !== event.target) {
-      return;
-    }
-
-    if ([13, 32].includes(event.keyCode)) {
-      event.preventDefault();
-
-      if (event.repeat) {
-        return;
-      }
-
-      toggleStateExpanded();
-    }
-  };
-
-  const collapsibleProps = {
-    'aria-controls': panelId,
-    'aria-expanded': expanded ? 'true' : 'false',
-    tabIndex,
-    'onClick': handleClick,
-    'onKeyDown': handleKeyDown,
-  } as const;
-
+export const SideBarCollapseGroup = function Item(
+  props: SideBarCollapseGroupProps
+) {
   return (
-    <section
-      className={['rcx-sidebar-v2__collapse-group', className && className]
-        .filter(Boolean)
-        .join(' ')}
-      {...props}
-    >
-      <div
-        role='button'
-        className={['rcx-sidebar-v2__collapse-group__bar', 'rcx-box--animated']
-          .filter(Boolean)
-          .join(' ')}
-        {...collapsibleProps}
-      >
-        {<Chevron size='x18' up={expanded} />}
-        {title && (
-          <h5 className='rcx-sidebar-v2__collapse-group__title' id={titleId}>
-            {title}
-          </h5>
-        )}
-        {!expanded && badge && badge}
-      </div>
-      <div
-        className={[
-          'rcx-sidebar-v2__collapse-group__panel',
-          'rcx-box--animated',
-          expanded && 'rcx-sidebar-v2__collapse-group__panel--expanded',
-        ]
-          .filter(Boolean)
-          .join(' ')}
-        id={panelId}
-        role='region'
-      >
-        {children}
-      </div>
-    </section>
-    // <div
-    //   role='button'
-    //   className='rcx-sidebar-v2__collapse-group'
-    //   {...collapsibleProps}
-    //   {...props}
-    // >
-    //   <Chevron size='x18' up={expanded} />
-    //   <span className='rcx-sidebar-v2__collapse-group__title'>{children}</span>
-    // </div>
+    <SideBarCollapser {...props} className='rcx-sidebar-v2__collapse-group' />
   );
 };
