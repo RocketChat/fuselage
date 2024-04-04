b6d596d2de86f3113ab2e0db4fe62142ce46421c
juliajforesti <juliajforesti@gmail.com>
2024-04-04 12:28:34 -0300

    test: add a11y tests

--- packages/fuselage/src/components/SideBarV2/Sidebar.spec.tsx
+++ packages/fuselage/src/components/SideBarV2/Sidebar.spec.tsx
@@ -1,13 +1,34 @@
 import { composeStories } from '@storybook/testing-react';
 import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
 import React from 'react';
+import { SSRProvider } from 'react-aria';
 
 import * as stories from './Sidebar.stories';
 
-const { Default } = composeStories(stories);
+const testCases = Object.values(composeStories(stories)).map((Story) => [
+  Story.storyName || 'Story',
+  Story,
+]);
 
 describe('[Sidebar Component]', () => {
-  it('renders without crashing', () => {
-    render(<Default />);
-  });
+  test.each(testCases)(
+    `renders %s without crashing`,
+    async (_storyname, Story) => {
+      const tree = render(<Story />, {
+        wrapper: ({ children }) => <SSRProvider>{children}</SSRProvider>,
+      });
+      expect(tree.baseElement).toMatchSnapshot();
+    }
+  );
+
+  test.each(testCases)(
+    '%s should have no a11y violations',
+    async (_storyname, Story) => {
+      const { container } = render(<Story />);
+
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    }
+  );
 });
