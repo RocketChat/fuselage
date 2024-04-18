7d2486f90276567a221dda4ee50f6d079eac6c00
dougfabris <devfabris@gmail.com>
2024-04-18 18:24:00 -0300

    fix: `FormContainer` props

--- packages/layout/src/FormPageLayout/index.ts
+++ packages/layout/src/FormPageLayout/index.ts
@@ -7,10 +7,38 @@ import FormSubtitle from './FormSubtitle';
 import FormTitle from './FormTitle';
 
 export default Object.assign(Form, {
+  /**
+   * @deprecated prefer using named imports
+   * */
   Header: FormHeader,
+  /**
+   * @deprecated prefer using named imports
+   * */
   Steps: FormSteps,
+  /**
+   * @deprecated prefer using named imports
+   * */
   Title: FormTitle,
+  /**
+   * @deprecated prefer using named imports
+   * */
   Subtitle: FormSubtitle,
+  /**
+   * @deprecated prefer using named imports
+   * */
   Container: FormContainer,
+  /**
+   * @deprecated prefer using named imports
+   * */
   Footer: FormFooter,
 });
+
+export {
+  Form,
+  FormContainer,
+  FormHeader,
+  FormFooter,
+  FormSteps,
+  FormSubtitle,
+  FormTitle,
+};
