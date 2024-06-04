7b0de9ada547feb48794d41f20143801cd6a11ea
dougfabris <devfabris@gmail.com>
2024-06-04 16:58:36 -0300

    feat: add RTL support

--- packages/fuselage-toastbar/src/ToastBar.spec.tsx
+++ packages/fuselage-toastbar/src/ToastBar.spec.tsx
@@ -1,12 +1,63 @@
 import { composeStories } from '@storybook/testing-react';
-import { render } from '@testing-library/react';
+import { render, getByRole, screen, fireEvent } from '@testing-library/react';
 
-import * as stories from './stories';
+import * as stories from './ToastBar.stories';
+import ToastBarProvider from './ToastBarProvider';
 
-const { ToastBarWithData } = composeStories(stories);
+const { Default, TopEnd } = composeStories(stories, {
+  decorators: [
+    (Story) => (
+      <ToastBarProvider>
+        <Story />
+      </ToastBarProvider>
+    ),
+  ],
+});
 
-describe('[ToastBarWithData Component]', () => {
-  it('renders without crashing', () => {
-    render(<ToastBarWithData />);
+const topEndStyle = {
+  top: '0',
+  right: '0',
+};
+
+const topStartStyle = {
+  top: '0',
+  left: '0',
+};
+
+describe('[fuselage-toastbar]', () => {
+  test('should dispatch the ToastBar on click', async () => {
+    const { container } = render(<Default />);
+    const button = getByRole(container, 'button');
+
+    fireEvent.click(button);
+    const toasts = screen.queryAllByRole('alert');
+    expect(toasts).toBeInTheDocument;
+  });
+
+  test('should ToastBar be displayed on the top right of the screen by default', async () => {
+    render(<TopEnd />);
+    const toast = screen.queryByRole('alert');
+    const toastContainer = toast?.parentElement?.parentElement?.parentElement;
+
+    expect(toastContainer).toHaveStyle(topEndStyle);
+  });
+
+  test('should ToastBar be displayed on the top right of the screen', async () => {
+    const { rerender } = render(<TopEnd />);
+    document.body.setAttribute('dir', 'ltr');
+    rerender(<TopEnd />);
+    const toast = screen.queryByRole('alert');
+    const toastContainer = toast?.parentElement?.parentElement?.parentElement;
+
+    expect(toastContainer).toHaveStyle(topEndStyle);
+  });
+
+  test('should ToastBar be displayed on the top left of the screen', async () => {
+    document.body.setAttribute('dir', 'rtl');
+    render(<TopEnd />);
+    const toast = screen.queryByRole('alert');
+    const toastContainer = toast?.parentElement?.parentElement?.parentElement;
+
+    expect(toastContainer).toHaveStyle(topStartStyle);
   });
 });
