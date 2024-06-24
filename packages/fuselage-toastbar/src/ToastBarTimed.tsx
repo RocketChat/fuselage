354e0acaa311ec72778e372a1c1d45af34f001a1
Debdut Chakraborty <debdutdeb@outlook.com>
2024-06-25 00:54:06 +0530

    chore: don't close toast when hovering

--- packages/fuselage-toastbar/src/ToastBarTimed.tsx
+++ packages/fuselage-toastbar/src/ToastBarTimed.tsx
@@ -1,6 +1,6 @@
 import { ToastBar } from '@rocket.chat/fuselage';
 import type { ReactElement } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import type { ToastBarPayload } from './ToastBarContext';
 import { useToastBarDismiss } from './ToastBarContext';
@@ -13,17 +13,37 @@ const ToastBarTimed = ({
 }: ToastBarPayload): ReactElement => {
   const dismissToastMessage = useToastBarDismiss();
 
+  const [hovering, setHovering] = useState(false);
+
+  const [timedOut, setTimedOut] = useState(false);
+
   useEffect(() => {
     const timeOut = setTimeout(() => {
+      if (hovering) {
+        setTimedOut(true);
+        return;
+      }
+
       dismissToastMessage(id);
     }, time * 1000);
 
     return () => clearTimeout(timeOut);
-  }, []);
+  }, [hovering]);
+
+  const onMouseLeave = () => {
+    if (timedOut) {
+      dismissToastMessage(id);
+      return;
+    }
+
+    setHovering(false);
+  };
 
   return (
     <ToastBar
       variant={type}
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={onMouseLeave}
       children={message}
       onClose={dismissToastMessage}
       id={id}
