2d166035e2c8b5ea259e72b2723d92820248e093
dougfabris <devfabris@gmail.com>
2024-07-17 10:35:13 -0300

    fix: types

--- packages/fuselage/src/components/Table/TableSelection/TableSelectionButton.tsx
+++ packages/fuselage/src/components/Table/TableSelection/TableSelectionButton.tsx
@@ -1,10 +1,9 @@
 import type { ComponentProps } from 'react';
 import React from 'react';
 
-import type Box from '../../Box';
 import { Button } from '../../Button';
 
-type TableSelectionButtonProps = ComponentProps<typeof Box>;
+type TableSelectionButtonProps = ComponentProps<typeof Button>;
 
 export const TableSelectionButton = (props: TableSelectionButtonProps) => (
   <Button small flexShrink={0} {...props} />
