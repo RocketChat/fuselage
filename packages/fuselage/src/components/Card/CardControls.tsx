b2d67527980e39a798cbe99c51399d65cfa38916
Rahul Singh Thakur <rahulsthakur19@gmail.com>
2024-09-03 23:57:39 +0530

    Added different behaviour for horizontal cards
    Horizontal cards should have flex-wrap only for mobile view

--- packages/fuselage/src/components/Card/CardControls.tsx
+++ packages/fuselage/src/components/Card/CardControls.tsx
@@ -1,9 +1,13 @@
+import { useBreakpoints } from '@rocket.chat/fuselage-hooks';
 import Box from '../Box/Box';
 
 const CardControls = ({...props }) => {
+  const breakpoints = useBreakpoints();
+  const isMobile = !breakpoints.includes('sm');
   return (
     <Box
       rcx-card__controls
+      rcx-card__controls--wrap={isMobile}
       {...props}
     />
   )
