11cddeaf7c602ab1e60135e4fee0e871bdeb8a70
Tasso <tasso.evangelista@rocket.chat>
2024-10-10 22:47:58 -0300

    Adjust babel usage

--- packages/fuselage/.babelrc.js
+++ packages/fuselage/.babelrc.js
@@ -23,10 +23,4 @@ module.exports = (api) => ({
       },
     ],
   ],
-  plugins: [
-    '@babel/plugin-transform-runtime',
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-  ],
 });
