e2a79ef1760a05970cf574811f9367b1437b96b3
Rahul Singh Thakur <65606499+Barrylimarti@users.noreply.github.com>
2024-10-23 01:18:19 +0530

    Update packages/fuselage/src/components/Card/CardControls.tsx
    Co-authored-by: Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>

--- packages/fuselage/src/components/Card/CardControls.tsx
+++ packages/fuselage/src/components/Card/CardControls.tsx
@@ -1,12 +1,7 @@
-import type { ReactNode, MouseEventHandler } from 'react';
+import { AllHTMLAttributes } from 'react';
 
-type CardControlsProps = {
-  children?: ReactNode;
-  onClick?: MouseEventHandler<HTMLDivElement>;
-};
-
-const CardControls = ({ onClick, children }: CardControlsProps) => (
-  <div className='rcx-card__controls' onClick={onClick} children={children} />
+const CardControls = (props: AllHTMLAttributes<HTMLDivElement>) => (
+  <div className='rcx-card__controls' {...props} />
 );
 
 export default CardControls;
