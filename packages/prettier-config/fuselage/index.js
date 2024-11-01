da537358d0bdad7a31135b9fae4a40e36eaad52f
Tasso <tasso.evangelista@rocket.chat>
2024-11-01 02:08:12 -0300

    Add JSDoc

--- packages/prettier-config/fuselage/index.js
+++ packages/prettier-config/fuselage/index.js
@@ -1,9 +1,12 @@
-module.exports = {
+module.exports = /** @type {import('prettier').Options} */ ({
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
   jsxSingleQuote: true,
   printWidth: 80,
   quoteProps: 'consistent',
-  semi: true,
   singleQuote: true,
+  trailingComma: 'all',
   useTabs: false,
   xmlWhitespaceSensitivity: 'ignore',
-};
+});
