599693cd18200c7168c6510d79f140d5b010f6f5
Tasso <tasso.evangelista@rocket.chat>
2024-11-05 11:54:03 -0300

    Remove `@rocket.chat/eslint-config-alt`

--- tools/lint-all/lint-and-fix.mjs
+++ tools/lint-all/lint-and-fix.mjs
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
-import { $, fs, glob } from 'zx';
+import { $, glob } from 'zx';
 
 $.verbose = true;
 
-const manifest = await fs.readJSON('./package.json');
-
-if ((await glob('.eslintrc*')).length || 'eslintConfig' in manifest) {
-  await $`eslint --fix '**/*.{js,mjs,ts,tsx,mdx}'`;
-}
+await $`eslint --fix`;
 
 if ((await glob('.stylelintrc*')).length) {
   await $`stylelint --allow-empty-input --fix '**/*.{css,scss}'`;
