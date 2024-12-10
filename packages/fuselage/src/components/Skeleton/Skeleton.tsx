2a80d8e49714e123460e3fa7b36ea91bd8c7721e
dougfabris <devfabris@gmail.com>
2024-12-10 14:07:13 -0300

    fix: Skeleton missing HTML attributes in type

--- packages/fuselage/src/components/Skeleton/Skeleton.tsx
+++ packages/fuselage/src/components/Skeleton/Skeleton.tsx
@@ -1,9 +1,11 @@
+import { AllHTMLAttributes } from 'react';
+
 import { cx, cxx } from '../../helpers/composeClassNames';
 import { StylingBox, StylingBoxProps } from '../Box';
 
 type SkeletonProps = Omit<StylingBoxProps, 'children'> & {
   variant?: 'text' | 'rect' | 'circle';
-};
+} & AllHTMLAttributes<HTMLSpanElement>;
 
 const Skeleton = ({ variant = 'text', ...props }: SkeletonProps) => (
   <StylingBox {...props}>
