f56b86d4c7118b33f3a9ca4f626678bd50ba9e9f
Tasso <tasso.evangelista@rocket.chat>
2025-01-21 16:32:14 -0300

    Fix lint issues

--- packages/fuselage-toastbar/src/ToastBarPortal.ts
+++ packages/fuselage-toastbar/src/ToastBarPortal.ts
@@ -3,40 +3,43 @@ import { memo, useLayoutEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const ensureAnchorElement = (id: string): HTMLElement => {
-	const existingAnchor = document.getElementById(id);
-	if (existingAnchor) return existingAnchor;
+  const existingAnchor = document.getElementById(id);
+  if (existingAnchor) return existingAnchor;
 
-	const newAnchor = document.createElement('div');
-	newAnchor.id = id;
-	document.body.appendChild(newAnchor);
-	return newAnchor;
+  const newAnchor = document.createElement('div');
+  newAnchor.id = id;
+  document.body.appendChild(newAnchor);
+  return newAnchor;
 };
 
 const getAnchorRefCount = (anchorElement: HTMLElement): number => {
-	const { refCount } = anchorElement.dataset;
-	if (refCount) return parseInt(refCount, 10);
-	return 0;
+  const { refCount } = anchorElement.dataset;
+  if (refCount) return parseInt(refCount, 10);
+  return 0;
 };
 
-const setAnchorRefCount = (anchorElement: HTMLElement, refCount: number): void => {
-	anchorElement.dataset.refCount = String(refCount);
+const setAnchorRefCount = (
+  anchorElement: HTMLElement,
+  refCount: number,
+): void => {
+  anchorElement.dataset.refCount = String(refCount);
 };
 
 const refAnchorElement = (anchorElement: HTMLElement): void => {
-	setAnchorRefCount(anchorElement, getAnchorRefCount(anchorElement) + 1);
+  setAnchorRefCount(anchorElement, getAnchorRefCount(anchorElement) + 1);
 
-	if (anchorElement.parentElement !== document.body) {
-		document.body.appendChild(anchorElement);
-	}
+  if (anchorElement.parentElement !== document.body) {
+    document.body.appendChild(anchorElement);
+  }
 };
 
 const unrefAnchorElement = (anchorElement: HTMLElement): void => {
-	const refCount = getAnchorRefCount(anchorElement) - 1;
-	setAnchorRefCount(anchorElement, refCount);
+  const refCount = getAnchorRefCount(anchorElement) - 1;
+  setAnchorRefCount(anchorElement, refCount);
 
-	if (refCount <= 0) {
-		document.body.removeChild(anchorElement);
-	}
+  if (refCount <= 0) {
+    document.body.removeChild(anchorElement);
+  }
 };
 
 type ToastBarPortalProps = {
@@ -46,13 +49,13 @@ type ToastBarPortalProps = {
 const ToastBarPortal = ({ children }: ToastBarPortalProps): ReactElement => {
   const toastBarRoot = ensureAnchorElement('toastBarRoot');
 
-	useLayoutEffect(() => {
-		refAnchorElement(toastBarRoot);
+  useLayoutEffect(() => {
+    refAnchorElement(toastBarRoot);
 
-		return () => {
-			unrefAnchorElement(toastBarRoot);
-		};
-	}, [toastBarRoot]);
+    return () => {
+      unrefAnchorElement(toastBarRoot);
+    };
+  }, [toastBarRoot]);
 
   return createPortal(children, toastBarRoot);
 };
