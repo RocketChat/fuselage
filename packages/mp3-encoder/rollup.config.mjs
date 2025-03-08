1210a14c95aef8bfce91407fc3530720692dc7bd
Tasso <tasso.evangelista@rocket.chat>
2025-03-08 15:35:13 -0300

    Upgrade Rollup configuration

--- packages/mp3-encoder/rollup.config.mjs
+++ packages/mp3-encoder/rollup.config.mjs
@@ -1,43 +1,33 @@
-import path from 'path';
+import { basename, dirname } from 'node:path';
 
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import { defineConfig } from 'rollup';
 
 import pkg from './package.json' with { type: 'json' };
 
-/** @type {import('rollup').RollupOptions[]} */
-export default [
-  {
-    input: 'src/index.ts',
-    output: {
-      dir: path.dirname(pkg.main),
-      entryFileNames: path.basename(pkg.main),
+export default defineConfig({
+  input: 'src/index.ts',
+  output: [
+    {
+      dir: dirname(pkg.main),
+      entryFileNames: basename(pkg.main),
       format: 'cjs',
       sourcemap: true,
       strict: false,
     },
-    plugins: [
-      typescript({
-        tsconfig: 'tsconfig.build.json',
-      }),
-      resolve(),
-      commonjs(),
-    ],
-  },
-  {
-    input: 'src/index.ts',
-    output: {
+    {
       file: pkg.module,
       format: 'esm',
       sourcemap: true,
     },
-    plugins: [
-      typescript({
-        tsconfig: 'tsconfig.build.json',
-      }),
-      resolve(),
-      commonjs(),
-    ],
-  },
-];
+  ],
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.build.json',
+    }),
+    resolve(),
+    commonjs(),
+  ],
+});
