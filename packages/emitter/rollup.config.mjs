36d15fe3b0151c306fe6a72f57e67498ec75a506
Tasso <tasso.evangelista@rocket.chat>
2025-03-08 12:08:14 -0300

    Upgrade Rollup

--- packages/emitter/rollup.config.mjs
+++ packages/emitter/rollup.config.mjs
@@ -6,7 +6,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
-import pkg from './package.json';
+import pkg from './package.json' with { type: 'json' };
 
 export default {
   external: ['react'],
