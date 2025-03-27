4e548a71a5f46faf685e8597dc06956d5b0ecb89
gabriellsh <40830821+gabriellsh@users.noreply.github.com>
2025-03-27 03:21:04 -0300

    refactor: use `useSyncExternalStore` in favor of `useState` in `useXyzStorage` hooks (#1595)

--- packages/fuselage-hooks/src/useStorage.ts
+++ packages/fuselage-hooks/src/useStorage.ts
@@ -1,6 +1,19 @@
 import { Emitter } from '@rocket.chat/emitter';
-import type { Dispatch, SetStateAction } from 'react';
-import { useRef, useState, useEffect, useCallback } from 'react';
+import type { Dispatch, MutableRefObject, SetStateAction } from 'react';
+import { useRef, useCallback, useSyncExternalStore, useState } from 'react';
+
+const getStorageItem = (
+  key: string,
+  initialValue: any,
+  storage?: Storage,
+): any => {
+  if (!storage) {
+    return initialValue;
+  }
+
+  const item = storage.getItem(key);
+  return item ? JSON.parse(item) : initialValue;
+};
 
 function makeStorageHook(
   storageFactory: Storage | (() => Storage),
@@ -24,49 +37,58 @@ function makeStorageHook(
     const initialValueRef = useRef(initialValue);
     initialValueRef.current = initialValue;
 
-    const [storedValue, setStoredValue] = useState<T>(() => {
-      if (!storage) {
-        return initialValue;
-      }
+    const [valueRef] = useState<MutableRefObject<T>>(() => ({
+      current: getStorageItem(getKey(key), initialValue, storage),
+    }));
 
-      const item = storage.getItem(getKey(key));
-      return item ? JSON.parse(item) : initialValue;
-    });
+    const getSnapshot = useCallback(() => valueRef.current, [valueRef]);
+
+    const getServerSnapshot = useCallback(() => initialValueRef.current, []);
+
+    const subscribe = useCallback(
+      (cb: () => void) => {
+        const handleEvent = (event: StorageEvent): void => {
+          if (event.key !== getKey(key)) {
+            return;
+          }
+          ee.emit(
+            key,
+            event.newValue
+              ? JSON.parse(event.newValue)
+              : initialValueRef.current,
+          );
+        };
+
+        const offCb = ee.on(key, (value: T): void => {
+          valueRef.current = value;
+          cb();
+        });
+
+        window.addEventListener('storage', handleEvent);
+        return () => {
+          offCb();
+          window.removeEventListener('storage', handleEvent);
+        };
+      },
+      [key, valueRef],
+    );
+
+    const storedValue = useSyncExternalStore(
+      subscribe,
+      getSnapshot,
+      getServerSnapshot,
+    );
 
     const setValue: Dispatch<SetStateAction<T>> = useCallback(
       (value) => {
-        setStoredValue((prevValue: T) => {
-          const valueToStore: T =
-            value instanceof Function ? value(prevValue) : value;
-          storage?.setItem(getKey(key), JSON.stringify(valueToStore));
-          ee.emit(key, valueToStore);
-          return valueToStore;
-        });
+        const valueToStore: T =
+          value instanceof Function ? value(valueRef.current) : value;
+        storage?.setItem(getKey(key), JSON.stringify(valueToStore));
+        ee.emit(key, valueToStore);
+        return valueToStore;
       },
-      [key],
+      [key, valueRef],
     );
-
-    useEffect(() => {
-      const handleEvent = (event: StorageEvent): void => {
-        if (event.key !== getKey(key)) {
-          return;
-        }
-
-        setStoredValue(
-          event.newValue ? JSON.parse(event.newValue) : initialValueRef.current,
-        );
-      };
-
-      const handleSyntheticEvent = (value: T): void => {
-        setStoredValue(value);
-      };
-      ee.on(key, handleSyntheticEvent);
-      window.addEventListener('storage', handleEvent);
-      return () => {
-        ee.off(key, handleSyntheticEvent);
-        window.removeEventListener('storage', handleEvent);
-      };
-    }, [key]);
 
     return [storedValue, setValue];
   };
