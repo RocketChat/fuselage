45af98614546defd0fb9c95ebb374c2d93369655
dougfabris <devfabris@gmail.com>
2025-03-27 18:26:42 -0300

    chore: remove unused `ManageWorkspaceFallback`

--- packages/layout/src/ManageWorkspaceFallback.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-
-import ManageWorkspaceFallback from './ManageWorkspaceFallback';
-
-it('renders without crashing', () => {
-  render(<ManageWorkspaceFallback onManageWorkspaceClick={() => undefined} />);
-});
