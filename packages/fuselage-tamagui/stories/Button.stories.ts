40c9eec7b1cf3a71de4f8ea1f1046ffd2c360073
Anxhul10 <anxhul10@gmail.com>
2025-05-07 13:24:00 +0530

    update:used tamagui button

--- packages/fuselage-tamagui/stories/Button.stories.ts
+++ packages/fuselage-tamagui/stories/Button.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-import { Button } from 'tamagui';
+import { CButton as Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
