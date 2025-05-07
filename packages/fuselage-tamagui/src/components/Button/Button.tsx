be48bfdb69fe86ea3d25fda0ef84a108658d3937
Anxhul10 <anxhul10@gmail.com>
2025-05-08 00:20:24 +0530

    refactor: custom implementation of button

--- packages/fuselage-tamagui/src/components/Button/Button.tsx
+++ packages/fuselage-tamagui/src/components/Button/Button.tsx
@@ -1,6 +1,89 @@
-import React from 'react'
-import {Button , ButtonProps} from 'tamagui'
+import { getSize, getSpace } from '@tamagui/get-token'
+import {
+  GetProps,
+  SizeTokens,
+  View,
+  Text,
+  createStyledContext,
+  styled,
+  useTheme,
+  withStaticProperties,
+} from '@tamagui/web'
+import { cloneElement, isValidElement, useContext } from 'react'
 
-export const CButton = () =>{
-  return <Button>Button</Button>
+export const ButtonContext = createStyledContext({
+  size: '$md' as SizeTokens,
+})
+
+export const ButtonFrame = styled(View, {
+  name: 'Button',
+  context: ButtonContext,
+  backgroundColor: '$background',
+  alignItems: 'center',
+  flexDirection: 'row',
+
+  hoverStyle: {
+    backgroundColor: '$backgroundHover',
+  },
+
+  pressStyle: {
+    backgroundColor: '$backgroundPress',
+  },
+
+  variants: {
+    size: {
+      '...size': (name, { tokens }) => {
+        return {
+          height: tokens.size[name],
+          borderRadius: tokens.radius[name],
+          // note the getSpace and getSize helpers will let you shift down/up token sizes
+          // whereas with gap we just multiply by 0.2
+          // this is a stylistic choice, and depends on your design system values
+          gap: tokens.space[name].val * 0.2,
+          paddingHorizontal: getSpace(name, {
+            shift: -1,
+          }),
+        }
+      },
+    },
+  } as const,
+
+  defaultVariants: {
+    size: '$md',
+  },
+})
+
+type ButtonProps = GetProps<typeof ButtonFrame>
+
+export const ButtonText = styled(Text, {
+  name: 'ButtonText',
+  context: ButtonContext,
+  color: '$color',
+  userSelect: 'none',
+
+  variants: {
+    size: {
+      '...fontSize': (name, { font }) => ({
+        fontSize: font?.size[name],
+      }),
+    },
+  } as const,
+})
+
+const ButtonIcon = (props: { children: any }) => {
+  const { size } = useContext(ButtonContext.context)
+  const smaller = getSize(size, {
+    shift: -2,
+  })
+  const theme = useTheme()
+  return isValidElement(props.children) ? cloneElement(props.children, {
+    size: smaller.val * 0.5,
+    color: theme.color.get(),
+  }) : null
 }
+
+export const Button = withStaticProperties(ButtonFrame, {
+  Props: ButtonContext.Provider,
+  Text: ButtonText,
+  Icon: ButtonIcon,
+})
