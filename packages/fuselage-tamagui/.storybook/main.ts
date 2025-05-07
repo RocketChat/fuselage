7ecd55b9cf42d293f188d9ec58fcdc91a48c4ff6
Anxhul10 <anxhul10@gmail.com>
2025-05-07 20:37:42 +0530

    refactor: use src folder instead of stories folder

--- packages/fuselage-tamagui/.storybook/main.ts
+++ packages/fuselage-tamagui/.storybook/main.ts
@@ -11,8 +11,8 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
