5fa0bc21db21df38fe89f8500cfcb438a8e1fcef
Anxhul10 <anxhul10@gmail.com>
2025-05-08 03:15:43 +0530

    update: added variants of button

--- packages/fuselage-tamagui/src/components/Button/Button.stories.tsx
+++ packages/fuselage-tamagui/src/components/Button/Button.stories.tsx
@@ -21,7 +21,7 @@ export const Default: StoryFn<typeof Button> = () => {
 
 export const Loading: StoryFn<typeof Button> = () => {
   return (
-  <Button borderRadius="$sm" >
+  <Button disabled borderRadius="$sm" >
   <Spinner color="#6C727A"></Spinner>
   Button
   </Button>);
@@ -41,7 +41,7 @@ export const LoadingInteraction: StoryFn<typeof Button> = () => {
   };
   if(loading){
     return (
-      <Button borderRadius="$sm" >
+      <Button disabled borderRadius="$sm" >
         <Spinner color="#6C727A"></Spinner>
         Button
       </Button>
@@ -56,25 +56,25 @@ export const LoadingInteraction: StoryFn<typeof Button> = () => {
 
 export const Variants: StoryFn<typeof Button> = () => {
   return (
-    <YStack space="$0">
-      <XStack space="$5">
-        <Button  borderRadius="$sm" >Primary</Button>
-        <Button  borderRadius="$sm">Secondary</Button>
+    <YStack space="$sm">
+      <XStack space="$sm">
+        <Button  Primary borderRadius="$sm" >Primary</Button>
+        <Button  Secondary borderRadius="$sm">Secondary</Button>
       </XStack>
 
-      <XStack space="$5">
-        <Button  borderRadius="$sm">Danger</Button>
-        <Button  borderRadius="$sm">Secondary Danger</Button>
+      <XStack space="$sm">
+        <Button  Danger borderRadius="$sm">Danger</Button>
+        <Button  Secondary Danger borderRadius="$sm">Secondary Danger</Button>
       </XStack>
 
-      <XStack space="$4">
-        <Button  borderRadius="$sm">Warning</Button>
-        <Button  borderRadius="$sm">Secondary Warning</Button>
+      <XStack space="$sm">
+        <Button  Warning borderRadius="$sm">Warning</Button>
+        <Button  Secondary Warning borderRadius="$sm">Secondary Warning</Button>
       </XStack>
       
-      <XStack space="$5">
-        <Button  borderRadius="$sm">Success</Button>
-        <Button  borderRadius="$sm">
+      <XStack space="$sm">
+        <Button  Success borderRadius="$sm">Success</Button>
+        <Button  Secondary Success borderRadius="$sm">
         Secondary Success
         </Button>
       </XStack>
@@ -87,10 +87,10 @@ export const Variants: StoryFn<typeof Button> = () => {
 
 export const Sizes: StoryFn<typeof Button> = () => {
   return <>
-    <XStack space='$3' alignItems='center'>
+    <XStack space='$sm' alignItems='center'>
       <Button borderRadius="$sm" size='$sm'>Small</Button>
       <Button borderRadius="$sm" size='$md'>Medium</Button>
-      <Button borderRadius="$sm">Default</Button>
+      <Button borderRadius="$sm" >Default</Button>
     </XStack>
   </>
 };
@@ -108,98 +108,98 @@ export const AsLink: StoryFn<typeof Button> = () => {
 
 export const States: StoryFn<typeof Button> = () => {
   return (
-    <YStack space="$4">
-      <XStack space="$5">
-        <Button borderRadius="$1" onPress={() => {}} className="hover">
+    <YStack space="$sm">
+      <XStack space="$sm">
+        <Button borderRadius="$sm" onPress={() => {}} className="hover">
           Hover
         </Button>
-        <Button borderRadius="$1" onPress={() => {}} className="active">
+        <Button borderRadius="$sm" onPress={() => {}} className="active">
           Active
         </Button>
       </XStack>
 
-      <XStack space="$5">
-        <Button borderRadius="$1" onPress={() => {}} className="focus">
+      <XStack space="$sm">
+        <Button borderRadius="$sm" onPress={() => {}} className="focus">
           Focus
         </Button>
-        <Button borderRadius="$1" onPress={() => {}} className="focus-visible">
+        <Button borderRadius="$sm" onPress={() => {}} className="focus-visible">
           Focus Visible
         </Button>
       </XStack>
 
-      <XStack space="$5">
-        <Button borderRadius="$1" disabled>
+      <XStack space="$md">
+        <Button borderRadius="$sm" disabled>
           Disabled
         </Button>
       </XStack>
 
       {/* Button Variations (Primary, Secondary, etc.) */}
-      <XStack space="$5">
-        <Button borderRadius="$1" primary>
+      <XStack space="$md">
+        <Button borderRadius="$sm" primary>
           Primary
         </Button>
-        <Button borderRadius="$1" secondary>
+        <Button borderRadius="$sm" secondary>
           Secondary
         </Button>
       </XStack>
 
-      <XStack space="$5">
-        <Button borderRadius="$1" danger>
+      <XStack space="$md">
+        <Button borderRadius="$sm" danger>
           Danger
         </Button>
-        <Button borderRadius="$1" secondaryDanger>
+        <Button borderRadius="$sm" secondaryDanger>
           Secondary Danger
         </Button>
       </XStack>
 
-      <XStack space="$5">
-        <Button borderRadius="$1" warning>
+      <XStack space="$md">
+        <Button borderRadius="$sm" warning>
           Warning
         </Button>
-        <Button borderRadius="$1" secondaryWarning>
+        <Button borderRadius="$sm" secondaryWarning>
           Secondary Warning
         </Button>
       </XStack>
 
-      <XStack space="$5">
-        <Button borderRadius="$1" success>
+      <XStack space="$md">
+        <Button borderRadius="$sm" success>
           Success
         </Button>
-        <Button borderRadius="$1" secondarySuccess>
+        <Button borderRadius="$sm" secondarySuccess>
           Secondary Success
         </Button>
       </XStack>
 
       {/* Small Button Variations */}
-      <XStack space="$5">
-        <Button borderRadius="$1" size="sm" onPress={() => {}} className="hover">
+      <XStack space="$md">
+        <Button borderRadius="$sm" size="$sm" onPress={() => {}} className="hover">
           Hover
         </Button>
-        <Button borderRadius="$1" size="sm" onPress={() => {}} className="active">
+        <Button borderRadius="$sm" size="$sm" onPress={() => {}} className="active">
           Active
         </Button>
       </XStack>
 
-      <XStack space="$5">
-        <Button borderRadius="$1" size="sm" onPress={() => {}} className="focus">
+      <XStack space="$md">
+        <Button borderRadius="$sm" size="$sm" onPress={() => {}} className="focus">
           Focus
         </Button>
-        <Button borderRadius="$1" size="sm" onPress={() => {}} className="focus-visible">
+        <Button borderRadius="$sm" size="$sm" onPress={() => {}} className="focus-visible">
           Focus Visible
         </Button>
       </XStack>
 
-      <XStack space="$5">
-        <Button borderRadius="$1" size="sm" disabled>
+      <XStack space="$md">
+        <Button borderRadius="$sm" size="$sm" disabled>
           Disabled
         </Button>
       </XStack>
 
-      <XStack space="$5">
-        <Button borderRadius="$1" size="sm" primary>
+      <XStack space="$md">
+        <Button borderRadius="$sm" size="$sm" primary>
           Primary
         </Button>
-        <Button borderRadius="$1" size="sm" secondary>
+        <Button borderRadius="$sm" size="$sm" secondary>
           Secondary
         </Button>
       </XStack>
@@ -209,7 +209,7 @@ export const States: StoryFn<typeof Button> = () => {
 
 export const AsIconButton: StoryFn<typeof Button> = () => {
   return (
-    <Button Icon='rocket'>
+    <Button borderRadius="$sm">
       Button
     </Button>
 )
