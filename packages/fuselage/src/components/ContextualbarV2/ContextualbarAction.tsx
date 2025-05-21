fe29c561b0de15902bd3697e8a920ad503024adc
yash-rajpal <rajpal.yash03@gmail.com>
2025-05-21 21:40:35 +0530

    ref forward close contextual bar action button

--- packages/fuselage/src/components/ContextualbarV2/ContextualbarAction.tsx
+++ packages/fuselage/src/components/ContextualbarV2/ContextualbarAction.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement, ComponentProps } from 'react';
-import { memo } from 'react';
+import { forwardRef, memo } from 'react';
 
 import type { Icon } from '..';
 import { IconButton } from '..';
@@ -8,11 +8,11 @@ type ContextualbarActionProps = {
   name: ComponentProps<typeof Icon>['name'];
 } & Omit<ComponentProps<typeof IconButton>, 'icon'>;
 
-const ContextualbarAction = ({
-  name,
-  ...props
-}: ContextualbarActionProps): ReactElement => (
-  <IconButton {...props} small flexShrink={0} icon={name} />
-);
+const ContextualbarAction = forwardRef<
+  HTMLButtonElement,
+  ContextualbarActionProps
+>(function ContextualbarAction({ name, ...props }, ref): ReactElement {
+  return <IconButton {...props} ref={ref} small flexShrink={0} icon={name} />;
+});
 
 export default memo(ContextualbarAction);
