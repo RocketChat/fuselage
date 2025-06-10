9d67db73828514aeae40ca88e6d1209bf56467fe
gabriellsh <henriques.gabriell@gmail.com>
2025-06-10 19:10:38 -0300

    update Inputs

--- packages/fuselage/src/components/RadioButton/RadioButton.tsx
+++ packages/fuselage/src/components/RadioButton/RadioButton.tsx
@@ -1,7 +1,9 @@
 import type { AllHTMLAttributes, ComponentProps, Ref } from 'react';
 import { forwardRef } from 'react';
+import { VisuallyHidden } from 'react-aria';
 
 import Box from '../Box';
+import { useFieldWrappedByInputLabel } from '../Field/Field';
 
 type RadioButtonProps = ComponentProps<typeof Box> &
   AllHTMLAttributes<HTMLInputElement>;
@@ -10,13 +12,16 @@ export const RadioButton = forwardRef(function RadioButton(
   { className, ...props }: RadioButtonProps,
   ref: Ref<HTMLInputElement>,
 ) {
+  const [label, radioLabelProps] = useFieldWrappedByInputLabel();
   return (
     <Box is='label' className={className} rcx-radio-button>
+      {!!label && <VisuallyHidden>{label}</VisuallyHidden>}
       <Box
         is='input'
         rcx-radio-button__input
         type='radio'
         ref={ref}
+        {...radioLabelProps}
         {...props}
       />
       <Box is='i' rcx-radio-button__fake aria-hidden='true' />
