628f8821021cd9eacf3598cd416459019e95ea57
Anxhul10 <anxhul10@gmail.com>
2025-06-10 23:50:01 +0530

    chore: update dealay hook

--- packages/fuselage/src/hooks/useDelayCapture.ts
+++ packages/fuselage/src/hooks/useDelayCapture.ts
@@ -1,14 +1,17 @@
+// utils/loki/use-loki-capture-delay.hook.js
 import {useEffect} from 'react';
 import isLokiRunning from '@loki/is-loki-running';
 import createAsyncCallback from "@loki/create-async-callback";
 
-const useDelayCapture = (delayInMs = 1000) => {
+const useLokiCaptureDelay = (delayInMs = 0) => {
   useEffect(() => {
-    if ( isLokiRunning() ) {
+    if ( isLokiRunning() && delayInMs ) {
       const onDone = createAsyncCallback();
+      // Here! This is where the delay is set and Loki wil not fire until onDone 
+      // is called, after the delay time has passed by.
       const timer = setTimeout(() => onDone(), delayInMs);
       return () => clearTimeout(timer);
     }
   }, [delayInMs]);
 }
-export default useDelayCapture;
+export default useLokiCaptureDelay;
