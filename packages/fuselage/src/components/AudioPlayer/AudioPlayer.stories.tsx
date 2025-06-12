8e95f8ad82d7323bda98d3272ed61fd56cdad5f2
Anxhul10 <anxhul10@gmail.com>
2025-06-13 00:51:54 +0530

    chore: add loki async callback for reliable test

--- packages/fuselage/src/components/AudioPlayer/AudioPlayer.stories.tsx
+++ packages/fuselage/src/components/AudioPlayer/AudioPlayer.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta } from '@storybook/react';
 
 import { AudioPlayer } from './AudioPlayer';
 
+import createAsyncCallback from '@loki/create-async-callback';
+
 export default {
   title: 'Media/AudioPlayer',
   component: AudioPlayer,
@@ -10,4 +12,4 @@ export default {
 const AUDIO_URL =
   'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-17.mp3';
 
-export const AudioPlayerDefault = () => <AudioPlayer src={AUDIO_URL} />;
+export const AudioPlayerDefault = () => <AudioPlayer src={AUDIO_URL} onDone={createAsyncCallback()} />;
