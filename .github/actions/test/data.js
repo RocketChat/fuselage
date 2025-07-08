a9eae200d0cd2b4ab6f01a9291289b85df61e773
Anxhul10 <anxhul10@gmail.com>
2025-07-09 04:57:46 +0530

    chore: add data

---
+++ .github/actions/test/data.js
@@ -0,0 +1,13 @@
+const fullTest = [
+    {
+        fuselage:[
+            '.babelrc.js',
+            'package.json',
+            'tsconfig.json',
+            'tsconfig.build.json',
+            'webpack.config.js',
+            '.storybook',
+            {'src':[]}
+        ]
+    }
+]
