fa1bb0f6d898f05bc2facfe9fca28c8fe3818712
Anshul Ekka <anxhul10@gmail.com>
2025-07-12 03:47:56 +0530

    chore: changed folder of git & used group to get changed files (#46)
    * chore: changed folder of git & used group to get changed files
    
    * fix: incorrect file path
    
    * chore: add componenet stats and webpack stats of storybook
    
    * chore: add needs in my action workflow of upload artifact
    
    * fix: typo in the for loop
    
    * chore: test
    
    * chroe: remove upload aritifact for fast builds
    
    * chore: run ls from the index.js
    
    * chore: use artifacts to ensure to get project.json
    
    * chore: add comments for context
    
    * chore: show directory in dist folder to ensure .json has generated
    
    * fix: typo i.e working directory used twice
    
    * chore: remove execa to show directory & remove steps i.e show directory
    
    * chore: remove console

--- .github/actions/test/index.js
+++ .github/actions/test/index.js
@@ -1,14 +1,34 @@
 import * as github from '@actions/github';
 import * as core from '@actions/core';
-import { getChangedFile } from './git.js';
+import { getChangedFile } from './src/git/git.js';
+import { copyFiles } from './src/utils/copyFiles.js';
 
+// yarn build-storybook --stats-json gives project-stats.json which has component titles
+// where as index.json gives the webpack base dependency graph
+// index.js run from the root of the project
 const context = github.context;
+const filesToCopy = [
+    {src:'./packages/fuselage/storybook-static/preview-stats.json', dest:'.github/actions/test/dist/fuselage-sb'},
+    {src:'./packages/fuselage-toastbar/storybook-static/index.json', dest:'.github/actions/test/dist/fuselage-toastbar-sb.json'},
+    {src:'./packages/onboarding-ui/storybook-static/index.json', dest:'.github/actions/test/dist/onboarding-ui-sb.json'},
+    {src:'./packages/layout/storybook-static/index.json', dest:'.github/actions/test/dist/layout-sb.json'},
+    {src:'./packages/fuselage/storybook-static/preview-stats.json', dest: '.github/actions/test/dist/fuselage-stats.json'},
+    {src:'./packages/fuselage-toastbar/storybook-static/preview-stats.json', dest:'.github/actions/test/dist/fuselage-toastbar-stats.json'},
+    {src:'./packages/onboarding-ui/storybook-static/preview-stats.json', dest:'.github/actions/test/dist/onboarding-ui-stats.json'},
+    {src:'./packages/layout/storybook-static/preview-stats.json', dest:'.github/actions/test/dist/layout-stats.json'},
+]
 
 async function run(context){
+    // later for loop be used in if statement
+    for( const {src, dest} of filesToCopy ){
+            copyFiles(src, dest);
+        }
     if(context.eventName === 'pull_request'){
-        core.info('\u001b[48;5;6mSuccess');
         const changedFiles = await getChangedFile(context);
+        core.info('\u001b[48;5;6mSuccess');
+        core.startGroup('click to see the changed files');
         console.log(changedFiles);
+        core.endGroup();
     }
     else {
         core.error('To use Loki OdinSnap please use trigger events like pull request or push');
