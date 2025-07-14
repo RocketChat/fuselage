5bc99c16369e47e5fa1f4cb9fd12ab0dab47b79b
Anxhul10 <anxhul10@gmail.com>
2025-07-14 12:00:27 +0530

    chore: add JSDoc annotations

--- .github/actions/test/src/getComponentTitle.js
+++ .github/actions/test/src/getComponentTitle.js
@@ -1,5 +1,11 @@
 import { readStatsFile } from './stats/readStatsFile.js';
 
+/**
+ * 
+ * @param {string[]} storyNameArray - name or file path from src which includes .stories
+ * @param {string} changedPackage - current pacakge which is changed
+ * @returns {promise<Set<string>>} - A Promise that resolves to a set of component titles.
+ */
 export const getComponentTitle = async (storyNameArray, changedPackage) => {
     const indexJSON = await readStatsFile(`.github/actions/test/dist/${ changedPackage }-sb.json`);
     const componentSet = new Set();
