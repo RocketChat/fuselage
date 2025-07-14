747a5559ea56478975101895577f3d534492fb68
Anxhul10 <anxhul10@gmail.com>
2025-07-14 16:26:07 +0530

    chore: add try and catch and inclueded logic to include .storybook names

--- .github/actions/test/src/getReasons.js
+++ .github/actions/test/src/getReasons.js
@@ -1,18 +1,20 @@
 import { readStatsFile } from './stats/readStatsFile.js';
 
-const seenModules = new Set();
 /**
  * 
  * @param {string} name - component filename or path from src
  * @param {string} statsPath - path to .json file of stats
- * @returns {promise<string[]>} - list of affected components stories
+ * @returns {promise<string[]>} - list of affected components stories if it returns .storybook run full test on this stats package
  */
-export const getReasons = async (name, statsPath) => {
+export const getResonsLogic = async (name, statsPath) => {
   const stats = await readStatsFile(statsPath);
   const affectedStories = new Set();
+  const seenModules = new Set();
 
   const walk = (file) => {
-    if (seenModules.has(file)) return;
+    if (seenModules.has(file)) {
+      return
+    }
     seenModules.add(file);
 
     for (const module of stats.modules) {
@@ -26,6 +28,9 @@ export const getReasons = async (name, statsPath) => {
         if (module.reasons) {
           for (const reason of module.reasons) {
             if (reason.moduleName) {
+              if(reason.moduleName.includes('.storybook')){
+                affectedStories.add(reason.moduleName);
+              }
               walk(reason.moduleName);
             }
           }
@@ -39,6 +44,19 @@ export const getReasons = async (name, statsPath) => {
   } else {
     walk(name);
   }
-
   return [...affectedStories];
 };
+export const getReasons = async (name, statsPath) => {
+  const arr = await getResonsLogic(name, statsPath);
+  try {
+    if(arr.length === 0) {
+      throw new Error(`no matching ${name} in the ${statsPath}`);
+    }
+    return arr;
+  } catch(error) {
+    console.log(error.message);
+    return [];
+  }
+
+}
+
