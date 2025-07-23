1bc56992cd72e9438ef613e1e5170b3c70c116a0
Anxhul10 <anxhul10@gmail.com>
2025-07-24 02:16:34 +0530

    chore: chrome flags

--- .github/actions/test/src/runLoki.js
+++ .github/actions/test/src/runLoki.js
@@ -1,14 +1,14 @@
 import { execa } from 'execa';
 import * as core from '@actions/core';
 
-export const runLoki =  (storyPkg, reg) => {
+export const runLoki = async (storyPkg, reg) => {
     try {
         if(reg === 'full test') {
-            const subprocess =  execa('sh', ['-c', `cd packages/${storyPkg}  && yarn loki --requireReference --reactUri file:./storybook-static`], {
+            const subprocess = await execa('sh', ['-c', `cd packages/${storyPkg}  && yarn loki --requireReference --reactUri file:./storybook-static --chromeFlags=\"--headless --no-sandbox --disable-gpu --disable-features=VizDisplayCompositor\"`], {
                 stdio: 'inherit',
             });
         } else {
-            const subprocess =  execa('sh', ['-c', `cd packages/${storyPkg}  && yarn loki --requireReference --reactUri file:./storybook-static --storiesFilter="${reg}" --chromeTolerance=0.1`], {
+            const subprocess = await execa('sh', ['-c', `cd packages/${storyPkg}  && yarn loki --requireReference --reactUri file:./storybook-static --storiesFilter="${reg}" --chromeTolerance=0.1`], {
                 stdio: 'inherit',
             });
         }
