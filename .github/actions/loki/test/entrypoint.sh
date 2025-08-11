7e0f36af4e60b3c14721d8586fe35e34e7b3c24d
Anxhul10 <anxhul10@gmail.com>
2025-08-11 22:22:26 +0530

    chore: add pkg name from steps

--- .github/actions/loki/test/entrypoint.sh
+++ .github/actions/loki/test/entrypoint.sh
@@ -1,21 +1,48 @@
 #!/bin/sh -ex
 
-for pkg in packages/*/package.json; do
-  cd "$(dirname $pkg)"
-  mv package.json package.json-old
-  cat package.json-old | sed s/\.docker/.app/ > package.json
-  cd -
-done
+cd packages/$5
+mv package.json package.json-old
+cat package.json-old | sed s/\.docker/.app/ > package.json
 
-echo "$1"
+regex=''
+if [ "$5" = "fuselage" ]; then
+    regex=$1
+elif [ "$5" = "fuselage-toastbar" ]; then
+    regex=$2
+elif [ "$5" = "layout" ]; then
+    regex=$3
+elif [ "$5" = "onboarding-ui" ]; then
+    regex=$4
+else
+    regex="null"
+fi
 
-for pkg in packages/*/package.json; do
-  cd "$(dirname $pkg)"
-  mv package.json-old package.json
-  cd -
-done
+if [ "$regex" = "skip" ]; then 
+    echo "skipping visual test for fuselage"
+elif [ "$regex" = "full test" ]; then 
+    yarn loki:test-ci
+else
+    yarn loki test --chromeFlags='--headless --no-sandbox --disable-gpu --disable-features=VizDisplayCompositor' --requireReference --verboseRenderer --reactUri file:./storybook-static --storiesFilter="$regex"
+fi
 
-echo "fuselage: $1"
-echo "fuselage-toastbar: $2"
-echo "layout: $3"
-echo "onboarding-ui: $4"
+mv package.json-old package.json
+
+# for pkg in packages/*/package.json; do
+#   cd "$(dirname $pkg)"
+#   mv package.json package.json-old
+#   cat package.json-old | sed s/\.docker/.app/ > package.json
+#   cd -
+# done
+
+# echo "$1"
+
+# for pkg in packages/*/package.json; do
+#   cd "$(dirname $pkg)"
+#   mv package.json-old package.json
+#   cd -
+# done
+
+# echo "fuselage: $1"
+# echo "fuselage-toastbar: $2"
+# echo "layout: $3"
+# echo "onboarding-ui: $4"
