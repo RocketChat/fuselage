976af0c13da1942e14c3223d150dfba26d049d85
Anxhul10 <anxhul10@gmail.com>
2025-08-11 20:52:32 +0530

    chore: divide in scripts

--- .github/actions/loki/test/entrypoint.sh
+++ .github/actions/loki/test/entrypoint.sh
@@ -7,23 +7,7 @@ for pkg in packages/*/package.json; do
   cd -
 done
 
-storybookPkgs="packages/fuselage packages/fuselage-toastbar packages/layout packages/onboarding-ui"
-arg=1
-
-for pkg in $storybookPkgs; do
-    cd $pkg
-    arg_value=$(eval "echo \$$arg")
-    if [ "$arg_value" = "skip" ]; then 
-        echo "skipping visual tests for $pkg"
-    elif [ "$arg_value" = "full test" ]; then 
-        # yarn loki:test-ci
-        echo 'full-test'
-    else
-        yarn loki test --chromeConcurrency=2 --chromeFlags='--headless --no-sandbox --disable-gpu --disable-features=VizDisplayCompositor' --requireReference --verboseRenderer --reactUri file:./storybook-static --storiesFilter="$arg"
-    fi
-    arg=$((arg + 1))
-    cd -
-done
+echo "$1"
 
 for pkg in packages/*/package.json; do
   cd "$(dirname $pkg)"
