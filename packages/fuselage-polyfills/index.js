9bd3bf931fd169eebcb90bdc32e617b8b1e3e1c9
Tasso <tasso.evangelista@rocket.chat>
2025-10-07 00:52:01 -0300

    Remove `navigator.clipboard` polyfill

--- packages/fuselage-polyfills/index.js
+++ packages/fuselage-polyfills/index.js
@@ -1,6 +1,3 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-if (!navigator.clipboard) {
-  navigator.clipboard = require('clipboard-polyfill');
-}
 require('element-closest-polyfill');
 require('new-event-polyfill');
