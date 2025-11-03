d3809921d974ee3402505dd3d2bfdbc5df85fcaa
Matheus Cardoso <matheus@cardo.so>
2025-11-03 13:02:37 -0300

    fix(onboarding-ui): exclude dist from tests

--- packages/onboarding-ui/jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  errorOnDeprecated: true,
-  testMatch: ['<rootDir>/src/**/*.spec.ts?(x)'],
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-};
