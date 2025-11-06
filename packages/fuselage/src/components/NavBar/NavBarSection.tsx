1d0ea98e15f6e577e84c0e1db0b43d33d6faa3d9
Douglas Fabris <devfabris@gmail.com>
2025-11-06 09:49:38 -0300

    feat: render divider conditionally

--- packages/fuselage/src/components/NavBar/NavBarSection.tsx
+++ packages/fuselage/src/components/NavBar/NavBarSection.tsx
@@ -1,5 +1,22 @@
 import type { HTMLAttributes } from 'react';
+import { Fragment, Children, isValidElement } from 'react';
 
-export const NavBarSection = (props: HTMLAttributes<HTMLSpanElement>) => (
-  <span className='rcx-navbar-section' {...props} />
-);
+import { NavBarDivider } from './NavBarDivider';
+
+export const NavBarSection = ({
+  children,
+  ...props
+}: HTMLAttributes<HTMLSpanElement>) => {
+  const validChildren = Children.toArray(children).filter(isValidElement);
+
+  return (
+    <span className='rcx-navbar-section' {...props}>
+      {validChildren.map((child, index) => (
+        <Fragment key={index}>
+          {child}
+          {index < validChildren.length - 1 && <NavBarDivider />}
+        </Fragment>
+      ))}
+    </span>
+  );
+};
