f8ffc409b546c12ccc90f66db0f6e8e3af4c0948
Tasso <tasso.evangelista@rocket.chat>
2026-01-19 00:49:21 -0300

    feat(fuselage): remove deprecated `MessageMetrics.*` components

--- packages/fuselage/src/components/Message/MessageMetrics/MessageMetrics.tsx
+++ packages/fuselage/src/components/Message/MessageMetrics/MessageMetrics.tsx
@@ -1,26 +1,13 @@
 import type { HTMLAttributes } from 'react';
 
 import MessageMetricsContentItem from './MessageMetricsContentItem';
-import MessageMetricsFollowing from './MessageMetricsFollowing';
-import { MessageMetricsItem } from './MessageMetricsItem';
-import MessageMetricsReply from './MessageMetricsReply';
 
 export type MessageMetricsProps = HTMLAttributes<HTMLDivElement>;
 
-const MessageMetrics = Object.assign(
-  (props: MessageMetricsProps) => (
-    <MessageMetricsContentItem>
-      <div className='rcx-message-metrics__content-wrapper' {...props} />
-    </MessageMetricsContentItem>
-  ),
-  {
-    /** @deprecated prefer using named imports */
-    Reply: MessageMetricsReply,
-    /** @deprecated prefer using named imports */
-    Item: MessageMetricsItem,
-    /** @deprecated prefer using named imports */
-    Following: MessageMetricsFollowing,
-  },
+const MessageMetrics = (props: MessageMetricsProps) => (
+  <MessageMetricsContentItem>
+    <div className='rcx-message-metrics__content-wrapper' {...props} />
+  </MessageMetricsContentItem>
 );
 
 export default MessageMetrics;
