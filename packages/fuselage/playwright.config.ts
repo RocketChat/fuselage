c0ce78f980a409ae39681362505929047fd82e8e
gabriellsh <henriques.gabriell@gmail.com>
2026-02-05 18:57:12 -0300

    setup visual regression config

---
+++ packages/fuselage/playwright.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const BASE_URL = process.env.TEST_BASE_URL || 'http://localhost:6006';
+
+export default defineConfig({
+  testDir: './test',
+  fullyParallel: true,
+  workers: 4,
+  // ...
+  // Using the `html` reporter for visual diffing.
+  reporter: process.env.CI ? 'html' : 'dot',
+  // ...
+  use: {
+    baseURL: BASE_URL,
+  },
+  projects: [
+    {
+      name: 'desktop',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
