2f1b333b17bd1ed5070a1de17da34e541cf5dbd5
gabriellsh <henriques.gabriell@gmail.com>
2026-02-06 15:01:46 -0300

    fix: blank snapshots

--- packages/fuselage/playwright.config.ts
+++ packages/fuselage/playwright.config.ts
@@ -5,7 +5,7 @@ const BASE_URL = process.env.TEST_BASE_URL || 'http://localhost:6006';
 export default defineConfig({
   testDir: './test',
   fullyParallel: true,
-  workers: 4,
+  workers: process.env.CI ? 2 : 4,
   snapshotPathTemplate: './test/snapshots/{arg}-{projectName}.png',
   // ...
   // Using the `html` reporter for visual diffing.
