9e38c3868a368c6d80dd8758f79c45f4abf13378
gabriellsh <henriques.gabriell@gmail.com>
2026-02-06 10:36:51 -0300

    fix jest test match

--- packages/fuselage/jest.config.ts
+++ packages/fuselage/jest.config.ts
@@ -3,6 +3,10 @@ import type { Config } from 'jest';
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testMatch: [
+    '<rootDir>/src/**/*.spec.{ts,tsx}',
+    '!**/visualRegression.spec.ts',
+  ],
   setupFilesAfterEnv: [
     '<rootDir>/jest-setup.ts',
     'testing-utils/setup/noErrorsLogged',
