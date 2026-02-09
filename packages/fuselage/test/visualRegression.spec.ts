6b272b56c56ebb97b0e59595f968c0300fe09865
gabriellsh <henriques.gabriell@gmail.com>
2026-02-09 12:44:20 -0300

    log fonts

--- packages/fuselage/test/visualRegression.spec.ts
+++ packages/fuselage/test/visualRegression.spec.ts
@@ -1,3 +1,6 @@
+// import * as fs from 'fs';
+// import * as path from 'path';
+
 import { expect, test } from '@playwright/test';
 
 // This file is created by Storybook
@@ -8,6 +11,11 @@ import storybook from '../storybook-static/index.json' with { type: 'json' };
 const stories = Object.values(storybook.entries).filter(
   (e) => e.type === 'story' && !e.tags.includes('no-visual'),
 );
+
+// const PATH_USED_FONTS = path.join(process.cwd(), 'usedFonts');
+// await fs.promises.mkdir(PATH_USED_FONTS, { recursive: true });
+
+const loaded_fonts_all: unknown[][] = [];
 
 for (const story of stories) {
   test(`${story.title} ${story.name} should not have visual regressions`, async ({
@@ -24,9 +32,27 @@ for (const story of stories) {
 
     await page.locator('#storybook-root').waitFor({ state: 'visible' });
 
+    const loaded_fonts = await page.evaluate(async () => {
+      await document.fonts.ready; // Ensure all fonts are loaded
+      const fontFamilies = new Set();
+      for (const fontFace of document.fonts.values()) {
+        fontFamilies.add(fontFace.family);
+      }
+      return Array.from(fontFamilies);
+    });
+    loaded_fonts_all.push([story.name, loaded_fonts]);
+    console.log('loaded_fonts', loaded_fonts);
+
     await expect(page).toHaveScreenshot(`${story.id}.png`, {
       fullPage: true,
       animations: 'disabled',
     });
   });
 }
+
+// test.afterAll(async () => {
+//   await fs.promises.writeFile(
+//     path.join(PATH_USED_FONTS, 'loaded_fonts_all.json'),
+//     JSON.stringify(loaded_fonts_all, null, 2),
+//   );
+// });
