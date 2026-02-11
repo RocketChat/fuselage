259a7f5c082d4a9097a3008fa7c3185aaefa4990
gabriellsh <henriques.gabriell@gmail.com>
2026-02-11 14:13:54 -0300

    remove remote font from story wrapper

--- packages/fuselage/.storybook/preview.tsx
+++ packages/fuselage/.storybook/preview.tsx
@@ -81,22 +81,6 @@ export default {
 
       return (
         <>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link
-            rel='preconnect'
-            href='https://fonts.gstatic.com'
-            crossOrigin='anonymous'
-          />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
-            rel='stylesheet'
-          />
-          <style>
-            {`@font-face {
-            font-family: "Inter";
-            src: url(https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap) format("truetype");
-          }`}
-          </style>
           <PaletteStyleTag theme={dark ? 'dark' : 'light'} />
           <Story />
         </>
