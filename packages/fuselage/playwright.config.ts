5a626bc845fae560db10a5f116093420abd455d2
gabriellsh <henriques.gabriell@gmail.com>
2026-02-12 10:50:37 -0300

    Add flags back :P

--- packages/fuselage/playwright.config.ts
+++ packages/fuselage/playwright.config.ts
@@ -19,6 +19,16 @@ export default defineConfig({
       name: 'desktop',
       use: {
         ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: [
+            '--disable-lcd-text',
+            // Other useful flags for VRT consistency
+            '--font-render-hinting=none',
+            '--disable-skia-runtime-opts',
+            '--disable-font-subpixel-positioning',
+            '--disable-accelerated-2d-canvas',
+          ],
+        },
       },
     },
   ],
