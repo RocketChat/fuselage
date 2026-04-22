a7e248e21998f2066edf8df6d5a7d74dd01c6f69
juliajforesti <juliajforesti@gmail.com>
2026-04-22 18:41:01 -0300

    feat(fuselage): accept all `objectFit` css properties on `Avatar` component

--- packages/fuselage/src/components/Avatar/Avatar.tsx
+++ packages/fuselage/src/components/Avatar/Avatar.tsx
@@ -1,17 +1,17 @@
-import type { AllHTMLAttributes } from 'react';
+import type { AllHTMLAttributes, CSSProperties } from 'react';
 
 import AvatarContainer, { type AvatarContainerProps } from './AvatarContainer';
 
 export type AvatarProps = AvatarContainerProps & {
   rounded?: boolean;
-  objectFit?: boolean;
+  objectFit?: CSSProperties['objectFit'];
   url: string;
 } & Omit<AllHTMLAttributes<HTMLImageElement>, 'size'>;
 
 const Avatar = ({
   size = 'x36',
   rounded = false,
-  objectFit = false,
+  objectFit,
   url,
   className,
   alt,
@@ -19,9 +19,9 @@ const Avatar = ({
 }: AvatarProps) => {
   const innerClass = [
     'rcx-avatar__element',
-    objectFit && 'rcx-avatar__element--object-fit',
     size && `rcx-avatar__element--${size}`,
     rounded && 'rcx-avatar__element--rounded',
+    objectFit && `rcx-avatar__element--object-fit--${objectFit}`,
   ]
     .filter(Boolean)
     .join(' ');
