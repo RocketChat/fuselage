19e411832b6fa5aa94ed9ec7227b686884b7ed6b
Douglas Fabris <devfabris@gmail.com>
2026-04-23 17:59:52 -0300

    test: refactor MultiSelect tests for improved accessibility checks

--- packages/fuselage/src/components/MultiSelect/MultiSelect.spec.tsx
+++ packages/fuselage/src/components/MultiSelect/MultiSelect.spec.tsx
@@ -6,20 +6,27 @@ import { render } from '../../testing';
 
 import * as stories from './MultiSelect.stories';
 
-const { Default } = composeStories(stories);
+const testCases = Object.values(composeStories(stories)).map((Story) => [
+  Story.storyName || 'Story',
+  Story,
+]);
 
 withResizeObserverMock();
 
-describe('[MultiSelect Component]', () => {
-  it('renders without crashing', () => {
-    const tree = render(<Default />);
+test.each(testCases)(
+  `renders %s without crashing`,
+  async (_storyname, Story) => {
+    const tree = render(<Story />);
     expect(tree.baseElement).toMatchSnapshot();
-  });
+  },
+);
 
-  it('%s should have no a11y violations', async () => {
-    const { container } = render(<Default />);
+test.each(testCases)(
+  '%s should have no a11y violations',
+  async (_storyname, Story) => {
+    const { container } = render(<Story />);
 
     const results = await axe(container);
     expect(results).toHaveNoViolations();
-  });
-});
+  },
+);
