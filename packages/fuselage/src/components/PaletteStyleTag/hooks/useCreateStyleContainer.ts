4842800d9a590ea1a3c1fe815679a725bbc867c1
gabriellsh <henriques.gabriell@gmail.com>
2026-04-23 16:01:36 -0300

    Update `useCreateStyleContainer`

--- packages/fuselage/src/components/PaletteStyleTag/hooks/useCreateStyleContainer.ts
+++ packages/fuselage/src/components/PaletteStyleTag/hooks/useCreateStyleContainer.ts
@@ -1,20 +1,25 @@
 import { useMemo } from 'react';
 
-export const useCreateStyleContainer = (id: string) =>
-  useMemo(() => {
-    const refElement =
-      document.getElementById('rcx-styles') || document.head.lastChild;
+import { useTargetDocument } from '../../../contexts';
 
-    const el = document.getElementById(id);
+export const useCreateStyleContainer = (id: string) => {
+  const { document: targetDocument } = useTargetDocument();
+  return useMemo(() => {
+    const refElement =
+      targetDocument.getElementById('rcx-styles') ||
+      targetDocument.head.lastChild;
+
+    const el = targetDocument.getElementById(id);
 
     if (el) {
       return el;
     }
 
-    const styleElement = document.createElement('style');
+    const styleElement = targetDocument.createElement('style');
     styleElement.setAttribute('id', id);
 
-    document.head.insertBefore(styleElement, refElement);
-    document.head.appendChild(document.createElement('style'));
+    targetDocument.head.insertBefore(styleElement, refElement);
+    targetDocument.head.appendChild(targetDocument.createElement('style'));
     return styleElement;
-  }, [id]);
+  }, [id, targetDocument]);
+};
