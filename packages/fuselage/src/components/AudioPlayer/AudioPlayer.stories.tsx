bd1a6b297831306151b50ca7a5c70769ef18d16d
Guilherme Gazzo <guilherme@gazzo.xyz>
2026-07-01 11:39:35 -0300

    chore(fuselage): drop Controls story (needs visual-regression baseline)

--- packages/fuselage/src/components/AudioPlayer/AudioPlayer.stories.tsx
+++ packages/fuselage/src/components/AudioPlayer/AudioPlayer.stories.tsx
@@ -1,8 +1,6 @@
 import type { Meta } from '@storybook/react-webpack5';
-import { useState } from 'react';
 
 import AudioPlayer from './AudioPlayer';
-import AudioPlayerControls from './AudioPlayerControls';
 
 export default {
   title: 'Media/AudioPlayer',
@@ -13,24 +11,3 @@ const AUDIO_URL =
   'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-17.mp3';
 
 export const AudioPlayerDefault = () => <AudioPlayer src={AUDIO_URL} />;
-
-export const Controls = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [currentTime, setCurrentTime] = useState(12);
-  const [playbackSpeed, setPlaybackSpeed] = useState(1);
-  const durationTime = 86;
-
-  return (
-    <AudioPlayerControls
-      isPlaying={isPlaying}
-      currentTime={currentTime}
-      durationTime={durationTime}
-      playbackSpeed={playbackSpeed}
-      onTogglePlay={() => setIsPlaying((playing) => !playing)}
-      onSeek={setCurrentTime}
-      onChangePlaybackSpeed={() =>
-        setPlaybackSpeed((speed) => (speed >= 2 ? 1 : speed + 0.5))
-      }
-    />
-  );
-};
