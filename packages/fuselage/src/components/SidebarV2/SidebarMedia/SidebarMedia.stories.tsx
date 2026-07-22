92e05d0232480515c612db85159df40a8bf0ab06
juliajforesti <juliajforesti@gmail.com>
2026-07-22 14:48:42 -0300

    chore: convert `Sidebar` and `Sidepanel` stories to CSF3

--- packages/fuselage/src/components/SidebarV2/SidebarMedia/SidebarMedia.stories.tsx
+++ packages/fuselage/src/components/SidebarV2/SidebarMedia/SidebarMedia.stories.tsx
@@ -1,4 +1,4 @@
-import type { StoryFn, Meta } from '@storybook/react-webpack5';
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
 
 import {
   SidebarV2 as Sidebar,
@@ -13,20 +13,38 @@ import { GenericCallItem } from '../helpers';
 export default {
   title: 'Navigation/SidebarV2/Media',
   component: Sidebar,
+  argTypes: {
+    collapsed: {
+      control: 'boolean',
+      description: 'Renders the sidebar in its collapsed (narrow) state.',
+    },
+    children: {
+      control: false,
+      description: 'Content of the sidebar.',
+    },
+  },
 } satisfies Meta<typeof Sidebar>;
 
-export const Default: StoryFn<typeof Sidebar> = () => (
-  <Box>
-    <Sidebar>
-      <SidebarMedia>
-        <SidebarMediaTitle>3 calls in queue</SidebarMediaTitle>
-        <SidebarMediaController label='Call'>
-          <IconButton icon='user-arrow-right' small aria-label='user-forward' />
-          <IconButton icon='mic' small aria-label='mic' />
-          <IconButton icon='pause-unfilled' small aria-label='pause' />
-        </SidebarMediaController>
-        <GenericCallItem is='div' />
-      </SidebarMedia>
-    </Sidebar>
-  </Box>
-);
+type Story = StoryObj<typeof Sidebar>;
+
+export const Default: Story = {
+  render: () => (
+    <Box>
+      <Sidebar>
+        <SidebarMedia>
+          <SidebarMediaTitle>3 calls in queue</SidebarMediaTitle>
+          <SidebarMediaController label='Call'>
+            <IconButton
+              icon='user-arrow-right'
+              small
+              aria-label='user-forward'
+            />
+            <IconButton icon='mic' small aria-label='mic' />
+            <IconButton icon='pause-unfilled' small aria-label='pause' />
+          </SidebarMediaController>
+          <GenericCallItem is='div' />
+        </SidebarMedia>
+      </Sidebar>
+    </Box>
+  ),
+};
