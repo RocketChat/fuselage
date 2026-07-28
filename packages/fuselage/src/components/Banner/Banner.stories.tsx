bb08f3560c41666c7b3e65cb9d3806aea7b709ca
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2026-07-28 12:37:18 -0300

    chore(fuselage): convert stories to CSF3 (1/8) (#2116)

--- packages/fuselage/src/components/Banner/Banner.stories.tsx
+++ packages/fuselage/src/components/Banner/Banner.stories.tsx
@@ -1,3 +1,4 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
 import { action } from 'storybook/actions';
 
 import { Icon } from '../Icon';
@@ -7,155 +8,225 @@ import Banner from './Banner';
 export default {
   title: 'Feedback/Banner',
   component: Banner,
+  argTypes: {
+    title: {
+      control: 'text',
+      description: 'Heading text displayed at the top of the banner.',
+      table: { category: 'Content' },
+    },
+    children: {
+      control: 'text',
+      description: 'Body content of the banner.',
+      table: { category: 'Content' },
+    },
+    icon: {
+      control: false,
+      description:
+        'Icon element displayed before the content when the banner is wide enough.',
+      table: { category: 'Content' },
+    },
+    link: {
+      control: 'text',
+      description: 'URL rendered as the "more info" link.',
+      table: { category: 'Content' },
+    },
+    linkText: {
+      control: 'text',
+      description: 'Label of the link rendered when `link` is set.',
+      table: { category: 'Content', defaultValue: { summary: 'More info' } },
+    },
+    linkTarget: {
+      control: 'select',
+      options: ['_blank', '_self', '_parent', '_top'],
+      description: 'Anchor `target` used for the link rendered by `link`.',
+      table: { category: 'Content', defaultValue: { summary: '_blank' } },
+    },
+    variant: {
+      control: 'select',
+      options: ['neutral', 'info', 'success', 'warning', 'danger'],
+      description: 'Color kind of the banner.',
+      table: { category: 'Kind', defaultValue: { summary: 'neutral' } },
+    },
+    inline: {
+      control: 'boolean',
+      description: 'Renders a more compact, single-line layout.',
+      table: { category: 'Layout' },
+    },
+    actionable: {
+      control: 'boolean',
+      description:
+        'Makes the whole banner clickable, invoking `onAction` when activated.',
+      table: { category: 'State' },
+    },
+    closeable: {
+      control: 'boolean',
+      description: 'Shows a close button that invokes `onClose` when clicked.',
+      table: { category: 'State' },
+    },
+    onAction: {
+      control: false,
+      description: 'Called when an actionable banner is clicked.',
+      table: { category: 'Events' },
+    },
+    onClose: {
+      control: false,
+      description: 'Called when the close button is clicked.',
+      table: { category: 'Events' },
+    },
+  },
+} satisfies Meta<typeof Banner>;
+
+type Story = StoryObj<typeof Banner>;
+
+export const _Banner: Story = {
+  args: {
+    closeable: true,
+    icon: <Icon name='info' size={24} />,
+    title: 'Sed ut perspiciatis unde',
+    onClose: action('close'),
+    link: 'https://rocket.chat/',
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+  },
 };
 
-export const _Banner = () => (
-  <Banner
-    closeable
-    icon={<Icon name='info' size={24} />}
-    title='Sed ut perspiciatis unde'
-    onClose={action('close')}
-    link='https://rocket.chat/'
-  >
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor
-  </Banner>
-);
+export const Variations: Story = {
+  render: () => (
+    <>
+      <Banner
+        closeable
+        icon={<Icon name='info' size={24} />}
+        title='Sed ut perspiciatis unde'
+        variant='neutral'
+        onClose={action('close')}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </Banner>
+      <br />
+      <Banner
+        closeable
+        icon={<Icon name='info' size={24} />}
+        title='Sed ut perspiciatis unde'
+        variant='info'
+        onClose={action('close')}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </Banner>
+      <br />
+      <Banner
+        closeable
+        icon={<Icon name='warning' size={24} />}
+        title='Sed ut perspiciatis unde'
+        variant='warning'
+        onClose={action('close')}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </Banner>
+      <br />
+      <Banner
+        closeable
+        icon={<Icon name='ban' size={24} />}
+        title='Sed ut perspiciatis unde'
+        variant='danger'
+        onClose={action('close')}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </Banner>
+      <br />
+      <Banner
+        closeable
+        icon={<Icon name='circle-check' size={24} />}
+        title='Sed ut perspiciatis unde'
+        variant='success'
+        onClose={action('close')}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </Banner>
+    </>
+  ),
+};
 
-export const Variations = () => (
-  <>
-    <Banner
-      closeable
-      icon={<Icon name='info' size={24} />}
-      title='Sed ut perspiciatis unde'
-      variant='neutral'
-      onClose={action('close')}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor
-    </Banner>
-    <br />
-    <Banner
-      closeable
-      icon={<Icon name='info' size={24} />}
-      title='Sed ut perspiciatis unde'
-      variant='info'
-      onClose={action('close')}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor
-    </Banner>
-    <br />
-    <Banner
-      closeable
-      icon={<Icon name='warning' size={24} />}
-      title='Sed ut perspiciatis unde'
-      variant='warning'
-      onClose={action('close')}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor
-    </Banner>
-    <br />
-    <Banner
-      closeable
-      icon={<Icon name='ban' size={24} />}
-      title='Sed ut perspiciatis unde'
-      variant='danger'
-      onClose={action('close')}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor
-    </Banner>
-    <br />
-    <Banner
-      closeable
-      icon={<Icon name='circle-check' size={24} />}
-      title='Sed ut perspiciatis unde'
-      variant='success'
-      onClose={action('close')}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor
-    </Banner>
-  </>
-);
+export const Inline: Story = {
+  render: () => (
+    <>
+      <Banner
+        inline
+        closeable
+        icon={<Icon name='info' size={24} />}
+        title='Sed ut perspiciatis unde'
+        variant='neutral'
+        onClose={action('close')}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </Banner>
+      <br />
+      <Banner
+        inline
+        closeable
+        icon={<Icon name='info' size={24} />}
+        title='Sed ut perspiciatis unde'
+        variant='info'
+        onClose={action('close')}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </Banner>
+      <br />
+      <Banner
+        inline
+        closeable
+        icon={<Icon name='circle-check' size={24} />}
+        title='Sed ut perspiciatis unde'
+        variant='success'
+        onClose={action('close')}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </Banner>
+      <br />
+      <Banner
+        inline
+        closeable
+        icon={<Icon name='warning' size={24} />}
+        title='Sed ut perspiciatis unde'
+        variant='warning'
+        onClose={action('close')}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </Banner>
+      <br />
+      <Banner
+        inline
+        closeable
+        icon={<Icon name='ban' size={24} />}
+        title='Sed ut perspiciatis unde'
+        variant='danger'
+        onClose={action('close')}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor
+      </Banner>
+      <br />
+    </>
+  ),
+};
 
-export const Inline = () => (
-  <>
-    <Banner
-      inline
-      closeable
-      icon={<Icon name='info' size={24} />}
-      title='Sed ut perspiciatis unde'
-      variant='neutral'
-      onClose={action('close')}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor
-    </Banner>
-    <br />
-    <Banner
-      inline
-      closeable
-      icon={<Icon name='info' size={24} />}
-      title='Sed ut perspiciatis unde'
-      variant='info'
-      onClose={action('close')}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor
-    </Banner>
-    <br />
-    <Banner
-      inline
-      closeable
-      icon={<Icon name='circle-check' size={24} />}
-      title='Sed ut perspiciatis unde'
-      variant='success'
-      onClose={action('close')}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor
-    </Banner>
-    <br />
-    <Banner
-      inline
-      closeable
-      icon={<Icon name='warning' size={24} />}
-      title='Sed ut perspiciatis unde'
-      variant='warning'
-      onClose={action('close')}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor
-    </Banner>
-    <br />
-    <Banner
-      inline
-      closeable
-      icon={<Icon name='ban' size={24} />}
-      title='Sed ut perspiciatis unde'
-      variant='danger'
-      onClose={action('close')}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor
-    </Banner>
-    <br />
-  </>
-);
-
-export const Actionable = () => (
-  <Banner
-    actionable
-    closeable
-    icon={<Icon name='info' size={24} />}
-    title='Sed ut perspiciatis unde'
-    onAction={action('action')}
-    onClose={action('close')}
-  >
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor
-  </Banner>
-);
+export const Actionable: Story = {
+  args: {
+    actionable: true,
+    closeable: true,
+    icon: <Icon name='info' size={24} />,
+    title: 'Sed ut perspiciatis unde',
+    onAction: action('action'),
+    onClose: action('close'),
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+  },
+};
--- packages/fuselage/src/components/CodeSnippet/CodeSnippet.stories.tsx
+++ packages/fuselage/src/components/CodeSnippet/CodeSnippet.stories.tsx
@@ -1,4 +1,4 @@
-import type { StoryFn, Meta } from '@storybook/react-webpack5';
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
 import { action } from 'storybook/actions';
 
 import CodeSnippet from './CodeSnippet';
@@ -6,40 +6,67 @@ import CodeSnippet from './CodeSnippet';
 export default {
   title: 'Data Display/CodeSnippet',
   component: CodeSnippet,
+  argTypes: {
+    children: {
+      control: 'text',
+      description:
+        'Code or command text to display; renders a loading skeleton when empty.',
+      table: { category: 'Content' },
+    },
+    buttonText: {
+      control: 'text',
+      description: 'Label for the copy button. Defaults to `Copy`.',
+      table: { category: 'Content', defaultValue: { summary: 'Copy' } },
+    },
+    buttonDisabled: {
+      control: 'boolean',
+      description: 'Disables the copy button.',
+      table: { category: 'State' },
+    },
+    onClick: {
+      control: false,
+      description:
+        'Called when the copy button is clicked; the button is only rendered when this is provided.',
+      table: { category: 'Events' },
+    },
+  },
 } satisfies Meta<typeof CodeSnippet>;
 
-const Template: StoryFn<typeof CodeSnippet> = (args) => (
-  <CodeSnippet {...args} />
-);
+type Story = StoryObj<typeof CodeSnippet>;
 
-export const Default = Template.bind({});
-Default.args = {
-  children: 'curl -L https://go.rocket.chat/i/docker-compose.yml -O',
+export const Default: Story = {
+  args: {
+    children: 'curl -L https://go.rocket.chat/i/docker-compose.yml -O',
+  },
 };
 
-export const CopyButton = Template.bind({});
-CopyButton.args = {
-  children: 'curl -L https://go.rocket.chat/i/docker-compose.yml -O',
-  onClick: action('click'),
+export const CopyButton: Story = {
+  args: {
+    children: 'curl -L https://go.rocket.chat/i/docker-compose.yml -O',
+    onClick: action('click'),
+  },
 };
 
-export const CustomButtonName = Template.bind({});
-CustomButtonName.args = {
-  children: 'curl -L https://go.rocket.chat/i/docker-compose.yml -O',
-  onClick: action('click'),
-  buttonText: 'Custom name',
+export const CustomButtonName: Story = {
+  args: {
+    children: 'curl -L https://go.rocket.chat/i/docker-compose.yml -O',
+    onClick: action('click'),
+    buttonText: 'Custom name',
+  },
 };
 
-export const LoadingCode = Template.bind({});
-LoadingCode.args = {
-  children: '',
-  onClick: action('click'),
+export const LoadingCode: Story = {
+  args: {
+    children: '',
+    onClick: action('click'),
+  },
 };
 
-export const DisabledButton = Template.bind({});
-DisabledButton.args = {
-  children: 'curl -L https://go.rocket.chat/i/docker-compose.yml -O',
-  onClick: action('click'),
-  buttonText: 'Disabled Button',
-  buttonDisabled: true,
+export const DisabledButton: Story = {
+  args: {
+    children: 'curl -L https://go.rocket.chat/i/docker-compose.yml -O',
+    onClick: action('click'),
+    buttonText: 'Disabled Button',
+    buttonDisabled: true,
+  },
 };
