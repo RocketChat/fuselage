e8e53d861322d3d59d8c72aa505dfa95b3c565c7
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2026-07-29 16:25:08 -0300

    chore(fuselage): convert `Box` stories to CSF3 (7/8) (#2122)

--- packages/fuselage/src/components/Box/richContentBlock.stories.tsx
+++ packages/fuselage/src/components/Box/richContentBlock.stories.tsx
@@ -1,4 +1,4 @@
-import type { StoryFn, Meta } from '@storybook/react-webpack5';
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
 
 import Box from './Box';
 
@@ -14,152 +14,174 @@ export default {
   },
 } satisfies Meta<typeof Box>;
 
-export const Hx: StoryFn<typeof Box> = () => (
-  <>
-    <h1>Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
-    <h4>Heading 4</h4>
-    <h5>Heading 5</h5>
-    <h6>Heading 6</h6>
-  </>
-);
-Hx.storyName = 'hx';
+type Story = StoryObj<typeof Box>;
 
-export const P: StoryFn<typeof Box> = () => <p>Paragraph</p>;
-P.storyName = 'p';
+export const Hx: Story = {
+  name: 'hx',
+  render: () => (
+    <>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+    </>
+  ),
+};
 
-export const Ul: StoryFn<typeof Box> = () => (
-  <ul>
-    <li>unordered list item 1</li>
-    <li>unordered list item 2</li>
-    <li>unordered list item 3</li>
-  </ul>
-);
-Ul.storyName = 'ul';
+export const P: Story = {
+  name: 'p',
+  render: () => <p>Paragraph</p>,
+};
 
-export const Ol: StoryFn<typeof Box> = () => (
-  <ol>
-    <li>ordered list item 1</li>
-    <li>ordered list item 2</li>
-    <li>ordered list item 3</li>
-  </ol>
-);
-Ol.storyName = 'ol';
+export const Ul: Story = {
+  name: 'ul',
+  render: () => (
+    <ul>
+      <li>unordered list item 1</li>
+      <li>unordered list item 2</li>
+      <li>unordered list item 3</li>
+    </ul>
+  ),
+};
 
-export const Dl: StoryFn<typeof Box> = () => (
-  <dl>
-    <dt>description list term 1</dt>
-    <dd>description list description 1</dd>
-    <dt>description list term 2</dt>
-    <dd>description list description 2</dd>
-    <dt>description list term 3</dt>
-    <dd>description list description 3</dd>
-  </dl>
-);
-Dl.storyName = 'dl';
+export const Ol: Story = {
+  name: 'ol',
+  render: () => (
+    <ol>
+      <li>ordered list item 1</li>
+      <li>ordered list item 2</li>
+      <li>ordered list item 3</li>
+    </ol>
+  ),
+};
 
-export const Table: StoryFn<typeof Box> = () => (
-  <table>
-    <thead>
-      <tr>
-        <th>Column 1</th>
-        <th>Column 2</th>
-        <th>Column 3</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Item 1 of row 1</td>
-        <td>Item 2 of row 1</td>
-        <td>Item 3 of row 1</td>
-      </tr>
-      <tr>
-        <td>Item 1 of row 2</td>
-        <td>Item 2 of row 2</td>
-        <td>Item 3 of row 2</td>
-      </tr>
-      <tr>
-        <td>Item 1 of row 3</td>
-        <td>Item 2 of row 3</td>
-        <td>Item 3 of row 3</td>
-      </tr>
-    </tbody>
-  </table>
-);
-Table.storyName = 'table';
+export const Dl: Story = {
+  name: 'dl',
+  render: () => (
+    <dl>
+      <dt>description list term 1</dt>
+      <dd>description list description 1</dd>
+      <dt>description list term 2</dt>
+      <dd>description list description 2</dd>
+      <dt>description list term 3</dt>
+      <dd>description list description 3</dd>
+    </dl>
+  ),
+};
 
-export const TableComplex: StoryFn<typeof Box> = () => (
-  <table>
-    <caption>Caption</caption>
-    <thead>
-      <tr>
-        <th>Column 1</th>
-        <th>Column 2</th>
-        <th>Column 3</th>
-      </tr>
-    </thead>
-    <tfoot>
-      <tr>
-        <th>Column 1</th>
-        <th>Column 2</th>
-        <th>Column 3</th>
-      </tr>
-    </tfoot>
-    <tbody>
-      <tr>
-        <td>Item 1 of row 1</td>
-        <td>Item 2 of row 1</td>
-        <td>Item 3 of row 1</td>
-      </tr>
-      <tr>
-        <td>Item 1 of row 2</td>
-        <td>Item 2 of row 2</td>
-        <td>Item 3 of row 2</td>
-      </tr>
-      <tr>
-        <td>Item 1 of row 3</td>
-        <td>Item 2 of row 3</td>
-        <td>Item 3 of row 3</td>
-      </tr>
-    </tbody>
-  </table>
-);
-TableComplex.storyName = 'table (complex)';
+export const Table: Story = {
+  name: 'table',
+  render: () => (
+    <table>
+      <thead>
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+          <th>Column 3</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Item 1 of row 1</td>
+          <td>Item 2 of row 1</td>
+          <td>Item 3 of row 1</td>
+        </tr>
+        <tr>
+          <td>Item 1 of row 2</td>
+          <td>Item 2 of row 2</td>
+          <td>Item 3 of row 2</td>
+        </tr>
+        <tr>
+          <td>Item 1 of row 3</td>
+          <td>Item 2 of row 3</td>
+          <td>Item 3 of row 3</td>
+        </tr>
+      </tbody>
+    </table>
+  ),
+};
 
-export const BlockquoteAndCite: StoryFn<typeof Box> = () => (
-  <blockquote>
-    <p>
-      Words can be like X-rays, if you use them properly — they’ll go through
-      anything. You read and you’re pierced.
-    </p>
-    <footer>
-      — Aldous Huxley, <cite>Brave New World</cite>
-    </footer>
-  </blockquote>
-);
-BlockquoteAndCite.storyName = 'blockquote and cite';
+export const TableComplex: Story = {
+  name: 'table (complex)',
+  render: () => (
+    <table>
+      <caption>Caption</caption>
+      <thead>
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+          <th>Column 3</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+          <th>Column 3</th>
+        </tr>
+      </tfoot>
+      <tbody>
+        <tr>
+          <td>Item 1 of row 1</td>
+          <td>Item 2 of row 1</td>
+          <td>Item 3 of row 1</td>
+        </tr>
+        <tr>
+          <td>Item 1 of row 2</td>
+          <td>Item 2 of row 2</td>
+          <td>Item 3 of row 2</td>
+        </tr>
+        <tr>
+          <td>Item 1 of row 3</td>
+          <td>Item 2 of row 3</td>
+          <td>Item 3 of row 3</td>
+        </tr>
+      </tbody>
+    </table>
+  ),
+};
 
-export const Pre: StoryFn<typeof Box> = () => (
-  <pre>
-    # A UNINTENDED LOL-ZONE: SORRY FOR THIS{'\n'}#
-    ------------+----------+-------------{'\n'}# /\O | _O | O {'\n'}# /\/ | //|_
-    | /_ {'\n'}# /\ | | | |\ {'\n'}# / \ | /| | / | {'\n'}# LOL LOL | LLOL |
-    LOLLOL {'\n'}# ------------+----------+-------------{'\n'}# BLACK MAGIC FULL
-    FEATURED ENABLED
-  </pre>
-);
-Pre.storyName = 'pre';
+export const BlockquoteAndCite: Story = {
+  name: 'blockquote and cite',
+  render: () => (
+    <blockquote>
+      <p>
+        Words can be like X-rays, if you use them properly — they’ll go through
+        anything. You read and you’re pierced.
+      </p>
+      <footer>
+        — Aldous Huxley, <cite>Brave New World</cite>
+      </footer>
+    </blockquote>
+  ),
+};
 
-export const PreWithCode: StoryFn<typeof Box> = () => (
-  <pre>
-    <code>
+export const Pre: Story = {
+  name: 'pre',
+  render: () => (
+    <pre>
       # A UNINTENDED LOL-ZONE: SORRY FOR THIS{'\n'}#
       ------------+----------+-------------{'\n'}# /\O | _O | O {'\n'}# /\/ |
       //|_ | /_ {'\n'}# /\ | | | |\ {'\n'}# / \ | /| | / | {'\n'}# LOL LOL |
       LLOL | LOLLOL {'\n'}# ------------+----------+-------------{'\n'}# BLACK
       MAGIC FULL FEATURED ENABLED
-    </code>
-  </pre>
-);
-PreWithCode.storyName = 'pre with code';
+    </pre>
+  ),
+};
+
+export const PreWithCode: Story = {
+  name: 'pre with code',
+  render: () => (
+    <pre>
+      <code>
+        # A UNINTENDED LOL-ZONE: SORRY FOR THIS{'\n'}#
+        ------------+----------+-------------{'\n'}# /\O | _O | O {'\n'}# /\/ |
+        //|_ | /_ {'\n'}# /\ | | | |\ {'\n'}# / \ | /| | / | {'\n'}# LOL LOL |
+        LLOL | LOLLOL {'\n'}# ------------+----------+-------------{'\n'}# BLACK
+        MAGIC FULL FEATURED ENABLED
+      </code>
+    </pre>
+  ),
+};
